b305e48234d1c2c8442bd2c39bc36087fe422c9c
rgronback <rgronback>
2005-08-19 00:02:33 +0000

    Added %* to allow arguments to pass.

--- releng/org.eclipse.gmf.releng.builder/scripts/cruise.bat
+++ releng/org.eclipse.gmf.releng.builder/scripts/cruise.bat
@@ -1,4 +1,4 @@
 @echo off
 rem You must have the variable CC_HOME set pointing to the CruiseControl installation directory
 
-java -Xmx1024m -jar %CC_HOME%\main\dist\cruisecontrol.jar
+java -Xmx1024m -jar %CC_HOME%\main\dist\cruisecontrol.jar %*
