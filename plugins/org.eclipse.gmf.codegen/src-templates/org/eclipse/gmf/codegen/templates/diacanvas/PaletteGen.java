45efa2e3234daa5068c329fe47c1a27f4ebc66f8
atikhomirov <atikhomirov>
2005-09-26 14:32:37 +0000

    [ashatalin] Hand-wirtten code for additional initialization in CreationFactory should be subject to "@generated NOT" protection (generating separate inner class for each palette tool's factory).

--- plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/diacanvas/PaletteGen.java
+++ plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/diacanvas/PaletteGen.java
@@ -41,47 +41,49 @@ public class PaletteGen
   protected final String TEXT_21 = NL + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate void setupCommonTools() {";
   protected final String TEXT_22 = NL;
   protected final String TEXT_23 = "\t\tPaletteGroup common = new PaletteGroup(\"Common controls\");" + NL + "" + NL + "\t\tToolEntry selectionTool = new PanningSelectionToolEntry();" + NL + "\t\tcommon.add(selectionTool);" + NL + "\t\tcommon.add(new MarqueeToolEntry());" + NL + "" + NL + "\t\tgetRoot().setDefaultEntry(selectionTool);" + NL + "" + NL + "\t\tgetRoot().add(common);" + NL + "\t}" + NL;
-  protected final String TEXT_24 = NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate void createNodeTool";
-  protected final String TEXT_25 = "(PaletteContainer paletteContainer) {";
-  protected final String TEXT_26 = NL;
-  protected final String TEXT_27 = NL + "//\t\tImageDescriptor img = ImageDescriptor.createFromFile(";
-  protected final String TEXT_28 = ".class, \"";
-  protected final String TEXT_29 = "\");" + NL + "\t\tImageDescriptor img = getImage(";
-  protected final String TEXT_30 = ");" + NL + "\t\tCreationFactory creationFactory = new CreationFactory() {" + NL + "\t\t\tpublic Object getNewObject() {" + NL + "\t\t\t\t";
-  protected final String TEXT_31 = " diagramNode = ";
-  protected final String TEXT_32 = ".eINSTANCE.create";
-  protected final String TEXT_33 = "();" + NL + "\t\t\t\tdiagramNode.setDomainModelElement(";
-  protected final String TEXT_34 = ");" + NL + "\t\t\t\tdiagramNode.setVisualID(";
-  protected final String TEXT_35 = ".VISUAL_ID);";
-  protected final String TEXT_36 = NL + "\t\t\t\tdiagramNode.setGroupID(\"";
-  protected final String TEXT_37 = "\");";
-  protected final String TEXT_38 = NL + "\t\t\t\tinitialize(diagramNode);" + NL + "\t\t\t\treturn diagramNode;" + NL + "\t\t\t}" + NL + "\t\t\tpublic Object getObjectType() {";
-  protected final String TEXT_39 = NL + "\t\t\t\treturn ";
-  protected final String TEXT_40 = ".class;";
-  protected final String TEXT_41 = NL + "\t\t\t\treturn ";
-  protected final String TEXT_42 = ".class;";
-  protected final String TEXT_43 = NL + "\t\t\t}" + NL + "\t\t\tprivate void initialize(";
-  protected final String TEXT_44 = " diagramNode) {" + NL + "\t\t\t\t// place initialization code for attributes specific to your Diagram RunTime model here" + NL + "\t\t\t}" + NL + "\t\t};" + NL + "\t\tpaletteContainer.add(new CreationToolEntry(\"";
-  protected final String TEXT_45 = "\", \"";
-  protected final String TEXT_46 = "\", creationFactory, img, img));" + NL + "\t}";
-  protected final String TEXT_47 = NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate void createLinkTool";
-  protected final String TEXT_48 = "(PaletteContainer paletteContainer) {";
-  protected final String TEXT_49 = NL;
-  protected final String TEXT_50 = NL + "\t\t//ImageDescriptor img = ImageDescriptor.createFromFile(";
+  protected final String TEXT_24 = NL;
+  protected final String TEXT_25 = NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate void createNodeTool";
+  protected final String TEXT_26 = "(PaletteContainer paletteContainer) {" + NL + "" + NL + "//\t\tImageDescriptor img = ImageDescriptor.createFromFile(";
+  protected final String TEXT_27 = ".class, \"";
+  protected final String TEXT_28 = "\");" + NL + "\t\tImageDescriptor img = getImage(";
+  protected final String TEXT_29 = ");" + NL + "\t\tCreationFactory creationFactory = new CreationNodeFactory";
+  protected final String TEXT_30 = "();" + NL + "\t\tpaletteContainer.add(new CreationToolEntry(\"";
+  protected final String TEXT_31 = "\", \"";
+  protected final String TEXT_32 = "\", creationFactory, img, img));" + NL + "\t}" + NL + "\t" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate class CreationNodeFactory";
+  protected final String TEXT_33 = " implements CreationFactory {" + NL + "\t" + NL + "\t\t/**" + NL + "\t     * @generated" + NL + "\t     */" + NL + "\t\tpublic Object getNewObject() {" + NL + "\t\t\t";
+  protected final String TEXT_34 = " diagramNode = ";
+  protected final String TEXT_35 = ".eINSTANCE.create";
+  protected final String TEXT_36 = "();" + NL + "\t\t\tdiagramNode.setDomainModelElement(";
+  protected final String TEXT_37 = ");" + NL + "\t\t\tdiagramNode.setVisualID(";
+  protected final String TEXT_38 = ".VISUAL_ID);";
+  protected final String TEXT_39 = NL + "\t\t\tdiagramNode.setGroupID(\"";
+  protected final String TEXT_40 = "\");";
+  protected final String TEXT_41 = NL + "\t\t\tinitialize(diagramNode);" + NL + "\t\t\treturn diagramNode;" + NL + "\t\t}" + NL + "\t\t" + NL + "\t\t/**" + NL + "\t     * @generated" + NL + "\t     */" + NL + "\t\tpublic Object getObjectType() {";
+  protected final String TEXT_42 = NL + "\t\t\treturn ";
+  protected final String TEXT_43 = ".class;";
+  protected final String TEXT_44 = NL + "\t\t\treturn ";
+  protected final String TEXT_45 = ".class;";
+  protected final String TEXT_46 = NL + "\t\t}" + NL + "\t\t" + NL + "\t\t/**" + NL + "\t     * @generated" + NL + "\t     */" + NL + "\t\tprivate void initialize(";
+  protected final String TEXT_47 = " diagramNode) {" + NL + "\t\t// place initialization code for attributes specific to your Diagram RunTime model here" + NL + "\t\t}" + NL + "\t};";
+  protected final String TEXT_48 = NL;
+  protected final String TEXT_49 = NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate void createLinkTool";
+  protected final String TEXT_50 = "(PaletteContainer paletteContainer) {" + NL + "" + NL + "\t\t//ImageDescriptor img = ImageDescriptor.createFromFile(";
   protected final String TEXT_51 = ".class, \"";
   protected final String TEXT_52 = "\");*/";
   protected final String TEXT_53 = NL + "\t\tImageDescriptor img = getImage(";
-  protected final String TEXT_54 = ");" + NL + "\t\tCreationFactory creationFactory = new CreationFactory() {" + NL + "\t\t\tpublic Object getNewObject() {" + NL + "\t\t\t\t";
-  protected final String TEXT_55 = " diagramLink = ";
-  protected final String TEXT_56 = ".eINSTANCE.create";
-  protected final String TEXT_57 = "();" + NL + "\t\t\t\tdiagramLink.setVisualID(";
-  protected final String TEXT_58 = ".VISUAL_ID);" + NL + "\t\t\t\tdiagramLink.setDomainModelElement(";
-  protected final String TEXT_59 = ");" + NL + "\t\t\t\tinitialize(diagramLink);" + NL + "\t\t\t\treturn diagramLink;" + NL + "\t\t\t}" + NL + "\t\t\tpublic Object getObjectType() {" + NL + "\t\t\t\treturn ";
-  protected final String TEXT_60 = ".class;" + NL + "\t\t\t}" + NL + "\t\t\tprivate void initialize(";
-  protected final String TEXT_61 = " diagramLink) {" + NL + "\t\t\t\t// place initialization code for attributes specific to your Diagram RunTime model here" + NL + "\t\t\t}" + NL + "\t\t};" + NL + "\t\tpaletteContainer.add(new ConnectionCreationToolEntry(\"";
-  protected final String TEXT_62 = "\", \"";
-  protected final String TEXT_63 = "\", creationFactory, img, img));" + NL + "\t}";
-  protected final String TEXT_64 = NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate PaletteContainer createContainer(String title) {" + NL + "\t\treturn new PaletteDrawer(title);" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate ImageDescriptor getImage(Object o) {" + NL + "\t\tIItemLabelProvider labelProvider = (IItemLabelProvider) myAdapterFactory.adapt(o, IItemLabelProvider.class);" + NL + "\t\tif (labelProvider != null) {" + NL + "\t\t\treturn ExtendedImageRegistry.getInstance().getImageDescriptor(labelProvider.getImage(o));" + NL + "\t\t}" + NL + "\t\treturn null;" + NL + "\t}" + NL + "}";
+  protected final String TEXT_54 = ");" + NL + "\t\tCreationFactory creationFactory = new CreationLinkFactory";
+  protected final String TEXT_55 = "();" + NL + "\t\tpaletteContainer.add(new ConnectionCreationToolEntry(\"";
+  protected final String TEXT_56 = "\", \"";
+  protected final String TEXT_57 = "\", creationFactory, img, img));" + NL + "\t}" + NL + "\t" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate class CreationLinkFactory";
+  protected final String TEXT_58 = " implements CreationFactory {" + NL + "\t" + NL + "\t\t/**" + NL + "\t     * @generated" + NL + "\t     */" + NL + "\t\tpublic Object getNewObject() {" + NL + "\t\t\t";
+  protected final String TEXT_59 = " diagramLink = ";
+  protected final String TEXT_60 = ".eINSTANCE.create";
+  protected final String TEXT_61 = "();" + NL + "\t\t\tdiagramLink.setVisualID(";
+  protected final String TEXT_62 = ".VISUAL_ID);" + NL + "\t\t\tdiagramLink.setDomainModelElement(";
+  protected final String TEXT_63 = ");" + NL + "\t\t\tinitialize(diagramLink);" + NL + "\t\t\treturn diagramLink;" + NL + "\t\t}" + NL + "" + NL + "\t" + NL + "\t\t/**" + NL + "\t     * @generated" + NL + "\t     */" + NL + "\t\tpublic Object getObjectType() {" + NL + "\t\t\treturn ";
+  protected final String TEXT_64 = ".class;" + NL + "\t\t}" + NL + "\t\t" + NL + "\t" + NL + "\t\t/**" + NL + "\t     * @generated" + NL + "\t     */" + NL + "\t\tprivate void initialize(";
+  protected final String TEXT_65 = " diagramLink) {" + NL + "\t\t\t// place initialization code for attributes specific to your Diagram RunTime model here" + NL + "\t\t}" + NL + "\t};";
+  protected final String TEXT_66 = NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate PaletteContainer createContainer(String title) {" + NL + "\t\treturn new PaletteDrawer(title);" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate ImageDescriptor getImage(Object o) {" + NL + "\t\tIItemLabelProvider labelProvider = (IItemLabelProvider) myAdapterFactory.adapt(o, IItemLabelProvider.class);" + NL + "\t\tif (labelProvider != null) {" + NL + "\t\t\treturn ExtendedImageRegistry.getInstance().getImageDescriptor(labelProvider.getImage(o));" + NL + "\t\t}" + NL + "\t\treturn null;" + NL + "\t}" + NL + "}";
 
   public String generate(Object argument)
   {
@@ -148,66 +150,68 @@ for(Iterator it = palette.getGroups().iterator(); it.hasNext(); toolGroupIndex++
 		NodeEntry nodeEntry = (NodeEntry) it2.next();
 
     stringBuffer.append(TEXT_24);
-    stringBuffer.append(nodeToolIndex);
-    stringBuffer.append(TEXT_25);
-    stringBuffer.append(TEXT_26);
     
 GenNode genNode = nodeEntry.getGenNode(); 
 GenClass genClass = genDiagram.findGenClass(genNode.getDomainMetaClass());
 GenClass drtClass = genNode.getDiagramRunTimeClass();
 final String domainElementInstanceCreationCode = importManager.getImportedName(genClass.getGenPackage().getQualifiedFactoryInterfaceName()) + ".eINSTANCE.create" + genClass.getName() + "()";
 
-    stringBuffer.append(TEXT_27);
+    stringBuffer.append(TEXT_25);
+    stringBuffer.append(nodeToolIndex);
+    stringBuffer.append(TEXT_26);
     stringBuffer.append(palette.getFactoryClassName());
-    stringBuffer.append(TEXT_28);
+    stringBuffer.append(TEXT_27);
     stringBuffer.append(nodeEntry.getSmallIconPath());
+    stringBuffer.append(TEXT_28);
+    stringBuffer.append(domainElementInstanceCreationCode);
     stringBuffer.append(TEXT_29);
-    stringBuffer.append(domainElementInstanceCreationCode);
+    stringBuffer.append(nodeToolIndex);
     stringBuffer.append(TEXT_30);
-    stringBuffer.append(importManager.getImportedName(drtClass.getQualifiedInterfaceName()));
+    stringBuffer.append(nodeEntry.getTitleKey());
     stringBuffer.append(TEXT_31);
-    stringBuffer.append(importManager.getImportedName(drtClass.getGenPackage().getQualifiedFactoryInterfaceName()));
+    stringBuffer.append(nodeEntry.getDescriptionKey());
     stringBuffer.append(TEXT_32);
-    stringBuffer.append(drtClass.getName());
+    stringBuffer.append(nodeToolIndex);
     stringBuffer.append(TEXT_33);
-    stringBuffer.append(domainElementInstanceCreationCode);
+    stringBuffer.append(importManager.getImportedName(drtClass.getQualifiedInterfaceName()));
     stringBuffer.append(TEXT_34);
-    stringBuffer.append(importManager.getImportedName(genNode.getEditPartQualifiedClassName()));
+    stringBuffer.append(importManager.getImportedName(drtClass.getGenPackage().getQualifiedFactoryInterfaceName()));
     stringBuffer.append(TEXT_35);
-    if (genNode instanceof GenChildNode) {
+    stringBuffer.append(drtClass.getName());
     stringBuffer.append(TEXT_36);
-    stringBuffer.append(((GenChildNode) genNode).getGroupID());
+    stringBuffer.append(domainElementInstanceCreationCode);
     stringBuffer.append(TEXT_37);
-    }
+    stringBuffer.append(importManager.getImportedName(genNode.getEditPartQualifiedClassName()));
     stringBuffer.append(TEXT_38);
     if (genNode instanceof GenChildNode) {
     stringBuffer.append(TEXT_39);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.diagramrt.ChildNode"));
+    stringBuffer.append(((GenChildNode) genNode).getGroupID());
     stringBuffer.append(TEXT_40);
-    } else {
-    stringBuffer.append(TEXT_41);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.diagramrt.DiagramNode"));
-    stringBuffer.append(TEXT_42);
     }
+    stringBuffer.append(TEXT_41);
+    if (genNode instanceof GenChildNode) {
+    stringBuffer.append(TEXT_42);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.diagramrt.ChildNode"));
     stringBuffer.append(TEXT_43);
-    stringBuffer.append(importManager.getImportedName(drtClass.getQualifiedInterfaceName()));
+    } else {
     stringBuffer.append(TEXT_44);
-    stringBuffer.append(nodeEntry.getTitleKey());
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.diagramrt.DiagramNode"));
     stringBuffer.append(TEXT_45);
-    stringBuffer.append(nodeEntry.getDescriptionKey());
+    }
     stringBuffer.append(TEXT_46);
+    stringBuffer.append(importManager.getImportedName(drtClass.getQualifiedInterfaceName()));
+    stringBuffer.append(TEXT_47);
     	}
 	for (Iterator it2 = tg.getLinkTools().iterator(); it2.hasNext(); linkToolIndex++) {
 		LinkEntry linkEntry = (LinkEntry) it2.next();
 
-    stringBuffer.append(TEXT_47);
-    stringBuffer.append(linkToolIndex);
     stringBuffer.append(TEXT_48);
-    stringBuffer.append(TEXT_49);
     
 GenLink genLink = linkEntry.getGenLink();
 GenClass drtClass = genLink.getDiagramRunTimeClass();
 
+    stringBuffer.append(TEXT_49);
+    stringBuffer.append(linkToolIndex);
     stringBuffer.append(TEXT_50);
     stringBuffer.append(palette.getFactoryClassName());
     stringBuffer.append(TEXT_51);
@@ -227,27 +231,31 @@ if (genLink instanceof GenLinkWithClass) {
     stringBuffer.append(TEXT_53);
     stringBuffer.append(domainElementInstanceCreationCode );
     stringBuffer.append(TEXT_54);
-    stringBuffer.append(importManager.getImportedName(drtClass.getQualifiedInterfaceName()));
+    stringBuffer.append(linkToolIndex);
     stringBuffer.append(TEXT_55);
-    stringBuffer.append(importManager.getImportedName(drtClass.getGenPackage().getQualifiedFactoryInterfaceName()));
-    stringBuffer.append(TEXT_56);
-    stringBuffer.append(drtClass.getName());
-    stringBuffer.append(TEXT_57);
-    stringBuffer.append(importManager.getImportedName(genLink.getEditPartQualifiedClassName()));
-    stringBuffer.append(TEXT_58);
-    stringBuffer.append(domainElementInstanceCreationCode);
-    stringBuffer.append(TEXT_59);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.diagramrt.DiagramLink"));
-    stringBuffer.append(TEXT_60);
-    stringBuffer.append(importManager.getImportedName(drtClass.getQualifiedInterfaceName()));
-    stringBuffer.append(TEXT_61);
     stringBuffer.append(linkEntry.getTitleKey());
-    stringBuffer.append(TEXT_62);
+    stringBuffer.append(TEXT_56);
     stringBuffer.append(linkEntry.getDescriptionKey());
+    stringBuffer.append(TEXT_57);
+    stringBuffer.append(linkToolIndex);
+    stringBuffer.append(TEXT_58);
+    stringBuffer.append(importManager.getImportedName(drtClass.getQualifiedInterfaceName()));
+    stringBuffer.append(TEXT_59);
+    stringBuffer.append(importManager.getImportedName(drtClass.getGenPackage().getQualifiedFactoryInterfaceName()));
+    stringBuffer.append(TEXT_60);
+    stringBuffer.append(drtClass.getName());
+    stringBuffer.append(TEXT_61);
+    stringBuffer.append(importManager.getImportedName(genLink.getEditPartQualifiedClassName()));
+    stringBuffer.append(TEXT_62);
+    stringBuffer.append(domainElementInstanceCreationCode);
     stringBuffer.append(TEXT_63);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.diagramrt.DiagramLink"));
+    stringBuffer.append(TEXT_64);
+    stringBuffer.append(importManager.getImportedName(drtClass.getQualifiedInterfaceName()));
+    stringBuffer.append(TEXT_65);
     	}
 }
-    stringBuffer.append(TEXT_64);
+    stringBuffer.append(TEXT_66);
     importManager.emitSortedImports();
     return stringBuffer.toString();
   }
