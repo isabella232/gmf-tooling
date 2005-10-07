3beba05c343119fceb5f40127a58ebba2ac9ce08
atikhomirov <atikhomirov>
2005-10-07 18:02:45 +0000

    [ashatalin] - import all required EMF.Edit plugins - avoid duplicated plugin imports

--- plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/PluginXML.java
+++ plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/PluginXML.java
@@ -21,32 +21,29 @@ public class PluginXML
   protected final String TEXT_3 = "\"" + NL + "    version=\"1.0.0\"" + NL + "    class=\"";
   protected final String TEXT_4 = "\"" + NL + "    provider-name = \"";
   protected final String TEXT_5 = "\">" + NL + "" + NL + "  <requires>" + NL + "    <import plugin=\"org.eclipse.core.runtime\"/>" + NL + "    <import plugin=\"org.eclipse.core.resources\"/>" + NL + "    <import plugin=\"org.eclipse.jface\"/>" + NL + "    <import plugin=\"org.eclipse.ui.ide\"/>" + NL + "    <import plugin=\"org.eclipse.ui.views\"/>" + NL + "    <import plugin=\"org.eclipse.ui.workbench\"/>" + NL + "    <import plugin=\"org.eclipse.emf.ecore\"/>";
-  protected final String TEXT_6 = NL + "    <import plugin=\"org.eclipse.emf.edit.ui\"/>" + NL + "    <import plugin=\"org.eclipse.gef\" export=\"true\"/>" + NL + "    <import plugin=\"org.eclipse.gmf.runtime.gef\"/>" + NL + "    <import plugin=\"org.eclipse.gmf.runtime.emf.commands.core\"/>" + NL + "    <import plugin=\"org.eclipse.gmf.runtime.diagram.ui\"/>" + NL + "    <import plugin=\"org.eclipse.gmf.runtime.diagram.ui.resources.editor\"/>" + NL + "    <import plugin=\"org.eclipse.gmf.runtime.diagram.ui.resources.editor.ide\"/>" + NL + "    <import plugin=\"";
-  protected final String TEXT_7 = "\" export=\"true\"/>";
-  protected final String TEXT_8 = NL + "    <import plugin=\"";
-  protected final String TEXT_9 = "\" export=\"true\"/>";
-  protected final String TEXT_10 = NL + "    <import plugin=\"";
-  protected final String TEXT_11 = "\" export=\"true\"/>";
-  protected final String TEXT_12 = NL + "  </requires>" + NL + "" + NL + "  <runtime>" + NL + "    <library name=\"plugin.jar\">" + NL + "      <export name=\"*\"/>" + NL + "    </library>" + NL + "  </runtime>" + NL + "" + NL + "   <extension point=\"org.eclipse.core.runtime.preferences\">" + NL + "      <initializer class=\"";
-  protected final String TEXT_13 = ".PreferencesInitializer\"/>" + NL + "   </extension>" + NL + "" + NL + "  <extension point=\"org.eclipse.team.core.fileTypes\">" + NL + "      <fileTypes" + NL + "         type=\"text\"" + NL + "         extension=\"";
-  protected final String TEXT_14 = "\">" + NL + "      </fileTypes>" + NL + "  </extension>" + NL + "" + NL + "   <extension point=\"org.eclipse.emf.ecore.extension_parser\">" + NL + "      <parser" + NL + "         type=\"";
-  protected final String TEXT_15 = "\"" + NL + "         class=\"org.eclipse.gmf.runtime.emf.core.resources.MResourceFactory\">" + NL + "      </parser>" + NL + "   </extension>" + NL + "" + NL + "   <extension point=\"org.eclipse.ui.editors\">" + NL + "     <editor" + NL + "        id=\"";
-  protected final String TEXT_16 = "ID\"" + NL + "        name=\"";
-  protected final String TEXT_17 = " Diagram Editor\"" + NL + "        icon=\"icons/full/obj16/";
-  protected final String TEXT_18 = "ModelFile.gif\"" + NL + "        extensions=\"";
-  protected final String TEXT_19 = "\"" + NL + "        default=\"true\"" + NL + "        matchingStrategy=\"";
-  protected final String TEXT_20 = ".MDiagramEditorMatchingStrategy\"" + NL + "        class=\"";
-  protected final String TEXT_21 = ".";
-  protected final String TEXT_22 = "\"" + NL + "        contributorClass=\"org.eclipse.gmf.runtime.diagram.ui.internal.parts.MinimalActionBarContributor\">" + NL + "     </editor>" + NL + "   </extension>" + NL + "" + NL + "   <extension point=\"org.eclipse.ui.newWizards\">" + NL + "  \t  <wizard" + NL + "  \t     name=\"";
-  protected final String TEXT_23 = " Diagram\"" + NL + "  \t     icon=\"icons/full/obj16/";
-  protected final String TEXT_24 = "ModelFile.gif\"" + NL + "  \t     category=\"org.eclipse.ui.Examples\"" + NL + "  \t     class=\"";
-  protected final String TEXT_25 = ".CreationWizard\"" + NL + "  \t     id=\"";
-  protected final String TEXT_26 = ".CreationWizardID\">" + NL + "  \t  \t <description>" + NL + "  \t  \t\tCreates ";
-  protected final String TEXT_27 = " diagram." + NL + "  \t  \t </description>  " + NL + "      </wizard>" + NL + "   </extension>" + NL + "" + NL + "   <extension point=\"org.eclipse.gmf.runtime.diagram.core.viewProviders\">" + NL + "      <viewProvider class=\"";
-  protected final String TEXT_28 = ".ViewProvider\">" + NL + "         <Priority name=\"Lowest\"/>" + NL + "         <context viewClass=\"org.eclipse.gmf.runtime.notation.Diagram\" semanticHints=\"";
-  protected final String TEXT_29 = "\"/>" + NL + "         <context viewClass=\"org.eclipse.gmf.runtime.notation.Node\" semanticHints=\"\"/>" + NL + "         <context viewClass=\"org.eclipse.gmf.runtime.notation.Edge\" semanticHints=\"\"/>" + NL + "      </viewProvider>" + NL + "   </extension>" + NL + "" + NL + "   <extension point=\"org.eclipse.gmf.runtime.diagram.ui.editpartProviders\">" + NL + "      <editpartProvider class=\"";
-  protected final String TEXT_30 = ".EditPartProvider\">" + NL + "         <Priority name=\"Lowest\"/>" + NL + "      </editpartProvider>" + NL + "   </extension>" + NL + "" + NL + "</plugin>";
-  protected final String TEXT_31 = NL;
+  protected final String TEXT_6 = NL + "    <import plugin=\"org.eclipse.emf.edit.ui\"/>" + NL + "    <import plugin=\"org.eclipse.gef\" export=\"true\"/>" + NL + "    <import plugin=\"org.eclipse.gmf.runtime.gef\"/>" + NL + "    <import plugin=\"org.eclipse.gmf.runtime.emf.commands.core\"/>" + NL + "    <import plugin=\"org.eclipse.gmf.runtime.diagram.ui\"/>" + NL + "    <import plugin=\"org.eclipse.gmf.runtime.diagram.ui.resources.editor\"/>" + NL + "    <import plugin=\"org.eclipse.gmf.runtime.diagram.ui.resources.editor.ide\"/>";
+  protected final String TEXT_7 = NL + "    <import plugin=\"";
+  protected final String TEXT_8 = "\" export=\"true\"/>";
+  protected final String TEXT_9 = NL + "  </requires>" + NL + "" + NL + "  <runtime>" + NL + "    <library name=\"plugin.jar\">" + NL + "      <export name=\"*\"/>" + NL + "    </library>" + NL + "  </runtime>" + NL + "" + NL + "   <extension point=\"org.eclipse.core.runtime.preferences\">" + NL + "      <initializer class=\"";
+  protected final String TEXT_10 = ".PreferencesInitializer\"/>" + NL + "   </extension>" + NL + "" + NL + "  <extension point=\"org.eclipse.team.core.fileTypes\">" + NL + "      <fileTypes" + NL + "         type=\"text\"" + NL + "         extension=\"";
+  protected final String TEXT_11 = "\">" + NL + "      </fileTypes>" + NL + "  </extension>" + NL + "" + NL + "   <extension point=\"org.eclipse.emf.ecore.extension_parser\">" + NL + "      <parser" + NL + "         type=\"";
+  protected final String TEXT_12 = "\"" + NL + "         class=\"org.eclipse.gmf.runtime.emf.core.resources.MResourceFactory\">" + NL + "      </parser>" + NL + "   </extension>" + NL + "" + NL + "   <extension point=\"org.eclipse.ui.editors\">" + NL + "     <editor" + NL + "        id=\"";
+  protected final String TEXT_13 = "ID\"" + NL + "        name=\"";
+  protected final String TEXT_14 = " Diagram Editor\"" + NL + "        icon=\"icons/full/obj16/";
+  protected final String TEXT_15 = "ModelFile.gif\"" + NL + "        extensions=\"";
+  protected final String TEXT_16 = "\"" + NL + "        default=\"true\"" + NL + "        matchingStrategy=\"";
+  protected final String TEXT_17 = ".MDiagramEditorMatchingStrategy\"" + NL + "        class=\"";
+  protected final String TEXT_18 = ".";
+  protected final String TEXT_19 = "\"" + NL + "        contributorClass=\"org.eclipse.gmf.runtime.diagram.ui.internal.parts.MinimalActionBarContributor\">" + NL + "     </editor>" + NL + "   </extension>" + NL + "" + NL + "   <extension point=\"org.eclipse.ui.newWizards\">" + NL + "  \t  <wizard" + NL + "  \t     name=\"";
+  protected final String TEXT_20 = " Diagram\"" + NL + "  \t     icon=\"icons/full/obj16/";
+  protected final String TEXT_21 = "ModelFile.gif\"" + NL + "  \t     category=\"org.eclipse.ui.Examples\"" + NL + "  \t     class=\"";
+  protected final String TEXT_22 = ".CreationWizard\"" + NL + "  \t     id=\"";
+  protected final String TEXT_23 = ".CreationWizardID\">" + NL + "  \t  \t <description>" + NL + "  \t  \t\tCreates ";
+  protected final String TEXT_24 = " diagram." + NL + "  \t  \t </description>  " + NL + "      </wizard>" + NL + "   </extension>" + NL + "" + NL + "   <extension point=\"org.eclipse.gmf.runtime.diagram.core.viewProviders\">" + NL + "      <viewProvider class=\"";
+  protected final String TEXT_25 = ".ViewProvider\">" + NL + "         <Priority name=\"Lowest\"/>" + NL + "         <context viewClass=\"org.eclipse.gmf.runtime.notation.Diagram\" semanticHints=\"";
+  protected final String TEXT_26 = "\"/>" + NL + "         <context viewClass=\"org.eclipse.gmf.runtime.notation.Node\" semanticHints=\"\"/>" + NL + "         <context viewClass=\"org.eclipse.gmf.runtime.notation.Edge\" semanticHints=\"\"/>" + NL + "      </viewProvider>" + NL + "   </extension>" + NL + "" + NL + "   <extension point=\"org.eclipse.gmf.runtime.diagram.ui.editpartProviders\">" + NL + "      <editpartProvider class=\"";
+  protected final String TEXT_27 = ".EditPartProvider\">" + NL + "         <Priority name=\"Lowest\"/>" + NL + "      </editpartProvider>" + NL + "   </extension>" + NL + "" + NL + "</plugin>";
+  protected final String TEXT_28 = NL;
 
   public String generate(Object argument)
   {
@@ -67,61 +64,67 @@ public class PluginXML
 	// FIXME move image registry to plugin class code
 
     stringBuffer.append(TEXT_6);
-    stringBuffer.append(genModel.getModelPluginID());
-    stringBuffer.append(TEXT_7);
-    if (!genModel.sameModelEditProject()) {
-    stringBuffer.append(TEXT_8);
-    stringBuffer.append(genModel.getEditPluginID());
-    stringBuffer.append(TEXT_9);
-    }
     
+Set requiredPluginIDs = new LinkedHashSet();
+requiredPluginIDs.add(genModel.getModelPluginID());
+requiredPluginIDs.add(genModel.getEditPluginID());
+
+for (Iterator it = genModel.getAllUsedGenPackagesWithClassifiers().iterator(); it.hasNext();) {
+	GenModel nextGenModel = ((GenPackage) it.next()).getGenModel();
+	if (nextGenModel.hasEditSupport()) {
+		requiredPluginIDs.add(nextGenModel.getEditPluginID());
+	}
+}
+
 String[] requiredPlugins = genDiagram.getRequiredPluginIDs();
 if (requiredPlugins == null) {
 	requiredPlugins = new String[0];
 }
-for (Iterator it = Arrays.asList(requiredPlugins).iterator(); it.hasNext();) {
-    stringBuffer.append(TEXT_10);
+requiredPluginIDs.addAll(Arrays.asList(requiredPlugins));
+
+for (Iterator it = requiredPluginIDs.iterator(); it.hasNext();) {
+    stringBuffer.append(TEXT_7);
     stringBuffer.append(it.next());
-    stringBuffer.append(TEXT_11);
+    stringBuffer.append(TEXT_8);
     }
+    stringBuffer.append(TEXT_9);
+    stringBuffer.append(genDiagram.getEditorPackageName());
+    stringBuffer.append(TEXT_10);
+    stringBuffer.append(genDiagram.getDiagramFileExtension());
+    stringBuffer.append(TEXT_11);
+    stringBuffer.append(genDiagram.getDiagramFileExtension());
     stringBuffer.append(TEXT_12);
-    stringBuffer.append(genDiagram.getEditorPackageName());
-    stringBuffer.append(TEXT_13);
-    stringBuffer.append(genDiagram.getDiagramFileExtension());
-    stringBuffer.append(TEXT_14);
-    stringBuffer.append(genDiagram.getDiagramFileExtension());
-    stringBuffer.append(TEXT_15);
     stringBuffer.append(genDiagram.getEditorQualifiedClassName());
-    stringBuffer.append(TEXT_16);
+    stringBuffer.append(TEXT_13);
     stringBuffer.append(genModel.getModelName());
-    stringBuffer.append(TEXT_17);
+    stringBuffer.append(TEXT_14);
     stringBuffer.append(genModel.getModelName());
-    stringBuffer.append(TEXT_18);
+    stringBuffer.append(TEXT_15);
     stringBuffer.append(genDiagram.getDiagramFileExtension());
-    stringBuffer.append(TEXT_19);
+    stringBuffer.append(TEXT_16);
     stringBuffer.append(genDiagram.getEditorPackageName());
-    stringBuffer.append(TEXT_20);
+    stringBuffer.append(TEXT_17);
     stringBuffer.append(genDiagram.getEditorPackageName());
-    stringBuffer.append(TEXT_21);
+    stringBuffer.append(TEXT_18);
     stringBuffer.append(genDiagram.getEditorClassName());
-    stringBuffer.append(TEXT_22);
+    stringBuffer.append(TEXT_19);
     stringBuffer.append(genModel.getModelName());
+    stringBuffer.append(TEXT_20);
+    stringBuffer.append(genModel.getModelName());
+    stringBuffer.append(TEXT_21);
+    stringBuffer.append(genDiagram.getEditorPackageName());
+    stringBuffer.append(TEXT_22);
+    stringBuffer.append(genDiagram.getEditorPackageName());
     stringBuffer.append(TEXT_23);
     stringBuffer.append(genModel.getModelName());
     stringBuffer.append(TEXT_24);
-    stringBuffer.append(genDiagram.getEditorPackageName());
+    stringBuffer.append(genDiagram.getProvidersPackageName());
     stringBuffer.append(TEXT_25);
-    stringBuffer.append(genDiagram.getEditorPackageName());
+    stringBuffer.append(genModel.getModelName());
     stringBuffer.append(TEXT_26);
-    stringBuffer.append(genModel.getModelName());
+    stringBuffer.append(genDiagram.getProvidersPackageName());
     stringBuffer.append(TEXT_27);
-    stringBuffer.append(genDiagram.getProvidersPackageName());
     stringBuffer.append(TEXT_28);
-    stringBuffer.append(genModel.getModelName());
-    stringBuffer.append(TEXT_29);
-    stringBuffer.append(genDiagram.getProvidersPackageName());
-    stringBuffer.append(TEXT_30);
-    stringBuffer.append(TEXT_31);
     return stringBuffer.toString();
   }
 }
