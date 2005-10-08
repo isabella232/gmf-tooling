7474179f36344357f1ba3f4353c5184e71150469
rgronback <rgronback>
2005-10-08 11:15:02 +0000

    Added scripts for Linux.

---
+++ releng/org.eclipse.gmf.releng.builder/scripts/build.sh
@@ -0,0 +1,1 @@
+java -Xmx1024m -cp ../../org.eclipse.releng.basebuilder/startup.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner "$@"
---
+++ releng/org.eclipse.gmf.releng.builder/scripts/cruise.sh
@@ -0,0 +1,1 @@
+ant.sh -f cruise.xml "$@"
