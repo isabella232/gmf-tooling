15b5dce0836f224cf08efa01f3badf378b498f91
atikhomirov <atikhomirov>
2005-10-29 15:27:30 +0000

    Comment on test launch from PDE and as part of build process

--- tests/org.eclipse.gmf.tests/src/org/eclipse/gmf/tests/setup/RuntimeWorkspaceSetup.java
+++ tests/org.eclipse.gmf.tests/src/org/eclipse/gmf/tests/setup/RuntimeWorkspaceSetup.java
@@ -35,7 +35,16 @@ import org.eclipse.pde.internal.ui.wizards.imports.PluginImportOperation;
 import org.osgi.framework.Bundle;
 
 /**
- * If you get CCE while running tests in PDE environment, try to set read-only attribute for the next file:
+ * With PDE, we need source code in the running workspace to allow compilation of our code 
+ * (because PDE doesn't reexport set of plugins from it's running configuration, and it's no longer possible 
+ * to set Target Platform to "same as running" as it was back in Eclipse 2.x).
+ * 
+ * Classloading works because there's -dev argument in the command line. With PDE launch, it's done by PDE.
+ * Without PDE, running tests as part of the build relies on Eclipse Testing Framework's org.eclipse.test_3.1.0/library.xml
+ * which specifies "-dev bin". Once it's not specified, or new format (properties file with plugin-id=binfolder) 
+ * is in use, classloading of the generated code will fail and another mechanism should be invented then.
+ * 
+ * If you get ClassNotFoundException while running tests in PDE environment, try to set read-only attribute for the next file:
  * 'development-workspace'\.metadata\.plugins\org.eclipse.pde.core\'JUnitLaunchConfigName'\dev.properties
  * @author artem
  */
