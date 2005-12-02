ca42999fcd142bd10d007c9bbd1fc47092fefd47
ashatalin <ashatalin>
2005-12-02 13:53:09 +0000

    Correcting imports

--- plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/PaletteFactoryGenerator.java
+++ plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/PaletteFactoryGenerator.java
@@ -18,7 +18,7 @@ public class PaletteFactoryGenerator
   protected final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
   protected final String TEXT_1 = "package ";
   protected final String TEXT_2 = ";" + NL;
-  protected final String TEXT_3 = NL + "import org.eclipse.gef.Tool;" + NL + "import org.eclipse.gef.palette.MarqueeToolEntry;" + NL + "import org.eclipse.gef.palette.PaletteContainer;" + NL + "import org.eclipse.gef.palette.PaletteDrawer;" + NL + "import org.eclipse.gef.palette.PaletteGroup;" + NL + "import org.eclipse.gef.palette.PaletteRoot;" + NL + "import org.eclipse.gef.palette.PaletteSeparator;" + NL + "import org.eclipse.gef.palette.PanningSelectionToolEntry;" + NL + "import org.eclipse.gef.palette.ToolEntry;" + NL + "import org.eclipse.gmf.runtime.diagram.ui.tools.ConnectionCreationTool;" + NL + "import org.eclipse.gmf.runtime.diagram.ui.tools.CreationTool;" + NL + "import org.eclipse.gmf.runtime.notation.Diagram;" + NL + "import org.eclipse.jface.resource.ImageDescriptor;";
+  protected final String TEXT_3 = NL + "import org.eclipse.gef.Tool;" + NL + "import org.eclipse.gef.palette.MarqueeToolEntry;" + NL + "import org.eclipse.gef.palette.PaletteContainer;" + NL + "import org.eclipse.gef.palette.PaletteDrawer;" + NL + "import org.eclipse.gef.palette.PaletteGroup;" + NL + "import org.eclipse.gef.palette.PaletteRoot;" + NL + "import org.eclipse.gef.palette.PanningSelectionToolEntry;" + NL + "import org.eclipse.gef.palette.ToolEntry;" + NL + "import org.eclipse.gmf.runtime.diagram.ui.tools.CreationTool;" + NL + "import org.eclipse.gmf.runtime.notation.Diagram;" + NL + "import org.eclipse.jface.resource.ImageDescriptor;";
   protected final String TEXT_4 = NL + NL + "/**" + NL + " * @generated" + NL + " */" + NL + "public class ";
   protected final String TEXT_5 = " {" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate Diagram diagram;" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate ";
   protected final String TEXT_6 = "(Diagram diagram) {" + NL + "\t\tthis.diagram = diagram;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected final Diagram getDiagram() {" + NL + "\t\treturn diagram;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic static PaletteRoot createPalette(Diagram diagram) {" + NL + "\t\tPaletteRoot paletteRoot = new PaletteRoot();" + NL + "\t\t";
@@ -41,30 +41,32 @@ public class PaletteFactoryGenerator
   protected final String TEXT_23 = "\");";
   protected final String TEXT_24 = NL + "\t\tpaletteContainer.add(create";
   protected final String TEXT_25 = "CreationTool());";
-  protected final String TEXT_26 = NL + "\t\tpaletteContainer.add(new PaletteSeparator());";
-  protected final String TEXT_27 = NL + "\t\tpaletteContainer.add(create";
-  protected final String TEXT_28 = "CreationTool());";
-  protected final String TEXT_29 = NL + "\t\treturn paletteContainer;" + NL + "\t}";
-  protected final String TEXT_30 = NL + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate ToolEntry create";
-  protected final String TEXT_31 = "CreationTool() {" + NL + "\t\tImageDescriptor image = ";
-  protected final String TEXT_32 = ".getImageDescriptor(" + NL + "\t\t\t";
-  protected final String TEXT_33 = ".";
-  protected final String TEXT_34 = ");" + NL + "\t\treturn new ToolEntry(\"";
-  protected final String TEXT_35 = "\", \"";
-  protected final String TEXT_36 = "\", image, image) {" + NL + "" + NL + "\t\t\tpublic Tool createTool() {" + NL + "\t\t\t\tTool tool = new CreationTool(";
-  protected final String TEXT_37 = ".";
-  protected final String TEXT_38 = ");" + NL + "\t\t\t\ttool.setProperties(getToolProperties());" + NL + "\t\t\t\treturn tool;" + NL + "\t\t\t}" + NL + "\t\t};" + NL + "\t}";
-  protected final String TEXT_39 = NL + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate ToolEntry create";
-  protected final String TEXT_40 = "CreationTool() {" + NL + "\t\tImageDescriptor image = ";
-  protected final String TEXT_41 = ".getImageDescriptor(" + NL + "\t\t\t";
-  protected final String TEXT_42 = ".";
-  protected final String TEXT_43 = ");" + NL + "\t\treturn new ToolEntry(\"";
-  protected final String TEXT_44 = "\", \"";
-  protected final String TEXT_45 = "\", image, image) {" + NL + "" + NL + "\t\t\tpublic Tool createTool() {" + NL + "\t\t\t\tTool tool = new ConnectionCreationTool(";
-  protected final String TEXT_46 = ".";
-  protected final String TEXT_47 = ");" + NL + "\t\t\t\ttool.setProperties(getToolProperties());" + NL + "\t\t\t\treturn tool;" + NL + "\t\t\t}" + NL + "\t\t};" + NL + "\t}";
-  protected final String TEXT_48 = NL + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate PaletteContainer createContainer(String title) {" + NL + "\t\treturn new PaletteDrawer(title);" + NL + "\t}" + NL + "}";
-  protected final String TEXT_49 = NL;
+  protected final String TEXT_26 = NL + "\t\tpaletteContainer.add(new ";
+  protected final String TEXT_27 = "());";
+  protected final String TEXT_28 = NL + "\t\tpaletteContainer.add(create";
+  protected final String TEXT_29 = "CreationTool());";
+  protected final String TEXT_30 = NL + "\t\treturn paletteContainer;" + NL + "\t}";
+  protected final String TEXT_31 = NL + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate ToolEntry create";
+  protected final String TEXT_32 = "CreationTool() {" + NL + "\t\tImageDescriptor image = ";
+  protected final String TEXT_33 = ".getImageDescriptor(" + NL + "\t\t\t";
+  protected final String TEXT_34 = ".";
+  protected final String TEXT_35 = ");" + NL + "\t\treturn new ToolEntry(\"";
+  protected final String TEXT_36 = "\", \"";
+  protected final String TEXT_37 = "\", image, image) {" + NL + "" + NL + "\t\t\tpublic Tool createTool() {" + NL + "\t\t\t\tTool tool = new CreationTool(";
+  protected final String TEXT_38 = ".";
+  protected final String TEXT_39 = ");" + NL + "\t\t\t\ttool.setProperties(getToolProperties());" + NL + "\t\t\t\treturn tool;" + NL + "\t\t\t}" + NL + "\t\t};" + NL + "\t}";
+  protected final String TEXT_40 = NL + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate ToolEntry create";
+  protected final String TEXT_41 = "CreationTool() {" + NL + "\t\tImageDescriptor image = ";
+  protected final String TEXT_42 = ".getImageDescriptor(" + NL + "\t\t\t";
+  protected final String TEXT_43 = ".";
+  protected final String TEXT_44 = ");" + NL + "\t\treturn new ToolEntry(\"";
+  protected final String TEXT_45 = "\", \"";
+  protected final String TEXT_46 = "\", image, image) {" + NL + "" + NL + "\t\t\tpublic Tool createTool() {" + NL + "\t\t\t\tTool tool = new ";
+  protected final String TEXT_47 = "(";
+  protected final String TEXT_48 = ".";
+  protected final String TEXT_49 = ");" + NL + "\t\t\t\ttool.setProperties(getToolProperties());" + NL + "\t\t\t\treturn tool;" + NL + "\t\t\t}" + NL + "\t\t};" + NL + "\t}";
+  protected final String TEXT_50 = NL + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate PaletteContainer createContainer(String title) {" + NL + "\t\treturn new PaletteDrawer(title);" + NL + "\t}" + NL + "}";
+  protected final String TEXT_51 = NL;
 
   public String generate(Object argument)
   {
@@ -138,12 +140,14 @@ for (int i = 0; i < toolGroups.size(); i++) {
 		if (j == 0) {
 
     stringBuffer.append(TEXT_26);
-    		}
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gef.palette.PaletteSeparator"));
     stringBuffer.append(TEXT_27);
-    stringBuffer.append(AccessUtil.getPaletteEntryId(linkCreationTool));
+    		}
     stringBuffer.append(TEXT_28);
-    	}
+    stringBuffer.append(AccessUtil.getPaletteEntryId(linkCreationTool));
     stringBuffer.append(TEXT_29);
+    	}
+    stringBuffer.append(TEXT_30);
     
 }
 for (int i = 0; i < toolGroups.size(); i++) {
@@ -153,23 +157,23 @@ for (int i = 0; i < toolGroups.size(); i++) {
 		NodeEntry nodeCreationTool = (NodeEntry) nodeCreationTools.get(j);
 		GenNode genNode = nodeCreationTool.getGenNode(); 
 
-    stringBuffer.append(TEXT_30);
-    stringBuffer.append(AccessUtil.getPaletteEntryId(nodeCreationTool));
     stringBuffer.append(TEXT_31);
-    stringBuffer.append(importManager.getImportedName(genDiagram.getElementTypesQualifiedClassName()));
+    stringBuffer.append(AccessUtil.getPaletteEntryId(nodeCreationTool));
     stringBuffer.append(TEXT_32);
     stringBuffer.append(importManager.getImportedName(genDiagram.getElementTypesQualifiedClassName()));
     stringBuffer.append(TEXT_33);
-    stringBuffer.append(genNode.getUniqueIdentifier());
-    stringBuffer.append(TEXT_34);
-    stringBuffer.append(nodeCreationTool.getTitleKey());
-    stringBuffer.append(TEXT_35);
-    stringBuffer.append(nodeCreationTool.getDescriptionKey());
-    stringBuffer.append(TEXT_36);
     stringBuffer.append(importManager.getImportedName(genDiagram.getElementTypesQualifiedClassName()));
-    stringBuffer.append(TEXT_37);
+    stringBuffer.append(TEXT_34);
     stringBuffer.append(genNode.getUniqueIdentifier());
+    stringBuffer.append(TEXT_35);
+    stringBuffer.append(nodeCreationTool.getTitleKey());
+    stringBuffer.append(TEXT_36);
+    stringBuffer.append(nodeCreationTool.getDescriptionKey());
+    stringBuffer.append(TEXT_37);
+    stringBuffer.append(importManager.getImportedName(genDiagram.getElementTypesQualifiedClassName()));
     stringBuffer.append(TEXT_38);
+    stringBuffer.append(genNode.getUniqueIdentifier());
+    stringBuffer.append(TEXT_39);
     
 	}
 	List linkCreationTools = toolGroup.getLinkTools();
@@ -177,30 +181,32 @@ for (int i = 0; i < toolGroups.size(); i++) {
 		LinkEntry linkCreationTool = (LinkEntry) linkCreationTools.get(j);
 		GenLink genLink = linkCreationTool.getGenLink();
 
-    stringBuffer.append(TEXT_39);
-    stringBuffer.append(AccessUtil.getPaletteEntryId(linkCreationTool));
     stringBuffer.append(TEXT_40);
-    stringBuffer.append(importManager.getImportedName(genDiagram.getElementTypesQualifiedClassName()));
+    stringBuffer.append(AccessUtil.getPaletteEntryId(linkCreationTool));
     stringBuffer.append(TEXT_41);
     stringBuffer.append(importManager.getImportedName(genDiagram.getElementTypesQualifiedClassName()));
     stringBuffer.append(TEXT_42);
-    stringBuffer.append(genLink.getUniqueIdentifier());
-    stringBuffer.append(TEXT_43);
-    stringBuffer.append(linkCreationTool.getTitleKey());
-    stringBuffer.append(TEXT_44);
-    stringBuffer.append(linkCreationTool.getDescriptionKey());
-    stringBuffer.append(TEXT_45);
     stringBuffer.append(importManager.getImportedName(genDiagram.getElementTypesQualifiedClassName()));
-    stringBuffer.append(TEXT_46);
+    stringBuffer.append(TEXT_43);
     stringBuffer.append(genLink.getUniqueIdentifier());
+    stringBuffer.append(TEXT_44);
+    stringBuffer.append(linkCreationTool.getTitleKey());
+    stringBuffer.append(TEXT_45);
+    stringBuffer.append(linkCreationTool.getDescriptionKey());
+    stringBuffer.append(TEXT_46);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.diagram.ui.tools.ConnectionCreationTool"));
     stringBuffer.append(TEXT_47);
+    stringBuffer.append(importManager.getImportedName(genDiagram.getElementTypesQualifiedClassName()));
+    stringBuffer.append(TEXT_48);
+    stringBuffer.append(genLink.getUniqueIdentifier());
+    stringBuffer.append(TEXT_49);
     
 	}
 }
 
-    stringBuffer.append(TEXT_48);
+    stringBuffer.append(TEXT_50);
     importManager.emitSortedImports();
-    stringBuffer.append(TEXT_49);
+    stringBuffer.append(TEXT_51);
     return stringBuffer.toString();
   }
 }
