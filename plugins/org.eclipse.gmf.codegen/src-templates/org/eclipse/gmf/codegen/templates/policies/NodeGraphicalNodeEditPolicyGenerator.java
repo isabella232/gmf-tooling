9fc1e67fbda410094af41620fcd15d7a5362bd58
ashatalin <ashatalin>
2005-12-05 18:48:06 +0000

    #119279 Reverting link in accordance with model link.

--- plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/policies/NodeGraphicalNodeEditPolicyGenerator.java
+++ plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/policies/NodeGraphicalNodeEditPolicyGenerator.java
@@ -23,13 +23,13 @@ public class NodeGraphicalNodeEditPolicyGenerator
   protected final String TEXT_4 = " extends ";
   protected final String TEXT_5 = " {";
   protected final String TEXT_6 = NL + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected ";
-  protected final String TEXT_7 = " getConnectionCreateCommand(" + NL + "\t\t\t";
+  protected final String TEXT_7 = " getConnectionCompleteCommand(" + NL + "\t\t\t";
   protected final String TEXT_8 = " request) {" + NL + "\t\t";
   protected final String TEXT_9 = " elementType = getElementType(request);";
   protected final String TEXT_10 = NL + "\t\tif (";
   protected final String TEXT_11 = ".";
-  protected final String TEXT_12 = " == elementType) {" + NL + "\t\t\treturn getConnectionWithReorientedViewCreateCommand(request);" + NL + "\t\t}";
-  protected final String TEXT_13 = NL + "\t\treturn super.getConnectionCreateCommand(request);" + NL + "\t}";
+  protected final String TEXT_12 = " == elementType) {" + NL + "\t\t\treturn getConnectionWithReorientedViewCompleteCommand(request);" + NL + "\t\t}";
+  protected final String TEXT_13 = NL + "\t\treturn super.getConnectionCompleteCommand(request);" + NL + "\t}";
   protected final String TEXT_14 = NL + "}";
   protected final String TEXT_15 = NL;
 
@@ -55,22 +55,48 @@ GenClass nodeMetaClass = genNode.getModelFacet().getMetaClass();
 Set ids = new HashSet();
 for (Iterator links = genDiagram.getLinks().iterator(); links.hasNext(); ) {
 	GenLink genLink = (GenLink) links.next();
+/**
+ * Do not perform any rotation of links in notation model if this property was not specified in 
+ * .gmfgen model
+ **/	
 	if (!genLink.isViewDirectionAlignedWithModel()) {
 		continue;
 	}
+	GenClass incomingClass;
+	GenClass outgoingClass;
 	if (genLink.getModelFacet() instanceof TypeLinkModelFacet) {
 		TypeLinkModelFacet modelFacet = (TypeLinkModelFacet) genLink.getModelFacet();
-		GenClass outgoingClass = modelFacet.getSourceMetaFeature() == null
+		outgoingClass = modelFacet.getSourceMetaFeature() == null
 			? modelFacet.getContainmentMetaFeature().getGenClass()
 			: modelFacet.getSourceMetaFeature().getTypeGenClass();
-		if (nodeMetaClass.equals(modelFacet.getTargetMetaFeature().getTypeGenClass()) && !nodeMetaClass.equals(outgoingClass)) {
-			ids.add(genLink.getUniqueIdentifier());
-		}
+		incomingClass = modelFacet.getTargetMetaFeature().getTypeGenClass();
 	} else if (genLink.getModelFacet() instanceof FeatureModelFacet) {
 		GenFeature metaFeature = ((FeatureModelFacet) genLink.getModelFacet()).getMetaFeature();
-		if (nodeMetaClass.equals(metaFeature.getTypeGenClass()) && !nodeMetaClass.equals(metaFeature.getGenClass())) {
-			ids.add(genLink.getUniqueIdentifier());
-		}
+		outgoingClass = metaFeature.getGenClass();
+		incomingClass = metaFeature.getTypeGenClass();
+	} else {
+// Should not be here!
+		continue;
+	}
+	boolean canBeSource = outgoingClass.getEcoreClass().isSuperTypeOf(nodeMetaClass.getEcoreClass());
+	boolean canBeTarget = incomingClass.getEcoreClass().isSuperTypeOf(nodeMetaClass.getEcoreClass());
+/**
+ * This logic is currently alligned with the logic in NodeItemSemanticEditPolicy.javajet i.e.:
+ * 
+ * - we do not perform link rotation if this link could be drawn from instance of this EP 
+ *   to the instance of this EP.
+ *
+ * - if link could be created in "opposite" direction (genLink.isIncomingCreationAllowed() == true)
+ *   and this EP could be only a source of the link then we should reverse link at the end of link 
+ *   creation
+ *
+ **/	
+	if (canBeSource && canBeTarget) {
+		continue;
+	}
+	
+	if (genLink.isIncomingCreationAllowed() && canBeSource) {
+		ids.add(genLink.getUniqueIdentifier());
 	}
 }
 if (!ids.isEmpty()) {
