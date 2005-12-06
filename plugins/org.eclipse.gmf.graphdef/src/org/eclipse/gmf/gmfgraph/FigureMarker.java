0f7f710226a209cb77403f7106016ed74967a85c
atikhomirov <atikhomirov>
2005-12-06 22:12:02 +0000

    #114177 gmfgraph model to replace diadef model

---
+++ plugins/org.eclipse.gmf.graphdef/src/org/eclipse/gmf/gmfgraph/FigureMarker.java
@@ -0,0 +1,27 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.eclipse.gmf.gmfgraph;
+
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Figure Marker</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <!-- begin-model-doc -->
+ * Anything you could combine visual representation from. Ordinary GEF figures, custom-defined or references to defined elsewhere.
+ * <!-- end-model-doc -->
+ *
+ *
+ * @see org.eclipse.gmf.gmfgraph.GMFGraphPackage#getFigureMarker()
+ * @model interface="true" abstract="true"
+ * @generated
+ */
+public interface FigureMarker extends EObject {
+} // FigureMarker
