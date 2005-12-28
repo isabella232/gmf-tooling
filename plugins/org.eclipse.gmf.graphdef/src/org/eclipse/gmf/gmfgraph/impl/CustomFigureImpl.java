0ccc94b5b187a58a075ed12c4b69a4673a2e87de
atikhomirov <atikhomirov>
2005-12-28 16:10:30 +0000

    removed unused imports

--- plugins/org.eclipse.gmf.graphdef/src/org/eclipse/gmf/gmfgraph/impl/CustomFigureImpl.java
+++ plugins/org.eclipse.gmf.graphdef/src/org/eclipse/gmf/gmfgraph/impl/CustomFigureImpl.java
@@ -17,7 +17,6 @@ import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.impl.EObjectImpl;
 import org.eclipse.emf.ecore.util.EObjectContainmentEList;
-import org.eclipse.emf.ecore.util.EObjectResolvingEList;
 import org.eclipse.emf.ecore.util.InternalEList;
 import org.eclipse.gmf.gmfgraph.ColorStyle;
 import org.eclipse.gmf.gmfgraph.CustomFigure;
