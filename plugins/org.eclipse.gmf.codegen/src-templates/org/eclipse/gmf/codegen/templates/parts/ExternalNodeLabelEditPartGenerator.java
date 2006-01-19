e1caad42a8221e64edbb509eb7f00fda1c0423d2
dstadnik <dstadnik>
2006-01-19 14:06:54 +0000

    #124347 Label snap back action not working in generated code...

--- plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/parts/ExternalNodeLabelEditPartGenerator.java
+++ plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/parts/ExternalNodeLabelEditPartGenerator.java
@@ -17,12 +17,15 @@ public class ExternalNodeLabelEditPartGenerator
   protected final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
   protected final String TEXT_1 = "package ";
   protected final String TEXT_2 = ";" + NL;
-  protected final String TEXT_3 = NL + "import org.eclipse.gmf.runtime.notation.View;";
+  protected final String TEXT_3 = NL + "import org.eclipse.draw2d.geometry.Point;" + NL + "import org.eclipse.gmf.runtime.notation.View;";
   protected final String TEXT_4 = NL + NL + "/**" + NL + " * @generated" + NL + " */" + NL + "public class ";
   protected final String TEXT_5 = " extends ";
-  protected final String TEXT_6 = " {" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic ";
-  protected final String TEXT_7 = "(View view) {" + NL + "\t\tsuper(view);" + NL + "\t}" + NL + "}";
-  protected final String TEXT_8 = NL;
+  protected final String TEXT_6 = " {" + NL + "" + NL + "\tstatic {" + NL + "\t\tregisterSnapBackPosition(";
+  protected final String TEXT_7 = ".";
+  protected final String TEXT_8 = "Labels.";
+  protected final String TEXT_9 = ", new Point(0, 0));" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic ";
+  protected final String TEXT_10 = "(View view) {" + NL + "\t\tsuper(view);" + NL + "\t}" + NL + "}";
+  protected final String TEXT_11 = NL;
 
   public String generate(Object argument)
   {
@@ -42,10 +45,16 @@ GenDiagram genDiagram = genLabel.getDiagram();
     stringBuffer.append(TEXT_5);
     stringBuffer.append(genDiagram.getBaseExternalNodeLabelEditPartClassName());
     stringBuffer.append(TEXT_6);
-    stringBuffer.append(genLabel.getEditPartClassName());
+    stringBuffer.append(importManager.getImportedName(genDiagram.getSemanticHintsQualifiedClassName()));
     stringBuffer.append(TEXT_7);
-    importManager.emitSortedImports();
+    stringBuffer.append(genLabel.getNode().getUniqueIdentifier());
     stringBuffer.append(TEXT_8);
+    stringBuffer.append(genLabel.getSemanticHintLabelFieldName());
+    stringBuffer.append(TEXT_9);
+    stringBuffer.append(genLabel.getEditPartClassName());
+    stringBuffer.append(TEXT_10);
+    importManager.emitSortedImports();
+    stringBuffer.append(TEXT_11);
     return stringBuffer.toString();
   }
 }
--- plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/parts/LinkLabelEditPartGenerator.java
+++ plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/parts/LinkLabelEditPartGenerator.java
@@ -17,12 +17,15 @@ public class LinkLabelEditPartGenerator
   protected final String NL = nl == null ? (System.getProperties().getProperty("line.separator")) : nl;
   protected final String TEXT_1 = "package ";
   protected final String TEXT_2 = ";" + NL;
-  protected final String TEXT_3 = NL + "import org.eclipse.draw2d.ConnectionLocator;" + NL + "import org.eclipse.gmf.runtime.diagram.ui.editparts.LabelEditPart;" + NL + "import org.eclipse.gmf.runtime.notation.View;";
+  protected final String TEXT_3 = NL + "import org.eclipse.draw2d.ConnectionLocator;" + NL + "import org.eclipse.draw2d.geometry.Point;" + NL + "import org.eclipse.gmf.runtime.diagram.ui.editparts.LabelEditPart;" + NL + "import org.eclipse.gmf.runtime.notation.View;";
   protected final String TEXT_4 = NL + NL + "/**" + NL + " * @generated" + NL + " */" + NL + "public class ";
-  protected final String TEXT_5 = " extends LabelEditPart {" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic ";
-  protected final String TEXT_6 = "(View view) {" + NL + "\t\tsuper(view);" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic int getKeyPoint() {" + NL + "\t\treturn ConnectionLocator.";
-  protected final String TEXT_7 = ";" + NL + "\t}" + NL + "}";
-  protected final String TEXT_8 = NL;
+  protected final String TEXT_5 = " extends LabelEditPart {" + NL + "" + NL + "\tstatic {" + NL + "\t\tregisterSnapBackPosition(";
+  protected final String TEXT_6 = ".";
+  protected final String TEXT_7 = "Labels.";
+  protected final String TEXT_8 = ", new Point(0, 0));" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic ";
+  protected final String TEXT_9 = "(View view) {" + NL + "\t\tsuper(view);" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic int getKeyPoint() {" + NL + "\t\treturn ConnectionLocator.";
+  protected final String TEXT_10 = ";" + NL + "\t}" + NL + "}";
+  protected final String TEXT_11 = NL;
 
   public String generate(Object argument)
   {
@@ -40,12 +43,18 @@ GenDiagram genDiagram = genLabel.getDiagram();
     stringBuffer.append(TEXT_4);
     stringBuffer.append(genLabel.getEditPartClassName());
     stringBuffer.append(TEXT_5);
-    stringBuffer.append(genLabel.getEditPartClassName());
+    stringBuffer.append(importManager.getImportedName(genDiagram.getSemanticHintsQualifiedClassName()));
     stringBuffer.append(TEXT_6);
-    stringBuffer.append(genLabel.getAlignment());
+    stringBuffer.append(genLabel.getLink().getUniqueIdentifier());
     stringBuffer.append(TEXT_7);
-    importManager.emitSortedImports();
+    stringBuffer.append(genLabel.getSemanticHintLabelFieldName());
     stringBuffer.append(TEXT_8);
+    stringBuffer.append(genLabel.getEditPartClassName());
+    stringBuffer.append(TEXT_9);
+    stringBuffer.append(genLabel.getAlignment());
+    stringBuffer.append(TEXT_10);
+    importManager.emitSortedImports();
+    stringBuffer.append(TEXT_11);
     return stringBuffer.toString();
   }
 }
