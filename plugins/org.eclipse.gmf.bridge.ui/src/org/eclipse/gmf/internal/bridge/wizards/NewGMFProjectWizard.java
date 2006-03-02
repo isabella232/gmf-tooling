4afce8edf52cbf2a0ad13d3b85989a3e2fec9d81
dstadnik <dstadnik>
2006-03-02 15:54:00 +0000

    add page to show dashboard

--- plugins/org.eclipse.gmf.bridge.ui/src/org/eclipse/gmf/internal/bridge/wizards/NewGMFProjectWizard.java
+++ plugins/org.eclipse.gmf.bridge.ui/src/org/eclipse/gmf/internal/bridge/wizards/NewGMFProjectWizard.java
@@ -12,14 +12,46 @@
 package org.eclipse.gmf.internal.bridge.wizards;
 
 import org.eclipse.emf.codegen.ecore.ui.EmptyProjectWizard;
+import org.eclipse.gmf.internal.bridge.ui.Plugin;
+import org.eclipse.gmf.internal.bridge.wizards.pages.ShowDashboardPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 
 /**
- * Quick way to introduce GMF Project wizard. 
- * XXX Get rid of EmptyProjectWizard and ecore.ui dependency once you know what could be added here
+ * Quick way to introduce GMF Project wizard.
+ * XXX Get rid of EmptyProjectWizard and ecore.ui dependency once you know what could be added here.
+ * 
  * @author artem
  */
 public class NewGMFProjectWizard extends EmptyProjectWizard {
-	//private JavaCapabilityConfigurationPage myFirstPage;
 
+	private boolean showDashboard;
 
+	private ShowDashboardPage sdp;
+
+	public void addPages() {
+		super.addPages();
+		sdp = new ShowDashboardPage("Show Dashboard", showDashboard); //$NON-NLS-1$
+		sdp.setTitle("Show Dashboard");
+		sdp.setDescription("Show dashboard view for the created project");
+		addPage(sdp);
+	}
+
+	public void setShowDashboard(boolean showDashboard) {
+		this.showDashboard = showDashboard;
+	}
+
+	public boolean performFinish() {
+		boolean created = super.performFinish();
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		if (created && sdp.isShowDashboard() && window != null && window.getActivePage() != null) {
+			try {
+				window.getActivePage().showView("org.eclipse.gmf.bridge.ui.dashboard.DashboardPart");
+			} catch (PartInitException e) {
+				Plugin.log(e);
+			}
+		}
+		return created;
+	}
 }
