401f0599abd7fc1614563e9e2e6cbe3acc756587
atikhomirov <atikhomirov>
2006-03-10 15:34:42 +0000

    use correct message bundle

--- plugins/org.eclipse.gmf.common/src/org/eclipse/gmf/common/codegen/GeneratorBaseMessages.java
+++ plugins/org.eclipse.gmf.common/src/org/eclipse/gmf/common/codegen/GeneratorBaseMessages.java
@@ -3,7 +3,7 @@ package org.eclipse.gmf.common.codegen;
 import org.eclipse.osgi.util.NLS;
 
 public class GeneratorBaseMessages extends NLS {
-	private static final String BUNDLE_NAME = "org.eclipse.gmf.codegen.util.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "org.eclipse.gmf.common.codegen.messages"; //$NON-NLS-1$
 
 	private GeneratorBaseMessages() {
 	}
@@ -12,10 +12,6 @@ public class GeneratorBaseMessages extends NLS {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, GeneratorBaseMessages.class);
 	}
-
-//	public static String start;
-//
-//	public static String initproject;
 
 	public static String merge;
 
