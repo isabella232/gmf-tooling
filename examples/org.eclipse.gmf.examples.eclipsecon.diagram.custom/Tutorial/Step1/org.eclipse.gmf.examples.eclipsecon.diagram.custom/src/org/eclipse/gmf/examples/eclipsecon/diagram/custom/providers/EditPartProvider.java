4fb3df1f687da6d20783ba383c95dc9e630a8df8
sshaw <sshaw>
2006-03-15 02:29:22 +0000

    Adding Tutorial

---
+++ examples/org.eclipse.gmf.examples.eclipsecon.diagram.custom/Tutorial/Step1/org.eclipse.gmf.examples.eclipsecon.diagram.custom/src/org/eclipse/gmf/examples/eclipsecon/diagram/custom/providers/EditPartProvider.java
@@ -0,0 +1,26 @@
+package org.eclipse.gmf.examples.eclipsecon.diagram.custom.providers;
+
+
+import org.eclipse.gmf.examples.eclipsecon.Presenter;
+import org.eclipse.gmf.examples.eclipsecon.diagram.custom.editparts.BetterLookingPresenterEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.services.editpart.AbstractEditPartProvider;
+import org.eclipse.gmf.runtime.notation.View;
+
+
+public class EditPartProvider extends AbstractEditPartProvider {
+
+	/* 
+	 * (non-Javadoc)
+	 * @see org.eclipse.gmf.runtime.diagram.ui.services.editpart.AbstractEditPartProvider#getNodeEditPartClass(org.eclipse.gmf.runtime.notation.View)
+	 */
+	protected Class getNodeEditPartClass(View view) {
+        
+		if (view.getElement() instanceof Presenter &&
+            view.getType() == "" ) { //$NON-NLS-1$
+            return BetterLookingPresenterEditPart.class;
+        }
+        
+		return super.getNodeEditPartClass(view);
+	}
+
+}
