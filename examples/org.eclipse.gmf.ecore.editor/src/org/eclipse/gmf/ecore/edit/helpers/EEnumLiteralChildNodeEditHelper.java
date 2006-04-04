345fbd222ee2b3b528b6f4dd947ded8239953650
ashatalin <ashatalin>
2006-04-04 11:02:12 +0000

    Updating sample with the latest templates.

---
+++ examples/org.eclipse.gmf.ecore.editor/src/org/eclipse/gmf/ecore/edit/helpers/EEnumLiteralChildNodeEditHelper.java
@@ -0,0 +1,9 @@
+package org.eclipse.gmf.ecore.edit.helpers;
+
+import org.eclipse.gmf.runtime.emf.type.core.edithelper.AbstractEditHelper;
+
+/**
+ * @generated
+ */
+public class EEnumLiteralChildNodeEditHelper extends AbstractEditHelper {
+}
