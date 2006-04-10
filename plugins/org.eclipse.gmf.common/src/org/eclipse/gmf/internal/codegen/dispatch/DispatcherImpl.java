2965ccd72595dc3991499815279105b4542683a0
atikhomirov <atikhomirov>
2006-04-10 13:08:52 +0000

    #135020 Provide option to trace missing templates

--- plugins/org.eclipse.gmf.common/src/org/eclipse/gmf/internal/codegen/dispatch/DispatcherImpl.java
+++ plugins/org.eclipse.gmf.common/src/org/eclipse/gmf/internal/codegen/dispatch/DispatcherImpl.java
@@ -12,6 +12,7 @@
 package org.eclipse.gmf.internal.codegen.dispatch;
 
 import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
@@ -25,19 +26,23 @@ import org.eclipse.gmf.common.UnexpectedBehaviourException;
  * @author artem
  */
 public class DispatcherImpl implements Dispatcher {
-
 	private final EmitterFactory myFactory;
 
 	private final KeyMap myKeyMap;
+	
+	protected final TraceFacility traceFacility;
+	private final boolean shouldTraceMissedTemplates;
 
 	public DispatcherImpl(EmitterFactory factory, KeyMap keyMap) {
 		myFactory = factory;
 		myKeyMap = keyMap;
+		final String pluginID = "org.eclipse.gmf.common";
+		traceFacility = new TraceFacility(pluginID);
+		final String option = pluginID + "/templates/missed/log";
+		shouldTraceMissedTemplates = Boolean.valueOf(Platform.getDebugOption(option)).booleanValue();
 	}
 
 	public String dispatch(Object key, Object argument) {
-		final String pluginID = "org.eclipse.gmf.common";
-		final ILog traceFacility = Platform.getLog(Platform.getBundle(pluginID));
 		try {
 			StringBuffer errors = new StringBuffer();
 			for (KeyChain it = myKeyMap.map(key); it.hasNext();) {
@@ -47,29 +52,57 @@ public class DispatcherImpl implements Dispatcher {
 						JETEmitter em = myFactory.acquireEmitter(nextKey);
 						return em.generate(new NullProgressMonitor(), new Object[] {argument});
 					} catch (NoSuchTemplateException ex) {
-						traceFacility.log(new Status(Status.ERROR, pluginID, 0, "Template for key '" + nextKey + "' failed", ex));
+						traceFacility.error("Template for key '" + nextKey + "' failed", ex);
 						errors.append(formatError(ex));
 						errors.append("\n");
 					} catch (UnexpectedBehaviourException ex) {
-						traceFacility.log(new Status(Status.ERROR, pluginID, 0, "Template for key '" + nextKey + "' failed", ex));
+						traceFacility.error("Template for key '" + nextKey + "' failed", ex);
 						errors.append(formatError(ex));
 						errors.append("\n");
 					}
 				}
 			}
 			if (errors.length() == 0) {
-				// TODO option silent?
-				return "// no suitable template found for '" + key + "'";
+				if (shouldTraceMissedTemplates) {
+					final String msg = "No suitable template found for '" + key + "'";
+					traceFacility.warn(msg, null);
+					return "// " + msg;
+				} else {
+					return "";
+				}
 			} else {
 				return errors.toString();
 			}
 		} catch (JETException ex) {
-			traceFacility.log(new Status(Status.ERROR, pluginID, 0, ex.getMessage(), ex));
+			traceFacility.error(ex.getMessage(), ex);
 			return "ERROR: " + formatError(ex);
 		}
 	}
 
 	private static String formatError(Exception ex) {
 		return ex.getClass().getName() + ": " + ex.getMessage();
+	}
+
+	private static class TraceFacility {
+		private final ILog traceFacility;
+		public TraceFacility(String pluginID) {
+			traceFacility = Platform.getLog(Platform.getBundle(pluginID));
+		}
+
+		public void error(String message, Exception ex) {
+			log(IStatus.ERROR, message, ex);
+		}
+
+		public void warn(String message, Exception ex) {
+			log(IStatus.WARNING, message, ex);
+		}
+
+		public String getPluginID() {
+			return traceFacility.getBundle().getSymbolicName();
+		}
+
+		private void log(int severity, String message, Exception ex) {
+			traceFacility.log(new Status(severity, getPluginID(), 0, message, ex));
+		}
 	}
 }
