e36b7c86b0321bc93a130180c79885a515378290
atikhomirov <atikhomirov>
2006-04-20 17:52:45 +0000

    [bblajer] #137225 Specify whether to generate gmf.runtime-based or pure draw2d figures

--- plugins/org.eclipse.gmf.graphdef.codegen.ui/src/org/eclipse/gmf/internal/graphdef/codegen/ui/GeneratePluginAction.java
+++ plugins/org.eclipse.gmf.graphdef.codegen.ui/src/org/eclipse/gmf/internal/graphdef/codegen/ui/GeneratePluginAction.java
@@ -26,17 +26,29 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.gmf.gmfgraph.FigureGallery;
+import org.eclipse.gmf.gmfgraph.util.FigureQualifiedNameSwitch;
 import org.eclipse.gmf.gmfgraph.util.RuntimeFQNSwitch;
+import org.eclipse.gmf.gmfgraph.util.RuntimeLiteFQNSwitch;
 import org.eclipse.gmf.graphdef.codegen.StandaloneGenerator;
 import org.eclipse.gmf.internal.graphdef.codegen.StandaloneGalleryConverter;
 import org.eclipse.jdt.core.JavaConventions;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IInputValidator;
-import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 
@@ -59,13 +71,20 @@ public class GeneratePluginAction implements IObjectActionDelegate, IInputValida
 			MessageDialog.openInformation(getShell(), "Nothing to do", "No figure galleries found in the selected files, nothing to do");
 			return;
 		}
-		InputDialog readInputDlg = new InputDialog(getShell(), "Specify plug-in id", "Please enter name of plug-in/main package", null, this);
-		if (readInputDlg.open() != InputDialog.OK) {
+		StandaloneGeneratorOptionsDialog dialog = new StandaloneGeneratorOptionsDialog(getShell(), null, true, this);
+		if (dialog.open() != IDialogConstants.OK_ID) {
 			return;
 		}
+		String pluginId = dialog.getPluginId();
 		FigureGallery[] input = (FigureGallery[]) galleries.toArray(new FigureGallery[galleries.size()]);
-		StandaloneGenerator.Config config = new StandaloneGenerator.ConfigImpl(readInputDlg.getValue(), readInputDlg.getValue(), false);
-		final StandaloneGenerator generator = new StandaloneGenerator(input, config, new RuntimeFQNSwitch());
+		StandaloneGenerator.Config config = new StandaloneGenerator.ConfigImpl(pluginId, pluginId, false);
+		FigureQualifiedNameSwitch fqnSwitch;
+		if (dialog.isUseRuntimeFigures()) {
+			fqnSwitch = new RuntimeFQNSwitch();
+		} else {
+			fqnSwitch = new RuntimeLiteFQNSwitch();
+		}
+		final StandaloneGenerator generator = new StandaloneGenerator(input, config, fqnSwitch);
 		generator.setSkipPluginStructure(false);
 
 		new Job(action.getText()) {
@@ -175,5 +194,92 @@ public class GeneratePluginAction implements IObjectActionDelegate, IInputValida
 		if (selection instanceof IStructuredSelection) {
 			mySelectedFiles.addAll(((IStructuredSelection) selection).toList());
 		}
+	}
+
+	private static class StandaloneGeneratorOptionsDialog extends Dialog {
+	    private IInputValidator pluginIdValidator;
+	    private Text pluginIdText;
+	    private Text errorMessageText;
+	    private Button useRuntimeFiguresButton;
+
+		private String pluginId;
+		private boolean useRuntimeFigures;
+
+		public StandaloneGeneratorOptionsDialog(Shell parentShell, String initialPluginId, boolean initialUseRuntimeFigures, IInputValidator pluginIdValidator) {
+			super(parentShell);
+			pluginId = initialPluginId;
+			if (pluginId == null) {
+				pluginId = "";	//$NON-NLS-1$
+			}
+			this.pluginIdValidator = pluginIdValidator;
+			useRuntimeFigures = initialUseRuntimeFigures;
+		}
+
+		protected void configureShell(Shell newShell) {
+			super.configureShell(newShell);
+			newShell.setText("Figure Gallery Generator");
+		}
+
+		protected Control createDialogArea(Composite parent) {
+			Composite result = (Composite) super.createDialogArea(parent);
+	        Label label = new Label(result, SWT.WRAP);
+	        label.setText("Please specify the name of plug-in/main package");
+	        GridData data = new GridData(GridData.GRAB_HORIZONTAL
+	                | GridData.GRAB_VERTICAL | GridData.HORIZONTAL_ALIGN_FILL
+	                | GridData.VERTICAL_ALIGN_CENTER);
+	        data.widthHint = convertHorizontalDLUsToPixels(IDialogConstants.MINIMUM_MESSAGE_AREA_WIDTH);
+	        label.setLayoutData(data);
+	        label.setFont(parent.getFont());
+	        pluginIdText = new Text(result, SWT.SINGLE | SWT.BORDER);
+	        pluginIdText.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL
+	                | GridData.HORIZONTAL_ALIGN_FILL));
+	        pluginIdText.addModifyListener(new ModifyListener() {
+	            public void modifyText(ModifyEvent e) {
+	                validateInput();
+	            }
+	        });
+	        useRuntimeFiguresButton = new Button(result, SWT.CHECK);
+	        useRuntimeFiguresButton.setText("Utilize enhanced features of GMF runtime");
+	        useRuntimeFiguresButton.setSelection(useRuntimeFigures);
+	        errorMessageText = new Text(result, SWT.READ_ONLY);
+	        errorMessageText.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL
+	                | GridData.HORIZONTAL_ALIGN_FILL));
+	        errorMessageText.setBackground(errorMessageText.getDisplay()
+	                .getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
+			return result;
+		}
+
+	    protected void validateInput() {
+	        String errorMessage = null;
+	        if (pluginIdValidator != null) {
+	            errorMessage = pluginIdValidator.isValid(pluginIdText.getText());
+	        }
+	        // Bug 16256: important not to treat "" (blank error) the same as null
+	        // (no error)
+	        setErrorMessage(errorMessage);
+	    }
+
+	    public void setErrorMessage(String errorMessage) {
+	        errorMessageText.setText(errorMessage == null ? "" : errorMessage); //$NON-NLS-1$
+	        errorMessageText.getParent().update();
+	        Control button = getButton(IDialogConstants.OK_ID);
+	        if (button != null) {
+				button.setEnabled(errorMessage == null);
+			}
+	    }
+
+	    protected void okPressed() {
+	    	useRuntimeFigures = useRuntimeFiguresButton.getSelection();
+	    	pluginId = pluginIdText.getText();
+	    	super.okPressed();
+	    }
+
+	    public String getPluginId() {
+	    	return pluginId;
+	    }
+
+	    public boolean isUseRuntimeFigures() {
+	    	return useRuntimeFigures;
+	    }
 	}
 }
