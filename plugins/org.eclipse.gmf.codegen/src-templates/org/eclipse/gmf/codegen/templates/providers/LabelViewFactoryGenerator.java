a502f3c0f7adb355993be087de20bf96d260c859
dstadnik <dstadnik>
2006-06-23 09:25:37 +0000

    set labels offset in view factories

--- plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/providers/LabelViewFactoryGenerator.java
+++ plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/providers/LabelViewFactoryGenerator.java
@@ -23,13 +23,16 @@ public class LabelViewFactoryGenerator
   protected final String TEXT_6 = " extends AbstractLabelViewFactory {" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic View createView(IAdaptable semanticAdapter, View containerView, String semanticHint, int index, boolean persisted, PreferencesHint preferencesHint) {" + NL + "\t\tNode view = (Node) super.createView(semanticAdapter, containerView, semanticHint, index, persisted, preferencesHint);";
   protected final String TEXT_7 = NL + "\t\t";
   protected final String TEXT_8 = " location = (";
-  protected final String TEXT_9 = ") view.getLayoutConstraint();" + NL + "\t\tlocation.setY(";
-  protected final String TEXT_10 = ".getMapMode(containerView.getDiagram().getMeasurementUnit()).DPtoLP(5));";
-  protected final String TEXT_11 = NL + "\t\treturn view;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected List createStyles(View view) {" + NL + "\t\tList styles = new ArrayList();";
-  protected final String TEXT_12 = NL + "\t\tstyles.add(";
-  protected final String TEXT_13 = ".eINSTANCE.createDescriptionStyle());";
-  protected final String TEXT_14 = NL + "\t\treturn styles;" + NL + "\t}" + NL + "}";
-  protected final String TEXT_15 = NL;
+  protected final String TEXT_9 = ") view.getLayoutConstraint();" + NL + "\t\t";
+  protected final String TEXT_10 = " mapMode = ";
+  protected final String TEXT_11 = ".getMapMode(containerView.getDiagram().getMeasurementUnit());" + NL + "\t\tlocation.setX(mapMode.DPtoLP(";
+  protected final String TEXT_12 = "));" + NL + "\t\tlocation.setY(mapMode.DPtoLP(";
+  protected final String TEXT_13 = "));";
+  protected final String TEXT_14 = NL + "\t\treturn view;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected List createStyles(View view) {" + NL + "\t\tList styles = new ArrayList();";
+  protected final String TEXT_15 = NL + "\t\tstyles.add(";
+  protected final String TEXT_16 = ".eINSTANCE.createDescriptionStyle());";
+  protected final String TEXT_17 = NL + "\t\treturn styles;" + NL + "\t}" + NL + "}";
+  protected final String TEXT_18 = NL;
 
   public String generate(Object argument)
   {
@@ -54,24 +57,41 @@ if (copyrightText != null && copyrightText.trim().length() > 0) {
     stringBuffer.append(TEXT_5);
     stringBuffer.append(genLabel.getNotationViewFactoryClassName());
     stringBuffer.append(TEXT_6);
-    if (genLabel instanceof GenExternalNodeLabel) {
+    
+int x = 0;
+int y = 0;
+LabelOffsetAttributes labelOffset = (LabelOffsetAttributes) genLabel.getViewmap().find(LabelOffsetAttributes.class);
+if (labelOffset != null) {
+	x = labelOffset.getX();
+	y = labelOffset.getY();
+} else if (genLabel instanceof GenExternalNodeLabel) {
+	y = 5;
+}
+if ((x != 0 || y != 0) && (genLabel instanceof GenLinkLabel || genLabel instanceof GenExternalNodeLabel)) {
+
     stringBuffer.append(TEXT_7);
     stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.notation.Location"));
     stringBuffer.append(TEXT_8);
     stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.notation.Location"));
     stringBuffer.append(TEXT_9);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.diagram.ui.util.MeasurementUnitHelper"));
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.draw2d.ui.mapmode.IMapMode"));
     stringBuffer.append(TEXT_10);
-    }
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.diagram.ui.util.MeasurementUnitHelper"));
     stringBuffer.append(TEXT_11);
-    if (genLabel.getModelFacet() instanceof DesignLabelModelFacet) {
+    stringBuffer.append(x);
     stringBuffer.append(TEXT_12);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.notation.NotationFactory"));
+    stringBuffer.append(y);
     stringBuffer.append(TEXT_13);
     }
     stringBuffer.append(TEXT_14);
-    importManager.emitSortedImports();
+    if (genLabel.getModelFacet() instanceof DesignLabelModelFacet) {
     stringBuffer.append(TEXT_15);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.notation.NotationFactory"));
+    stringBuffer.append(TEXT_16);
+    }
+    stringBuffer.append(TEXT_17);
+    importManager.emitSortedImports();
+    stringBuffer.append(TEXT_18);
     return stringBuffer.toString();
   }
 }
