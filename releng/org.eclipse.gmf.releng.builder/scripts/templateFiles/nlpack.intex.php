b81feb5c88cd56d21db9c2cc13fa14f03d24e563
mfeldman <mfeldman>
2006-07-21 15:01:20 +0000

    index.php template for the LanguagePacks

---
+++ releng/org.eclipse.gmf.releng.builder/scripts/templateFiles/nlpack.intex.php
@@ -0,0 +1,43 @@
+<!doctype html public "-//w3c//dtd html 4.0 transitional//en">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" href="../../../default_style.css" type="text/css">
+<title>GMF Language Pack</title>
+</head>
+<body>
+&nbsp;  <table BORDER=0 CELLSPACING=5 CELLPADDING=2 WIDTH="100%" > <tr> <td ALIGN=LEFT width="80%"> 
+<p><b><font face="Verdana" size="+3">Language Packs: 3.2.x</font></b><br>
+   July 21, 2006. These downloads are provided under the <a href="http://www.eclipse.org/legal/notice.html">Eclipse.org
+
+Software User Agreement</a>.</p></td></tr> </table><table BORDER=0 CELLSPACING=5 CELLPADDING=2 WIDTH="100%" > 
+
+<tr> <td ALIGN=LEFT VALIGN=TOP COLSPAN="3" BGCOLOR="#0080C0"><b><font color="#FFFFFF" face="Arial,Helvetica">GMF 
+Language Pack Downloads</font></b></td></tr> <tr> <td height="48" colspan="2"> <p>IBM has 
+donated translations from their products to Eclipse. The language 
+packs group translations for several languages into a single download which are distributed as a set of features which you can install by extracting over your Eclipse directory and restarting Eclipse.  Specifically,</p>
+<ul><li>NLpack1 - German, Spanish, French, Italian, Japanese, Korean, Portuguese (Brazil), Traditional Chinese, Simplified Chinese</li>
+<li>NLpack2 - Czech, Hungarian, Polish, Russian</li>
+<li>NLpack2a - Danish, Dutch, Finnish, Greek, Norwegian, Portuguese, Swedish and Turkish</li>
+<li>NLpackBidi - Arabic</li>
+</ul>
+ <p> These translations are based on GMF 1.0.0. The NLS translation fragment packs should work with all subsequent 1.0 maintenance releases, 
+with any new strings remaining untranslated. </p>
+</td>
+</tr>
+<tr> 
+<td>
+<ul><li><a href="NLpack1-GMF-runtime-1.0.zip">GMF Runtime NLpack1</a>
+<li><a href="NLpack2-GMF-runtime-1.0.zip">GMF Runtime NLpack2</a>
+<li><a href="NLpack2a-GMF-runtime-1.0.zip">GMF Runtime NLpack2a</a>
+<li><a href="NLpackBidi-GMF-runtime-1.0.zip">GMF Runtime NLpackBidi</a>
+<li><a href="NLpack1-GMF-sdk-1.0.zip">GMF SDK NLpack1</a>
+<li><a href="NLpack2-GMF-sdk-1.0.zip">GMF SDK NLpack2</a>
+<li><a href="NLpack2a-GMF-sdk-1.0.zip">GMF SDK NLpack2a</a>
+<li><a href="NLpackBidi-GMF-sdk-1.0.zip">GMF SDK NLpackBidi</a>
+</ul>
+</td>
+</tr>
+</table>
+<P>&nbsp;</P>
+
