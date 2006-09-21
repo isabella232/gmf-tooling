d70a319ba2150fe6e58842d9a7c0abf87e2226bf
atikhomirov <atikhomirov>
2006-09-21 12:14:21 +0000

    handle readOnly attribute of the property sheet

--- plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/EditorGenerator.java
+++ plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/EditorGenerator.java
@@ -20,24 +20,26 @@ public class EditorGenerator
   protected final String TEXT_3 = NL + " */";
   protected final String TEXT_4 = NL + NL + "import org.eclipse.draw2d.DelegatingLayout;" + NL + "import org.eclipse.draw2d.FreeformLayer;" + NL + "import org.eclipse.draw2d.LayeredPane;" + NL + "import org.eclipse.gef.LayerConstants;" + NL + "import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramRootEditPart;" + NL + "import org.eclipse.gmf.runtime.diagram.ui.resources.editor.ide.editor.FileDiagramEditor;" + NL + "import org.eclipse.ui.IEditorInput;" + NL + "import org.eclipse.ui.ide.IGotoMarker;" + NL + "import org.eclipse.emf.transaction.TransactionalEditingDomain;";
   protected final String TEXT_5 = NL + NL + "/**" + NL + " * @generated" + NL + " */" + NL + "public class ";
-  protected final String TEXT_6 = " extends FileDiagramEditor implements IGotoMarker {" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic static final String ID = \"";
-  protected final String TEXT_7 = "\"; //$NON-NLS-1$" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic ";
-  protected final String TEXT_8 = "() {" + NL + "\t\tsuper(";
-  protected final String TEXT_9 = ");" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected String getEditingDomainID() {" + NL + "\t\treturn \"";
-  protected final String TEXT_10 = "\"; //$NON-NLS-1$" + NL + "\t}" + NL + "\t" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected TransactionalEditingDomain createEditingDomain() {" + NL + "\t\tTransactionalEditingDomain domain = super.createEditingDomain();" + NL + "\t\tdomain.setID(getEditingDomainID());" + NL + "\t\treturn domain;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected void setDocumentProvider(IEditorInput input) {" + NL + "\t\tif (input.getAdapter(";
-  protected final String TEXT_11 = ".class) != null) {" + NL + "\t\t\tsetDocumentProvider(new ";
-  protected final String TEXT_12 = "());" + NL + "\t\t} else {" + NL + "\t\t\tsetDocumentProvider(new ";
-  protected final String TEXT_13 = "());" + NL + "\t\t}" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected void configureGraphicalViewer() {" + NL + "\t\tsuper.configureGraphicalViewer();" + NL + "\t\tDiagramRootEditPart root = (DiagramRootEditPart) getDiagramGraphicalViewer().getRootEditPart();" + NL + "\t\tLayeredPane printableLayers = (LayeredPane) root.getLayer(LayerConstants.PRINTABLE_LAYERS);" + NL + "\t\tFreeformLayer extLabelsLayer = new FreeformLayer();" + NL + "\t\textLabelsLayer.setLayoutManager(new DelegatingLayout());" + NL + "\t\tprintableLayers.addLayerAfter(extLabelsLayer, ";
-  protected final String TEXT_14 = ".EXTERNAL_NODE_LABELS_LAYER, LayerConstants.PRIMARY_LAYER);" + NL + "\t\tLayeredPane scalableLayers = (LayeredPane) root.getLayer(LayerConstants.SCALABLE_LAYERS);" + NL + "\t\tFreeformLayer scaledFeedbackLayer = new FreeformLayer();" + NL + "\t\tscaledFeedbackLayer.setEnabled(false);" + NL + "\t\tscalableLayers.addLayerAfter(scaledFeedbackLayer, LayerConstants.SCALED_FEEDBACK_LAYER, DiagramRootEditPart.DECORATION_UNPRINTABLE_LAYER);" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected ";
-  protected final String TEXT_15 = " getPreferencesHint() {";
-  protected final String TEXT_16 = NL + "\t\treturn ";
-  protected final String TEXT_17 = ".DIAGRAM_PREFERENCES_HINT;" + NL + "\t}";
-  protected final String TEXT_18 = NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic String getContributorId() {" + NL + "\t\treturn ";
-  protected final String TEXT_19 = ".ID;" + NL + "\t}";
-  protected final String TEXT_20 = NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic Object getAdapter(Class type) {" + NL + "\t\tif (type == ";
-  protected final String TEXT_21 = ".class) {" + NL + "\t\t\treturn null;" + NL + "\t\t}" + NL + "\t\treturn super.getAdapter(type);" + NL + "\t}";
-  protected final String TEXT_22 = NL + "}";
-  protected final String TEXT_23 = NL;
+  protected final String TEXT_6 = " extends FileDiagramEditor implements IGotoMarker";
+  protected final String TEXT_7 = ", ";
+  protected final String TEXT_8 = " {" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic static final String ID = \"";
+  protected final String TEXT_9 = "\"; //$NON-NLS-1$" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic ";
+  protected final String TEXT_10 = "() {" + NL + "\t\tsuper(";
+  protected final String TEXT_11 = ");" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected String getEditingDomainID() {" + NL + "\t\treturn \"";
+  protected final String TEXT_12 = "\"; //$NON-NLS-1$" + NL + "\t}" + NL + "\t" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected TransactionalEditingDomain createEditingDomain() {" + NL + "\t\tTransactionalEditingDomain domain = super.createEditingDomain();" + NL + "\t\tdomain.setID(getEditingDomainID());" + NL + "\t\treturn domain;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected void setDocumentProvider(IEditorInput input) {" + NL + "\t\tif (input.getAdapter(";
+  protected final String TEXT_13 = ".class) != null) {" + NL + "\t\t\tsetDocumentProvider(new ";
+  protected final String TEXT_14 = "());" + NL + "\t\t} else {" + NL + "\t\t\tsetDocumentProvider(new ";
+  protected final String TEXT_15 = "());" + NL + "\t\t}" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected void configureGraphicalViewer() {" + NL + "\t\tsuper.configureGraphicalViewer();" + NL + "\t\tDiagramRootEditPart root = (DiagramRootEditPart) getDiagramGraphicalViewer().getRootEditPart();" + NL + "\t\tLayeredPane printableLayers = (LayeredPane) root.getLayer(LayerConstants.PRINTABLE_LAYERS);" + NL + "\t\tFreeformLayer extLabelsLayer = new FreeformLayer();" + NL + "\t\textLabelsLayer.setLayoutManager(new DelegatingLayout());" + NL + "\t\tprintableLayers.addLayerAfter(extLabelsLayer, ";
+  protected final String TEXT_16 = ".EXTERNAL_NODE_LABELS_LAYER, LayerConstants.PRIMARY_LAYER);" + NL + "\t\tLayeredPane scalableLayers = (LayeredPane) root.getLayer(LayerConstants.SCALABLE_LAYERS);" + NL + "\t\tFreeformLayer scaledFeedbackLayer = new FreeformLayer();" + NL + "\t\tscaledFeedbackLayer.setEnabled(false);" + NL + "\t\tscalableLayers.addLayerAfter(scaledFeedbackLayer, LayerConstants.SCALED_FEEDBACK_LAYER, DiagramRootEditPart.DECORATION_UNPRINTABLE_LAYER);" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected ";
+  protected final String TEXT_17 = " getPreferencesHint() {";
+  protected final String TEXT_18 = NL + "\t\treturn ";
+  protected final String TEXT_19 = ".DIAGRAM_PREFERENCES_HINT;" + NL + "\t}";
+  protected final String TEXT_20 = NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic String getContributorId() {" + NL + "\t\treturn ";
+  protected final String TEXT_21 = ".ID;" + NL + "\t}";
+  protected final String TEXT_22 = NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic Object getAdapter(Class type) {" + NL + "\t\tif (type == ";
+  protected final String TEXT_23 = ".class) {" + NL + "\t\t\treturn null;" + NL + "\t\t}" + NL + "\t\treturn super.getAdapter(type);" + NL + "\t}";
+  protected final String TEXT_24 = NL + "}";
+  protected final String TEXT_25 = NL;
 
   public String generate(Object argument)
   {
@@ -62,40 +64,45 @@ if (copyrightText != null && copyrightText.trim().length() > 0) {
     stringBuffer.append(TEXT_5);
     stringBuffer.append(genEditor.getClassName());
     stringBuffer.append(TEXT_6);
-    stringBuffer.append(genEditor.getID());
+    if (genDiagram.getEditorGen().getPropertySheet().isReadOnly()) {
     stringBuffer.append(TEXT_7);
-    stringBuffer.append(genEditor.getClassName());
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.diagram.ui.properties.views.IReadOnlyDiagramPropertySheetPageContributor"));
+    }
     stringBuffer.append(TEXT_8);
-    stringBuffer.append(genDiagram.getPalette() != null && genDiagram.getPalette().isFlyout());
+    stringBuffer.append(genEditor.getID());
     stringBuffer.append(TEXT_9);
-    stringBuffer.append(genDiagram.getEditingDomainID());
+    stringBuffer.append(genEditor.getClassName());
     stringBuffer.append(TEXT_10);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.core.resources.IFile"));
+    stringBuffer.append(genDiagram.getPalette() != null && genDiagram.getPalette().isFlyout());
     stringBuffer.append(TEXT_11);
-    stringBuffer.append(genDiagram.getDocumentProviderClassName());
+    stringBuffer.append(genDiagram.getEditingDomainID());
     stringBuffer.append(TEXT_12);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.diagram.ui.resources.editor.ide.document.StorageDiagramDocumentProvider"));
+    stringBuffer.append(importManager.getImportedName("org.eclipse.core.resources.IFile"));
     stringBuffer.append(TEXT_13);
-    stringBuffer.append(importManager.getImportedName(genDiagram.getEditPartFactoryQualifiedClassName()));
+    stringBuffer.append(genDiagram.getDocumentProviderClassName());
     stringBuffer.append(TEXT_14);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.diagram.core.preferences.PreferencesHint"));
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.diagram.ui.resources.editor.ide.document.StorageDiagramDocumentProvider"));
     stringBuffer.append(TEXT_15);
-    /*XXX seems better we use preference store directly (in configureGraphicalViewer) instead all these indirect ids */
+    stringBuffer.append(importManager.getImportedName(genDiagram.getEditPartFactoryQualifiedClassName()));
     stringBuffer.append(TEXT_16);
-    stringBuffer.append(genDiagram.getEditorGen().getPlugin().getActivatorClassName());
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.diagram.core.preferences.PreferencesHint"));
     stringBuffer.append(TEXT_17);
-    if (genDiagram.getEditorGen().getPropertySheet() != null) { /*perhaps, we should override contributor id regardless of sheet presence, there's no much sense in default id either.*/
+    /*XXX seems better we use preference store directly (in configureGraphicalViewer) instead all these indirect ids */
     stringBuffer.append(TEXT_18);
     stringBuffer.append(genDiagram.getEditorGen().getPlugin().getActivatorClassName());
     stringBuffer.append(TEXT_19);
-    } else {
+    if (genDiagram.getEditorGen().getPropertySheet() != null) { /*perhaps, we should override contributor id regardless of sheet presence, there's no much sense in default id either.*/
     stringBuffer.append(TEXT_20);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.ui.views.properties.IPropertySheetPage"));
+    stringBuffer.append(genDiagram.getEditorGen().getPlugin().getActivatorClassName());
     stringBuffer.append(TEXT_21);
-    }
+    } else {
     stringBuffer.append(TEXT_22);
-    importManager.emitSortedImports();
+    stringBuffer.append(importManager.getImportedName("org.eclipse.ui.views.properties.IPropertySheetPage"));
     stringBuffer.append(TEXT_23);
+    }
+    stringBuffer.append(TEXT_24);
+    importManager.emitSortedImports();
+    stringBuffer.append(TEXT_25);
     return stringBuffer.toString();
   }
 }
--- plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/PropertySectionGenerator.java
+++ plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/PropertySectionGenerator.java
@@ -23,17 +23,27 @@ public class PropertySectionGenerator
   protected final String TEXT_6 = " extends ";
   protected final String TEXT_7 = " implements ";
   protected final String TEXT_8 = " {" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic ";
-  protected final String TEXT_9 = " getPropertySource(Object object) {" + NL + "\t\tif (object instanceof IPropertySource) {" + NL + "\t\t\treturn (IPropertySource) object;" + NL + "\t\t}" + NL + "\t\tAdapterFactory af = getAdapterFactory(object);" + NL + "\t\tif (af != null) {" + NL + "\t\t\tIItemPropertySource ips = (IItemPropertySource) af.adapt(object, IItemPropertySource.class);" + NL + "\t\t\tif (ips != null) {" + NL + "\t\t\t\treturn new PropertySource(object, ips);" + NL + "\t\t\t}" + NL + "\t\t}" + NL + "\t\tif (object instanceof IAdaptable) {" + NL + "\t\t\treturn (IPropertySource) ((IAdaptable) object).getAdapter(IPropertySource.class);" + NL + "\t\t}" + NL + "\t\treturn null;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * Modify/unwrap selection.  " + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected Object transformSelection(Object selected) {";
-  protected final String TEXT_10 = NL + "\t\tif (selected instanceof ";
-  protected final String TEXT_11 = ") {" + NL + "\t\t\tObject model = ((";
-  protected final String TEXT_12 = ") selected).getModel();" + NL + "\t\t\treturn model instanceof ";
-  protected final String TEXT_13 = " ? ((";
-  protected final String TEXT_14 = ") model).getElement() : null;" + NL + "\t\t}" + NL + "\t\tif (selected instanceof ";
-  protected final String TEXT_15 = ") {" + NL + "\t\t\treturn ((";
-  protected final String TEXT_16 = ") selected).getElement();" + NL + "\t\t}";
-  protected final String TEXT_17 = NL + "\t\treturn selected;" + NL + "\t}" + NL + "" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected ";
-  protected final String TEXT_18 = " getPropertySourceProvider() {" + NL + "\t\treturn this; " + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic void setInput(IWorkbenchPart part, ISelection selection) {" + NL + "\t\tif (selection.isEmpty() || false == selection instanceof StructuredSelection) {" + NL + "\t\t\tsuper.setInput(part, selection);" + NL + "\t\t\treturn;" + NL + "\t\t}" + NL + "\t\tfinal StructuredSelection structuredSelection = ((StructuredSelection) selection);" + NL + "\t\tArrayList transformedSelection = new ArrayList(structuredSelection.size());" + NL + "\t\tfor (Iterator it = structuredSelection.iterator();  it.hasNext();) {" + NL + "\t\t\tObject r = transformSelection(it.next());" + NL + "\t\t\tif (r != null) {" + NL + "\t\t\t\ttransformedSelection.add(r);" + NL + "\t\t\t}" + NL + "\t\t}" + NL + "\t\tsuper.setInput(part, new StructuredSelection(transformedSelection));" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected AdapterFactory getAdapterFactory(Object object) {" + NL + "\t\tif (getEditingDomain() instanceof AdapterFactoryEditingDomain) {" + NL + "\t\t\treturn ((AdapterFactoryEditingDomain) getEditingDomain()).getAdapterFactory();" + NL + "\t\t}" + NL + "\t\treturn null;" + NL + "\t}" + NL + "}";
-  protected final String TEXT_19 = NL;
+  protected final String TEXT_9 = " getPropertySource(Object object) {" + NL + "\t\tif (object instanceof IPropertySource) {" + NL + "\t\t\treturn (IPropertySource) object;" + NL + "\t\t}" + NL + "\t\tAdapterFactory af = getAdapterFactory(object);" + NL + "\t\tif (af != null) {" + NL + "\t\t\tIItemPropertySource ips = (IItemPropertySource) af.adapt(object, IItemPropertySource.class);" + NL + "\t\t\tif (ips != null) {" + NL + "\t\t\t\treturn new PropertySource(object, ips);" + NL + "\t\t\t}" + NL + "\t\t}" + NL + "\t\tif (object instanceof IAdaptable) {" + NL + "\t\t\treturn (IPropertySource) ((IAdaptable) object).getAdapter(IPropertySource.class);" + NL + "\t\t}" + NL + "\t\treturn null;" + NL + "\t}";
+  protected final String TEXT_10 = NL + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic void createControls(";
+  protected final String TEXT_11 = " parent, ";
+  protected final String TEXT_12 = " aTabbedPropertySheetPage) {" + NL + "\t\tsuper.createControls(parent, aTabbedPropertySheetPage);" + NL + "\t\tclass ROEntry extends ";
+  protected final String TEXT_13 = " {" + NL + "\t\t\tROEntry(";
+  protected final String TEXT_14 = " operationHistory) {" + NL + "\t\t\t\tsuper(operationHistory);" + NL + "\t\t\t}" + NL + "\t\t\tpublic ";
+  protected final String TEXT_15 = " getEditor(";
+  protected final String TEXT_16 = " parentComposite) {" + NL + "\t\t\t\treturn null; // do not allow editing" + NL + "\t\t\t}" + NL + "\t\t\tprotected ";
+  protected final String TEXT_17 = " createChildEntry() {" + NL + "\t\t\t\treturn new ROEntry(getOperationHistory());" + NL + "\t\t\t}" + NL + "\t\t};" + NL + "\t\tROEntry root = new ROEntry(";
+  protected final String TEXT_18 = ".getOperationHistory()); " + NL + "\t\troot.setPropertySourceProvider(getPropertySourceProvider());" + NL + "\t\tpage.setRootEntry(root);" + NL + "\t}";
+  protected final String TEXT_19 = NL + NL + "\t/**" + NL + "\t * Modify/unwrap selection.  " + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected Object transformSelection(Object selected) {";
+  protected final String TEXT_20 = NL + "\t\tif (selected instanceof ";
+  protected final String TEXT_21 = ") {" + NL + "\t\t\tObject model = ((";
+  protected final String TEXT_22 = ") selected).getModel();" + NL + "\t\t\treturn model instanceof ";
+  protected final String TEXT_23 = " ? ((";
+  protected final String TEXT_24 = ") model).getElement() : null;" + NL + "\t\t}" + NL + "\t\tif (selected instanceof ";
+  protected final String TEXT_25 = ") {" + NL + "\t\t\treturn ((";
+  protected final String TEXT_26 = ") selected).getElement();" + NL + "\t\t}";
+  protected final String TEXT_27 = NL + "\t\treturn selected;" + NL + "\t}" + NL + "" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected ";
+  protected final String TEXT_28 = " getPropertySourceProvider() {" + NL + "\t\treturn this; " + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic void setInput(IWorkbenchPart part, ISelection selection) {" + NL + "\t\tif (selection.isEmpty() || false == selection instanceof StructuredSelection) {" + NL + "\t\t\tsuper.setInput(part, selection);" + NL + "\t\t\treturn;" + NL + "\t\t}" + NL + "\t\tfinal StructuredSelection structuredSelection = ((StructuredSelection) selection);" + NL + "\t\tArrayList transformedSelection = new ArrayList(structuredSelection.size());" + NL + "\t\tfor (Iterator it = structuredSelection.iterator();  it.hasNext();) {" + NL + "\t\t\tObject r = transformSelection(it.next());" + NL + "\t\t\tif (r != null) {" + NL + "\t\t\t\ttransformedSelection.add(r);" + NL + "\t\t\t}" + NL + "\t\t}" + NL + "\t\tsuper.setInput(part, new StructuredSelection(transformedSelection));" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected AdapterFactory getAdapterFactory(Object object) {" + NL + "\t\tif (getEditingDomain() instanceof AdapterFactoryEditingDomain) {" + NL + "\t\t\treturn ((AdapterFactoryEditingDomain) getEditingDomain()).getAdapterFactory();" + NL + "\t\t}" + NL + "\t\treturn null;" + NL + "\t}" + NL + "}";
+  protected final String TEXT_29 = NL;
 
   public String generate(Object argument)
   {
@@ -64,28 +74,50 @@ if (copyrightText != null && copyrightText.trim().length() > 0) {
     stringBuffer.append(TEXT_8);
     stringBuffer.append(importManager.getImportedName("org.eclipse.ui.views.properties.IPropertySource"));
     stringBuffer.append(TEXT_9);
+    if (tab.getSheet().isReadOnly()) {
+	final String entryClass = importManager.getImportedName("org.eclipse.gmf.runtime.emf.ui.properties.sections.UndoableModelPropertySheetEntry");
+	final String compositeClass  = importManager.getImportedName("org.eclipse.swt.widgets.Composite");
+    stringBuffer.append(TEXT_10);
+    stringBuffer.append(compositeClass);
+    stringBuffer.append(TEXT_11);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage"));
+    stringBuffer.append(TEXT_12);
+    stringBuffer.append(entryClass);
+    stringBuffer.append(TEXT_13);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.core.commands.operations.IOperationHistory"));
+    stringBuffer.append(TEXT_14);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.jface.viewers.CellEditor"));
+    stringBuffer.append(TEXT_15);
+    stringBuffer.append(compositeClass);
+    stringBuffer.append(TEXT_16);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.emf.ui.properties.sections.PropertySheetEntry"));
+    stringBuffer.append(TEXT_17);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.core.commands.operations.OperationHistoryFactory"));
+    stringBuffer.append(TEXT_18);
+    }
+    stringBuffer.append(TEXT_19);
     if ("domain".equals(tab.getID())) /*perhaps, override setInput should obey same condition?*/{
 	final String epClassName = importManager.getImportedName("org.eclipse.gef.EditPart");
 	final String viewClassName = importManager.getImportedName("org.eclipse.gmf.runtime.notation.View");
-    stringBuffer.append(TEXT_10);
+    stringBuffer.append(TEXT_20);
     stringBuffer.append(epClassName);
-    stringBuffer.append(TEXT_11);
+    stringBuffer.append(TEXT_21);
     stringBuffer.append(epClassName);
-    stringBuffer.append(TEXT_12);
+    stringBuffer.append(TEXT_22);
     stringBuffer.append(viewClassName);
-    stringBuffer.append(TEXT_13);
+    stringBuffer.append(TEXT_23);
     stringBuffer.append(viewClassName);
-    stringBuffer.append(TEXT_14);
+    stringBuffer.append(TEXT_24);
     stringBuffer.append(viewClassName);
-    stringBuffer.append(TEXT_15);
+    stringBuffer.append(TEXT_25);
     stringBuffer.append(viewClassName);
-    stringBuffer.append(TEXT_16);
+    stringBuffer.append(TEXT_26);
     }
-    stringBuffer.append(TEXT_17);
+    stringBuffer.append(TEXT_27);
     stringBuffer.append(importManager.getImportedName("org.eclipse.ui.views.properties.IPropertySourceProvider"));
-    stringBuffer.append(TEXT_18);
+    stringBuffer.append(TEXT_28);
     importManager.emitSortedImports();
-    stringBuffer.append(TEXT_19);
+    stringBuffer.append(TEXT_29);
     return stringBuffer.toString();
   }
 }
