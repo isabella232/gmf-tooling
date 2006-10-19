61b6d5fe132f7ca2eb583c14fbada713c111337e
dstadnik <dstadnik>
2006-10-19 14:11:21 +0000

    format

--- plugins/org.eclipse.gmf.map/src/org/eclipse/gmf/mappings/util/GMFMapMigration.java
+++ plugins/org.eclipse.gmf.map/src/org/eclipse/gmf/mappings/util/GMFMapMigration.java
@@ -13,30 +13,27 @@ package org.eclipse.gmf.mappings.util;
 import org.eclipse.gmf.internal.common.migrate.MigrationConfig;
 import org.eclipse.gmf.mappings.GMFMapPackage;
 
-
 public class GMFMapMigration {
 
 	private static final String GMFMAP_FILE_EXT = "gmfmap"; //$NON-NLS-1$
+
 	private static final String eNS_URI_1_0 = "http://www.eclipse.org/gmf/2005/mappings"; //$NON-NLS-1$ 
 
 	public static final MigrationConfig.Descriptor CONFIG = new MigrationConfig.Descriptor() {
+
 		public String getExtension() {
-			return GMFMAP_FILE_EXT; //$NON-NLS-1$
+			return GMFMAP_FILE_EXT;
 		}
-		
+
 		public MigrationConfig getConfig() {
-			MigrationConfig config = new MigrationConfig(GMFMapPackage.eNS_URI, new String[] { eNS_URI_1_0 });			
+			MigrationConfig config = new MigrationConfig(GMFMapPackage.eNS_URI, new String[] { eNS_URI_1_0 });
 			initialize(config);
 			return config;
 		}
 	};
-	
+
 	static void initialize(MigrationConfig config) {
 		// https://bugs.eclipse.org/bugs/show_bug.cgi?id=138440
-		config.addNarrowReferenceType(
-			GMFMapPackage.eINSTANCE.getFeatureSeqInitializer_Initializers(),
-			GMFMapPackage.eINSTANCE.getFeatureValueSpec()
-		);
+		config.addNarrowReferenceType(GMFMapPackage.eINSTANCE.getFeatureSeqInitializer_Initializers(), GMFMapPackage.eINSTANCE.getFeatureValueSpec());
 	}
-
 }
