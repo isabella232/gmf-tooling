6ab2f46f35995d669ab03ae4c1ea26c7123bf191
dstadnik <dstadnik>
2006-10-24 15:55:34 +0000

    simplify file names handling

--- plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/CreationWizardPageGenerator.java
+++ plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/CreationWizardPageGenerator.java
@@ -20,15 +20,25 @@ public class CreationWizardPageGenerator
   protected final String TEXT_3 = NL + " */";
   protected final String TEXT_4 = NL + NL + "import org.eclipse.core.runtime.IPath;" + NL + "import org.eclipse.jface.viewers.IStructuredSelection;" + NL + "import org.eclipse.swt.widgets.Composite;" + NL + "import org.eclipse.ui.dialogs.WizardNewFileCreationPage;" + NL;
   protected final String TEXT_5 = NL + NL + "/**" + NL + " * @generated" + NL + " */" + NL + "public class ";
-  protected final String TEXT_6 = " extends WizardNewFileCreationPage {" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic ";
-  protected final String TEXT_7 = "(String pageName, IStructuredSelection selection) {" + NL + "\t\tsuper(pageName, selection);" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected String getDefaultFileName() {" + NL + "\t\treturn \"default\"; //$NON-NLS-1$" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic String getFileName() {" + NL + "\t\tString fileName = super.getFileName();" + NL + "\t\tif (fileName != null) {" + NL + "\t\t\tfileName = getDiagramFileCreator().appendExtensionToFileName(fileName);" + NL + "\t\t}" + NL + "\t\treturn fileName;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic ";
-  protected final String TEXT_8 = " getDiagramFileCreator() {" + NL + "\t\treturn ";
-  protected final String TEXT_9 = ".getInstance();" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic void createControl(Composite parent) {" + NL + "\t\tsuper.createControl(parent);" + NL + "\t\tIPath path = getContainerFullPath();" + NL + "\t\tif (path != null) {" + NL + "\t\t\tString fileName = getDiagramFileCreator().getUniqueFileName(path, getDefaultFileName());" + NL + "\t\t\tsetFileName(fileName);" + NL + "\t\t}" + NL + "\t\tsetPageComplete(validatePage());" + NL + "\t}";
-  protected final String TEXT_10 = NL + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected boolean validatePage() {" + NL + "\t\tif (super.validatePage()) {" + NL + "\t\t\tString fileName = getFileName();" + NL + "\t\t\tif (fileName == null) {" + NL + "\t\t\t\treturn false;" + NL + "\t\t\t}" + NL + "\t\t\t// appending file extension to correctly process file names including \".\" symbol" + NL + "\t\t\tIPath path = getContainerFullPath().append(getDiagramFileCreator().appendExtensionToFileName(fileName));" + NL + "\t\t\tpath = path.removeFileExtension().addFileExtension(\"";
-  protected final String TEXT_11 = "\"); //$NON-NLS-1$" + NL + "\t\t\tif (";
-  protected final String TEXT_12 = ".exists(path)) {" + NL + "\t\t\t\tsetErrorMessage(\"Model file already exists: \" + path.lastSegment());" + NL + "\t\t\t\treturn false;" + NL + "\t\t\t}" + NL + "\t\t\treturn true;" + NL + "\t\t}" + NL + "\t\treturn false;" + NL + "\t}";
-  protected final String TEXT_13 = NL + "}";
-  protected final String TEXT_14 = NL;
+  protected final String TEXT_6 = " extends WizardNewFileCreationPage {";
+  protected final String TEXT_7 = NL + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate static final String DOMAIN_EXT = \".";
+  protected final String TEXT_8 = "\"; //$NON-NLS-1$";
+  protected final String TEXT_9 = NL + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate static final String DIAGRAM_EXT = \".";
+  protected final String TEXT_10 = "\"; //$NON-NLS-1$" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic ";
+  protected final String TEXT_11 = "(String pageName, IStructuredSelection selection) {" + NL + "\t\tsuper(pageName, selection);" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected String getDefaultFileName() {" + NL + "\t\treturn \"default\"; //$NON-NLS-1$" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic String getFileName() {" + NL + "\t\tString fileName = super.getFileName();" + NL + "\t\tif (fileName != null && !fileName.endsWith(DIAGRAM_EXT)) {" + NL + "\t\t\tfileName += DIAGRAM_EXT;" + NL + "\t\t}" + NL + "\t\treturn fileName;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate String getUniqueFileName(IPath containerPath, String fileName) {" + NL + "\t\tString newFileName = fileName;" + NL + "\t\tIPath diagramFilePath = containerPath.append(newFileName + DIAGRAM_EXT);";
+  protected final String TEXT_12 = NL + "\t\tIPath modelFilePath = containerPath.append(newFileName + DOMAIN_EXT);";
+  protected final String TEXT_13 = NL + "\t\tint i = 1;" + NL + "\t\twhile (exists(diagramFilePath)";
+  protected final String TEXT_14 = NL + "\t\t\t\t|| exists(modelFilePath)";
+  protected final String TEXT_15 = NL + "\t\t\t\t) {" + NL + "\t\t\ti++;" + NL + "\t\t\tnewFileName = fileName + i;" + NL + "\t\t\tdiagramFilePath = containerPath.append(newFileName + DIAGRAM_EXT);";
+  protected final String TEXT_16 = NL + "\t\t\tmodelFilePath = containerPath.append(newFileName + DOMAIN_EXT);";
+  protected final String TEXT_17 = NL + "\t\t}" + NL + "\t\treturn newFileName;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic void createControl(Composite parent) {" + NL + "\t\tsuper.createControl(parent);" + NL + "\t\tIPath path = getContainerFullPath();" + NL + "\t\tif (path != null) {" + NL + "\t\t\tString fileName = getUniqueFileName(path, getDefaultFileName());" + NL + "\t\t\tsetFileName(fileName);" + NL + "\t\t} else {" + NL + "\t\t\tsetFileName(getDefaultFileName());" + NL + "\t\t}" + NL + "\t\tsetPageComplete(validatePage());" + NL + "\t}";
+  protected final String TEXT_18 = NL + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected boolean validatePage() {" + NL + "\t\tif (super.validatePage()) {" + NL + "\t\t\tString fileName = getFileName();" + NL + "\t\t\tif (fileName == null) {" + NL + "\t\t\t\treturn false;" + NL + "\t\t\t}" + NL + "\t\t\tfileName = fileName.substring(0, fileName.length() - DIAGRAM_EXT.length()) + DOMAIN_EXT;" + NL + "\t\t\tIPath path = getContainerFullPath().append(fileName);" + NL + "\t\t\tif (exists(path)) {" + NL + "\t\t\t\tsetErrorMessage(\"Model file already exists: \" + path.lastSegment());" + NL + "\t\t\t\treturn false;" + NL + "\t\t\t}" + NL + "\t\t\treturn true;" + NL + "\t\t}" + NL + "\t\treturn false;" + NL + "\t}";
+  protected final String TEXT_19 = NL + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic static boolean exists(IPath path) {";
+  protected final String TEXT_20 = NL + "\t\treturn ";
+  protected final String TEXT_21 = ".getWorkspace().getRoot().exists(path);";
+  protected final String TEXT_22 = NL + "\t\treturn path.toFile().exists();";
+  protected final String TEXT_23 = NL + "\t}" + NL + "}";
+  protected final String TEXT_24 = NL;
 
   public String generate(Object argument)
   {
@@ -37,6 +47,7 @@ public class CreationWizardPageGenerator
 final GenDiagram genDiagram = (GenDiagram) ((Object[]) argument)[0];
 final GenEditorGenerator editorGen = genDiagram.getEditorGen();
 final ImportAssistant importManager = (ImportAssistant) ((Object[]) argument)[1];
+final GenApplication application = editorGen.getApplication();
 
     stringBuffer.append(TEXT_1);
     
@@ -53,22 +64,42 @@ if (copyrightText != null && copyrightText.trim().length() > 0) {
     stringBuffer.append(TEXT_5);
     stringBuffer.append(importManager.getCompilationUnitName());
     stringBuffer.append(TEXT_6);
-    stringBuffer.append(importManager.getCompilationUnitName());
-    stringBuffer.append(TEXT_7);
-    stringBuffer.append(importManager.getImportedName(genDiagram.getDiagramFileCreatorQualifiedClassName()));
-    stringBuffer.append(TEXT_8);
-    stringBuffer.append(importManager.getImportedName(genDiagram.getDiagramFileCreatorQualifiedClassName()));
-    stringBuffer.append(TEXT_9);
     if (!editorGen.isSameFileForDiagramAndModel()) {
-    stringBuffer.append(TEXT_10);
+    stringBuffer.append(TEXT_7);
     stringBuffer.append(editorGen.getDomainFileExtension());
+    stringBuffer.append(TEXT_8);
+    }
+    stringBuffer.append(TEXT_9);
+    stringBuffer.append(editorGen.getDiagramFileExtension());
+    stringBuffer.append(TEXT_10);
+    stringBuffer.append(importManager.getCompilationUnitName());
     stringBuffer.append(TEXT_11);
-    stringBuffer.append(importManager.getImportedName(genDiagram.getDiagramFileCreatorQualifiedClassName()));
+    if (!editorGen.isSameFileForDiagramAndModel()) {
     stringBuffer.append(TEXT_12);
     }
     stringBuffer.append(TEXT_13);
-    importManager.emitSortedImports();
+    if (!editorGen.isSameFileForDiagramAndModel()) {
     stringBuffer.append(TEXT_14);
+    }
+    stringBuffer.append(TEXT_15);
+    if (!editorGen.isSameFileForDiagramAndModel()) {
+    stringBuffer.append(TEXT_16);
+    }
+    stringBuffer.append(TEXT_17);
+    if (!editorGen.isSameFileForDiagramAndModel()) {
+    stringBuffer.append(TEXT_18);
+    }
+    stringBuffer.append(TEXT_19);
+    if (application == null) {
+    stringBuffer.append(TEXT_20);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.core.resources.ResourcesPlugin"));
+    stringBuffer.append(TEXT_21);
+    } else {
+    stringBuffer.append(TEXT_22);
+    }
+    stringBuffer.append(TEXT_23);
+    importManager.emitSortedImports();
+    stringBuffer.append(TEXT_24);
     return stringBuffer.toString();
   }
 }
--- plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/DiagramFileCreatorGenerator.java
+++ plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/DiagramFileCreatorGenerator.java
@@ -20,32 +20,15 @@ public class DiagramFileCreatorGenerator
   protected final String TEXT_3 = NL + " */";
   protected final String TEXT_4 = NL + NL + "import java.io.ByteArrayInputStream;" + NL + "" + NL + "import org.eclipse.jface.dialogs.ErrorDialog;" + NL + "import org.eclipse.swt.widgets.Shell;" + NL + "import org.eclipse.core.resources.IFile;" + NL + "import org.eclipse.core.resources.IResource;" + NL + "import org.eclipse.core.resources.IResourceStatus;" + NL + "import org.eclipse.core.resources.ResourcesPlugin;" + NL + "" + NL + "import org.eclipse.core.runtime.CoreException;" + NL + "import org.eclipse.core.runtime.IPath;" + NL + "import org.eclipse.core.runtime.NullProgressMonitor;";
   protected final String TEXT_5 = NL + NL + "/**" + NL + " * @generated" + NL + " */" + NL + "public class ";
-  protected final String TEXT_6 = " {" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate static ";
-  protected final String TEXT_7 = " INSTANCE = new ";
-  protected final String TEXT_8 = "();" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic static ";
-  protected final String TEXT_9 = " getInstance() {" + NL + "\t\treturn INSTANCE;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic static boolean exists(IPath path) {";
-  protected final String TEXT_10 = NL + "\t\treturn ResourcesPlugin.getWorkspace().getRoot().exists(path);";
-  protected final String TEXT_11 = NL + "\t\treturn path.toFile().exists();";
-  protected final String TEXT_12 = NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic String getExtension() {" + NL + "\t\treturn \".";
-  protected final String TEXT_13 = "\"; //$NON-NLS-1$" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic String getUniqueFileName(IPath containerPath, String fileName) {" + NL + "\t\tint nFileNumber = 1;" + NL + "\t\tfileName = removeExtensionFromFileName(fileName);" + NL + "\t\tString newFileName = fileName;" + NL + "\t\tIPath diagramFilePath = containerPath.append(appendExtensionToFileName(newFileName));";
-  protected final String TEXT_14 = NL + "\t\tIPath modelFilePath = containerPath.append(newFileName + \".";
-  protected final String TEXT_15 = "\"); //$NON-NLS-1$";
-  protected final String TEXT_16 = NL + "\t\twhile (exists(diagramFilePath)";
-  protected final String TEXT_17 = NL + "\t\t\t\t|| exists(modelFilePath)";
-  protected final String TEXT_18 = NL + "\t\t\t\t) {" + NL + "\t\t\tnFileNumber++;" + NL + "\t\t\tnewFileName = fileName + nFileNumber;" + NL + "\t\t\tdiagramFilePath = containerPath.append(appendExtensionToFileName(newFileName));";
-  protected final String TEXT_19 = NL + "\t\t\tmodelFilePath = containerPath.append(newFileName + \".";
-  protected final String TEXT_20 = "\"); //$NON-NLS-1$";
-  protected final String TEXT_21 = NL + "\t\t}" + NL + "\t\treturn newFileName;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic String appendExtensionToFileName(String fileName) {" + NL + "\t\tif (!fileName.endsWith(getExtension())) {" + NL + "\t\t\treturn fileName + getExtension();" + NL + "\t\t}" + NL + "\t\treturn fileName;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate String removeExtensionFromFileName(String fileName) {" + NL + "\t\tif (fileName.endsWith(getExtension())) {" + NL + "\t\t\treturn fileName.substring(0, fileName.length() - getExtension().length());" + NL + "\t\t}" + NL + "\t\treturn fileName;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic IFile createNewFile(IPath containerPath, String fileName, Shell shell) {" + NL + "\t\tIPath newFilePath = containerPath.append(appendExtensionToFileName(fileName));" + NL + "\t\tIFile newFileHandle = ResourcesPlugin.getWorkspace().getRoot().getFile(newFilePath);" + NL + "\t\ttry {" + NL + "\t\t\tcreateFile(newFileHandle);" + NL + "\t\t} catch (CoreException e) {" + NL + "\t\t\tErrorDialog.openError(shell, \"Creation Problems\", null, e.getStatus());" + NL + "\t\t\treturn null;" + NL + "\t\t}" + NL + "\t\treturn newFileHandle;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected void createFile(IFile fileHandle) throws CoreException {" + NL + "\t\ttry {" + NL + "\t\t\tfileHandle.create(new ByteArrayInputStream(new byte[0]), false, new NullProgressMonitor());" + NL + "\t\t} catch (CoreException e) {" + NL + "\t\t\t// If the file already existed locally, just refresh to get contents" + NL + "\t\t\tif (e.getStatus().getCode() == IResourceStatus.PATH_OCCUPIED) {" + NL + "\t\t\t\tfileHandle.refreshLocal(IResource.DEPTH_ZERO, null);" + NL + "\t\t\t} else {" + NL + "\t\t\t\tthrow e;" + NL + "\t\t\t}" + NL + "\t\t}" + NL + "\t}" + NL + "}";
-  protected final String TEXT_22 = NL;
+  protected final String TEXT_6 = " {" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic static IFile createNewFile(IPath containerPath, String fileName, Shell shell) {" + NL + "\t\tIPath newFilePath = containerPath.append(fileName);" + NL + "\t\tIFile newFileHandle = ResourcesPlugin.getWorkspace().getRoot().getFile(newFilePath);" + NL + "\t\ttry {" + NL + "\t\t\tcreateFile(newFileHandle);" + NL + "\t\t} catch (CoreException e) {" + NL + "\t\t\tErrorDialog.openError(shell, \"Creation Problems\", null, e.getStatus());" + NL + "\t\t\treturn null;" + NL + "\t\t}" + NL + "\t\treturn newFileHandle;" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprotected static void createFile(IFile fileHandle) throws CoreException {" + NL + "\t\ttry {" + NL + "\t\t\tfileHandle.create(new ByteArrayInputStream(new byte[0]), false, new NullProgressMonitor());" + NL + "\t\t} catch (CoreException e) {" + NL + "\t\t\t// If the file already existed locally, just refresh to get contents" + NL + "\t\t\tif (e.getStatus().getCode() == IResourceStatus.PATH_OCCUPIED) {" + NL + "\t\t\t\tfileHandle.refreshLocal(IResource.DEPTH_ZERO, null);" + NL + "\t\t\t} else {" + NL + "\t\t\t\tthrow e;" + NL + "\t\t\t}" + NL + "\t\t}" + NL + "\t}" + NL + "}";
+  protected final String TEXT_7 = NL;
 
   public String generate(Object argument)
   {
     final StringBuffer stringBuffer = new StringBuffer();
     
 final GenDiagram genDiagram = (GenDiagram) ((Object[]) argument)[0];
-final GenEditorGenerator editorGen = genDiagram.getEditorGen();
 final ImportAssistant importManager = (ImportAssistant) ((Object[]) argument)[1];
-final GenApplication application = editorGen.getApplication();
 
     stringBuffer.append(TEXT_1);
     
@@ -62,38 +45,8 @@ if (copyrightText != null && copyrightText.trim().length() > 0) {
     stringBuffer.append(TEXT_5);
     stringBuffer.append(importManager.getCompilationUnitName());
     stringBuffer.append(TEXT_6);
-    stringBuffer.append(importManager.getCompilationUnitName());
-    stringBuffer.append(TEXT_7);
-    stringBuffer.append(importManager.getCompilationUnitName());
-    stringBuffer.append(TEXT_8);
-    stringBuffer.append(importManager.getCompilationUnitName());
-    stringBuffer.append(TEXT_9);
-    if (application == null) {
-    stringBuffer.append(TEXT_10);
-    } else {
-    stringBuffer.append(TEXT_11);
-    }
-    stringBuffer.append(TEXT_12);
-    stringBuffer.append(editorGen.getDiagramFileExtension());
-    stringBuffer.append(TEXT_13);
-    if (!editorGen.isSameFileForDiagramAndModel()) {
-    stringBuffer.append(TEXT_14);
-    stringBuffer.append(editorGen.getDomainFileExtension());
-    stringBuffer.append(TEXT_15);
-    }
-    stringBuffer.append(TEXT_16);
-    if (!editorGen.isSameFileForDiagramAndModel()) {
-    stringBuffer.append(TEXT_17);
-    }
-    stringBuffer.append(TEXT_18);
-    if (!editorGen.isSameFileForDiagramAndModel()) {
-    stringBuffer.append(TEXT_19);
-    stringBuffer.append(editorGen.getDomainFileExtension());
-    stringBuffer.append(TEXT_20);
-    }
-    stringBuffer.append(TEXT_21);
     importManager.emitSortedImports();
-    stringBuffer.append(TEXT_22);
+    stringBuffer.append(TEXT_7);
     return stringBuffer.toString();
   }
 }
