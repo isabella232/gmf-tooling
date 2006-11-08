61bde0d552cc70afc195206e53658a3432c7157b
ashatalin <ashatalin>
2006-11-08 19:03:14 +0000

    Removing commented out code.

--- plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/navigator/NavigatorActionProviderGenerator.java
+++ plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/navigator/NavigatorActionProviderGenerator.java
@@ -30,32 +30,30 @@ public class NavigatorActionProviderGenerator
   protected final String TEXT_13 = " selection = (";
   protected final String TEXT_14 = ") getContext().getSelection();" + NL + "\t\tmyOpenDiagramAction.selectionChanged(selection);" + NL + "\t\tif (myOpenDiagramAction.isEnabled()) {" + NL + "\t\t\tactionBars.setGlobalActionHandler(";
   protected final String TEXT_15 = ".OPEN, myOpenDiagramAction);" + NL + "\t\t}" + NL + "\t}" + NL + "" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tpublic void fillContextMenu(";
-  protected final String TEXT_16 = " menu) {" + NL + "/*\t\tif (!myContribute || getContext().getSelection().isEmpty()) {" + NL + "\t\t\treturn;" + NL + "\t\t}" + NL + "" + NL + "\t\t";
-  protected final String TEXT_17 = " selection = (";
-  protected final String TEXT_18 = ") getContext().getSelection();" + NL + "" + NL + "\t\tmyOpenDiagramAction.selectionChanged(selection);" + NL + "\t\tif (myOpenDiagramAction.isEnabled()) {" + NL + "\t\t\tmenu.insertAfter(";
-  protected final String TEXT_19 = ".GROUP_OPEN, myOpenDiagramAction);" + NL + "\t\t}*/" + NL + "\t}" + NL + "\t" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate class OpenDiagramAction extends ";
-  protected final String TEXT_20 = " {" + NL + "\t" + NL + "\t\t/**" + NL + "\t\t * @generated" + NL + "\t\t */" + NL + "\t\tprivate ";
-  protected final String TEXT_21 = " myDiagram;" + NL + "\t\t" + NL + "\t\t/**" + NL + "\t\t * @generated" + NL + "\t\t */" + NL + "\t\tprivate ";
-  protected final String TEXT_22 = " myViewerSite;" + NL + "\t" + NL + "\t\t/**" + NL + "\t\t * @generated" + NL + "\t\t */" + NL + "\t\tpublic OpenDiagramAction(";
-  protected final String TEXT_23 = " viewerSite) {" + NL + "\t\t\tsuper(\"Open Diagram\");" + NL + "\t\t\tmyViewerSite = viewerSite;" + NL + "\t\t}" + NL + "" + NL + "\t\t/**" + NL + "\t\t * @generated" + NL + "\t\t */" + NL + "\t\tpublic final void selectionChanged(";
-  protected final String TEXT_24 = " selection) {" + NL + "\t\t\tmyDiagram = null;" + NL + "\t\t\tif (selection.size() == 1) {" + NL + "\t\t\t\tObject selectedElement = selection.getFirstElement();" + NL + "\t\t\t\tif (selectedElement instanceof ";
+  protected final String TEXT_16 = " menu) {";
+  protected final String TEXT_17 = "\t\t" + NL + "\t}" + NL + "\t" + NL + "\t/**" + NL + "\t * @generated" + NL + "\t */" + NL + "\tprivate class OpenDiagramAction extends ";
+  protected final String TEXT_18 = " {" + NL + "\t" + NL + "\t\t/**" + NL + "\t\t * @generated" + NL + "\t\t */" + NL + "\t\tprivate ";
+  protected final String TEXT_19 = " myDiagram;" + NL + "\t\t" + NL + "\t\t/**" + NL + "\t\t * @generated" + NL + "\t\t */" + NL + "\t\tprivate ";
+  protected final String TEXT_20 = " myViewerSite;" + NL + "\t" + NL + "\t\t/**" + NL + "\t\t * @generated" + NL + "\t\t */" + NL + "\t\tpublic OpenDiagramAction(";
+  protected final String TEXT_21 = " viewerSite) {" + NL + "\t\t\tsuper(\"Open Diagram\");" + NL + "\t\t\tmyViewerSite = viewerSite;" + NL + "\t\t}" + NL + "" + NL + "\t\t/**" + NL + "\t\t * @generated" + NL + "\t\t */" + NL + "\t\tpublic final void selectionChanged(";
+  protected final String TEXT_22 = " selection) {" + NL + "\t\t\tmyDiagram = null;" + NL + "\t\t\tif (selection.size() == 1) {" + NL + "\t\t\t\tObject selectedElement = selection.getFirstElement();" + NL + "\t\t\t\tif (selectedElement instanceof ";
+  protected final String TEXT_23 = ") {" + NL + "\t\t\t\t\tselectedElement = ((";
+  protected final String TEXT_24 = ") selectedElement).getView();" + NL + "\t\t\t\t} else if (selectedElement instanceof ";
   protected final String TEXT_25 = ") {" + NL + "\t\t\t\t\tselectedElement = ((";
-  protected final String TEXT_26 = ") selectedElement).getView();" + NL + "\t\t\t\t} else if (selectedElement instanceof ";
-  protected final String TEXT_27 = ") {" + NL + "\t\t\t\t\tselectedElement = ((";
-  protected final String TEXT_28 = ") selectedElement).getAdapter(";
-  protected final String TEXT_29 = ".class);" + NL + "\t\t\t\t}" + NL + "\t\t\t\tif (selectedElement instanceof ";
-  protected final String TEXT_30 = ") {" + NL + "\t\t\t\t\t";
-  protected final String TEXT_31 = " diagram = (";
-  protected final String TEXT_32 = ") selectedElement;" + NL + "\t\t\t\t\tif (";
-  protected final String TEXT_33 = ".MODEL_ID.equals(";
-  protected final String TEXT_34 = ".getModelID(diagram))) {" + NL + "\t\t\t\t\t\tmyDiagram = diagram;" + NL + "\t\t\t\t\t}" + NL + "\t\t\t\t}" + NL + "\t\t\t}" + NL + "\t\t\tsetEnabled(myDiagram != null);" + NL + "\t\t}" + NL + "" + NL + "\t\t/**" + NL + "\t\t * @generated" + NL + "\t\t */" + NL + "\t\tpublic void run() {" + NL + "\t\t\tif (myDiagram == null) {" + NL + "\t\t\t\treturn;" + NL + "\t\t\t}" + NL + "\t\t\t";
-  protected final String TEXT_35 = " editorInput = new ";
-  protected final String TEXT_36 = "(myDiagram);" + NL + "\t\t\t";
-  protected final String TEXT_37 = " page = myViewerSite.getPage();" + NL + "\t\t \ttry {" + NL + "\t\t\t\tpage.openEditor(editorInput, ";
-  protected final String TEXT_38 = ".ID);" + NL + "\t\t\t} catch (";
-  protected final String TEXT_39 = " e) {" + NL + "\t\t\t\t";
-  protected final String TEXT_40 = ".getInstance().logError(\"Exception while openning diagram\", e);" + NL + "\t\t\t}" + NL + "\t\t}" + NL + "\t\t" + NL + "\t}" + NL + "" + NL + "}";
-  protected final String TEXT_41 = NL;
+  protected final String TEXT_26 = ") selectedElement).getAdapter(";
+  protected final String TEXT_27 = ".class);" + NL + "\t\t\t\t}" + NL + "\t\t\t\tif (selectedElement instanceof ";
+  protected final String TEXT_28 = ") {" + NL + "\t\t\t\t\t";
+  protected final String TEXT_29 = " diagram = (";
+  protected final String TEXT_30 = ") selectedElement;" + NL + "\t\t\t\t\tif (";
+  protected final String TEXT_31 = ".MODEL_ID.equals(";
+  protected final String TEXT_32 = ".getModelID(diagram))) {" + NL + "\t\t\t\t\t\tmyDiagram = diagram;" + NL + "\t\t\t\t\t}" + NL + "\t\t\t\t}" + NL + "\t\t\t}" + NL + "\t\t\tsetEnabled(myDiagram != null);" + NL + "\t\t}" + NL + "" + NL + "\t\t/**" + NL + "\t\t * @generated" + NL + "\t\t */" + NL + "\t\tpublic void run() {" + NL + "\t\t\tif (myDiagram == null) {" + NL + "\t\t\t\treturn;" + NL + "\t\t\t}" + NL + "\t\t\t";
+  protected final String TEXT_33 = " editorInput = new ";
+  protected final String TEXT_34 = "(myDiagram);" + NL + "\t\t\t";
+  protected final String TEXT_35 = " page = myViewerSite.getPage();" + NL + "\t\t \ttry {" + NL + "\t\t\t\tpage.openEditor(editorInput, ";
+  protected final String TEXT_36 = ".ID);" + NL + "\t\t\t} catch (";
+  protected final String TEXT_37 = " e) {" + NL + "\t\t\t\t";
+  protected final String TEXT_38 = ".getInstance().logError(\"Exception while openning diagram\", e);" + NL + "\t\t\t}" + NL + "\t\t}" + NL + "\t\t" + NL + "\t}" + NL + "" + NL + "}";
+  protected final String TEXT_39 = NL;
 
   public String generate(Object argument)
   {
@@ -101,56 +99,65 @@ if (copyrightText != null && copyrightText.trim().length() > 0) {
     stringBuffer.append(TEXT_15);
     stringBuffer.append(importManager.getImportedName("org.eclipse.jface.action.IMenuManager"));
     stringBuffer.append(TEXT_16);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.jface.viewers.IStructuredSelection"));
+    
+/*
+ * Put following code into the template to generate popup menu
+ *
+		if (!myContribute || getContext().getSelection().isEmpty()) {
+			return;
+		}
+
+		< %=importManager.getImportedName("org.eclipse.jface.viewers.IStructuredSelection")% > selection = (< %=importManager.getImportedName("org.eclipse.jface.viewers.IStructuredSelection")% >) getContext().getSelection();
+
+		myOpenDiagramAction.selectionChanged(selection);
+		if (myOpenDiagramAction.isEnabled()) {
+			menu.insertAfter(< %=importManager.getImportedName("org.eclipse.ui.navigator.ICommonMenuConstants")% >.GROUP_OPEN, myOpenDiagramAction);
+		}*/
     stringBuffer.append(TEXT_17);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.jface.viewers.IStructuredSelection"));
-    stringBuffer.append(TEXT_18);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.ui.navigator.ICommonMenuConstants"));
-    stringBuffer.append(TEXT_19);
     stringBuffer.append(importManager.getImportedName("org.eclipse.jface.action.Action"));
-    stringBuffer.append(TEXT_20);
+    stringBuffer.append(TEXT_18);
     stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.notation.Diagram"));
+    stringBuffer.append(TEXT_19);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.ui.navigator.ICommonViewerWorkbenchSite"));
+    stringBuffer.append(TEXT_20);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.ui.navigator.ICommonViewerWorkbenchSite"));
     stringBuffer.append(TEXT_21);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.ui.navigator.ICommonViewerWorkbenchSite"));
-    stringBuffer.append(TEXT_22);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.ui.navigator.ICommonViewerWorkbenchSite"));
-    stringBuffer.append(TEXT_23);
     stringBuffer.append(importManager.getImportedName("org.eclipse.jface.viewers.IStructuredSelection"));
+    stringBuffer.append(TEXT_22);
+    stringBuffer.append(importManager.getImportedName(genNavigator.getNavigatorItemQualifiedClassName()));
+    stringBuffer.append(TEXT_23);
+    stringBuffer.append(importManager.getImportedName(genNavigator.getNavigatorItemQualifiedClassName()));
     stringBuffer.append(TEXT_24);
-    stringBuffer.append(importManager.getImportedName(genNavigator.getNavigatorItemQualifiedClassName()));
+    stringBuffer.append(importManager.getImportedName("org.eclipse.core.runtime.IAdaptable"));
     stringBuffer.append(TEXT_25);
-    stringBuffer.append(importManager.getImportedName(genNavigator.getNavigatorItemQualifiedClassName()));
+    stringBuffer.append(importManager.getImportedName("org.eclipse.core.runtime.IAdaptable"));
     stringBuffer.append(TEXT_26);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.core.runtime.IAdaptable"));
-    stringBuffer.append(TEXT_27);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.core.runtime.IAdaptable"));
-    stringBuffer.append(TEXT_28);
     stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.notation.View"));
+    stringBuffer.append(TEXT_27);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.notation.Diagram"));
+    stringBuffer.append(TEXT_28);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.notation.Diagram"));
     stringBuffer.append(TEXT_29);
     stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.notation.Diagram"));
     stringBuffer.append(TEXT_30);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.notation.Diagram"));
-    stringBuffer.append(TEXT_31);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.notation.Diagram"));
-    stringBuffer.append(TEXT_32);
     stringBuffer.append(importManager.getImportedName(genDiagram.getEditPartQualifiedClassName()));
-    stringBuffer.append(TEXT_33);
+    stringBuffer.append(TEXT_31);
     stringBuffer.append(importManager.getImportedName(genDiagram.getVisualIDRegistryQualifiedClassName()));
+    stringBuffer.append(TEXT_32);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditorInput"));
+    stringBuffer.append(TEXT_33);
+    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditorInput"));
     stringBuffer.append(TEXT_34);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditorInput"));
-    stringBuffer.append(TEXT_35);
-    stringBuffer.append(importManager.getImportedName("org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditorInput"));
-    stringBuffer.append(TEXT_36);
     stringBuffer.append(importManager.getImportedName("org.eclipse.ui.IWorkbenchPage"));
-    stringBuffer.append(TEXT_37);
+    stringBuffer.append(TEXT_35);
     stringBuffer.append(importManager.getImportedName(genNavigator.getEditorGen().getEditor().getQualifiedClassName()));
-    stringBuffer.append(TEXT_38);
+    stringBuffer.append(TEXT_36);
     stringBuffer.append(importManager.getImportedName("org.eclipse.ui.PartInitException"));
-    stringBuffer.append(TEXT_39);
+    stringBuffer.append(TEXT_37);
     stringBuffer.append(importManager.getImportedName(genNavigator.getEditorGen().getPlugin().getActivatorQualifiedClassName()));
-    stringBuffer.append(TEXT_40);
+    stringBuffer.append(TEXT_38);
     importManager.emitSortedImports();
-    stringBuffer.append(TEXT_41);
+    stringBuffer.append(TEXT_39);
     return stringBuffer.toString();
   }
 }
