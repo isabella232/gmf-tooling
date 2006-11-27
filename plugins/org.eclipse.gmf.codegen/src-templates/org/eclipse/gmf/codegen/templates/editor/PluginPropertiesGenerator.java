1e3e74570790388db6fdbabb8d821a2ad69131c7
dstadnik <dstadnik>
2006-11-27 17:28:36 +0000

    generate common rcp actions

--- plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/PluginPropertiesGenerator.java
+++ plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/PluginPropertiesGenerator.java
@@ -27,21 +27,23 @@ public class PluginPropertiesGenerator
   protected final String TEXT_10 = NL + "loadResourceActionLabel=Load Resource...";
   protected final String TEXT_11 = NL + "newDiagramActionSetLabel=New ";
   protected final String TEXT_12 = " Diagram" + NL + "newDiagramActionLabel=";
-  protected final String TEXT_13 = " Diagram";
-  protected final String TEXT_14 = NL + "navigatorContentName=*.";
-  protected final String TEXT_15 = " diagram contents";
-  protected final String TEXT_16 = NL + "###" + NL + "# Property Sheet";
-  protected final String TEXT_17 = NL;
-  protected final String TEXT_18 = NL + "tab.";
-  protected final String TEXT_19 = "=";
-  protected final String TEXT_20 = NL + "###";
-  protected final String TEXT_21 = NL + "###" + NL + "# Palette Factory";
-  protected final String TEXT_22 = NL;
-  protected final String TEXT_23 = NL;
-  protected final String TEXT_24 = ".title=";
+  protected final String TEXT_13 = " Diagram" + NL + "applicationActionSetLabel=";
+  protected final String TEXT_14 = " Actions" + NL + "aboutActionLabel=";
+  protected final String TEXT_15 = " Diagram About..." + NL + "openURIActionLabel=Open URI..." + NL + "openURIActionDescription=Open file by URI" + NL + "openActionLabel=Open..." + NL + "openActionDescription=Open file";
+  protected final String TEXT_16 = NL + "navigatorContentName=*.";
+  protected final String TEXT_17 = " diagram contents";
+  protected final String TEXT_18 = NL + "###" + NL + "# Property Sheet";
+  protected final String TEXT_19 = NL;
+  protected final String TEXT_20 = NL + "tab.";
+  protected final String TEXT_21 = "=";
+  protected final String TEXT_22 = NL + "###";
+  protected final String TEXT_23 = NL + "###" + NL + "# Palette Factory";
+  protected final String TEXT_24 = NL;
   protected final String TEXT_25 = NL;
-  protected final String TEXT_26 = ".desc=";
-  protected final String TEXT_27 = NL + "###";
+  protected final String TEXT_26 = ".title=";
+  protected final String TEXT_27 = NL;
+  protected final String TEXT_28 = ".desc=";
+  protected final String TEXT_29 = NL + "###";
 
   public String generate(Object argument)
   {
@@ -76,44 +78,48 @@ if (genDiagram.generateCreateShortcutAction()) {
     stringBuffer.append(TEXT_12);
     stringBuffer.append(genPlugin.getEditorGen().getModelID());
     stringBuffer.append(TEXT_13);
-    }
+    stringBuffer.append(genPlugin.getEditorGen().getModelID());
     stringBuffer.append(TEXT_14);
-    stringBuffer.append(genPlugin.getEditorGen().getDiagramFileExtension());
+    stringBuffer.append(genPlugin.getEditorGen().getModelID());
     stringBuffer.append(TEXT_15);
+    }
+    stringBuffer.append(TEXT_16);
+    stringBuffer.append(genPlugin.getEditorGen().getDiagramFileExtension());
+    stringBuffer.append(TEXT_17);
     if (genPlugin.getEditorGen().getPropertySheet() != null) {
 	final GenPropertySheet propertySheet = genPlugin.getEditorGen().getPropertySheet();
-    stringBuffer.append(TEXT_16);
-    stringBuffer.append(TEXT_17);
+    stringBuffer.append(TEXT_18);
+    stringBuffer.append(TEXT_19);
     for (Iterator it = propertySheet.getTabs().iterator(); it.hasNext(); ) {
 	final GenPropertyTab tab = (GenPropertyTab) it.next();
-    stringBuffer.append(TEXT_18);
+    stringBuffer.append(TEXT_20);
     stringBuffer.append(tab.getID());
-    stringBuffer.append(TEXT_19);
+    stringBuffer.append(TEXT_21);
     stringBuffer.append(tab.getLabel());
     }
-    stringBuffer.append(TEXT_20);
+    stringBuffer.append(TEXT_22);
     }
     if (genDiagram.getPalette() != null) {
 	final Palette palette = genDiagram.getPalette();
-    stringBuffer.append(TEXT_21);
-    stringBuffer.append(TEXT_22);
+    stringBuffer.append(TEXT_23);
+    stringBuffer.append(TEXT_24);
     for (Iterator it = palette.eAllContents(); it.hasNext();) {
 	Object next = it.next();
 	if (next instanceof EntryBase) {
 		EntryBase b = (EntryBase) next;
-    stringBuffer.append(TEXT_23);
-    stringBuffer.append(b.getIntKey());
-    stringBuffer.append(TEXT_24);
-    stringBuffer.append(b.getTitle());
-    if (b.getDescription() != null) {
     stringBuffer.append(TEXT_25);
     stringBuffer.append(b.getIntKey());
     stringBuffer.append(TEXT_26);
+    stringBuffer.append(b.getTitle());
+    if (b.getDescription() != null) {
+    stringBuffer.append(TEXT_27);
+    stringBuffer.append(b.getIntKey());
+    stringBuffer.append(TEXT_28);
     stringBuffer.append(b.getDescription());
     }
     	} /*EntryBase*/
     }
-    stringBuffer.append(TEXT_27);
+    stringBuffer.append(TEXT_29);
     }
     return stringBuffer.toString();
   }
--- plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/PluginXML.java
+++ plugins/org.eclipse.gmf.codegen/src-templates/org/eclipse/gmf/codegen/templates/editor/PluginXML.java
@@ -299,8 +299,21 @@ public class PluginXML
   protected final String TEXT_281 = "\">" + NL + "      </perspective>" + NL + "   </extension>" + NL + "" + NL + "   <extension point=\"org.eclipse.ui.actionSets\">" + NL + "      <?gmfgen generated=\"true\"?>" + NL + "      <actionSet" + NL + "            label=\"%newDiagramActionSetLabel\"" + NL + "            visible=\"true\"" + NL + "            id=\"";
   protected final String TEXT_282 = ".NewDiagramActionSet\">" + NL + "         <action" + NL + "               label=\"%newDiagramActionLabel\"" + NL + "               class=\"";
   protected final String TEXT_283 = "$NewDiagramAction\"" + NL + "               menubarPath=\"file/new/additions\"" + NL + "               id=\"";
-  protected final String TEXT_284 = ".NewDiagramAction\">" + NL + "         </action>" + NL + "      </actionSet>" + NL + "   </extension>  ";
-  protected final String TEXT_285 = NL + "</plugin>";
+  protected final String TEXT_284 = ".NewDiagramAction\">" + NL + "         </action>" + NL + "      </actionSet>" + NL + "   </extension>  " + NL + "" + NL + "   <extension point=\"org.eclipse.ui.commands\">" + NL + "      <?gmfgen generated=\"true\"?>" + NL + "      <command" + NL + "            name=\"%openURIActionLabel\"" + NL + "            description=\"%openURIActionDescription\"" + NL + "            categoryId=\"org.eclipse.ui.category.file\"" + NL + "            id=\"";
+  protected final String TEXT_285 = ".OpenURICommand\"/>  " + NL + "      <command" + NL + "            name=\"%openActionLabel\"" + NL + "            description=\"%openActionDescription\"" + NL + "            categoryId=\"org.eclipse.ui.category.file\"" + NL + "            id=\"";
+  protected final String TEXT_286 = ".OpenCommand\"/>  " + NL + "   </extension>" + NL + "" + NL + "   <extension point=\"org.eclipse.ui.bindings\">" + NL + "      <?gmfgen generated=\"true\"?>" + NL + "      <key" + NL + "            commandId=\"";
+  protected final String TEXT_287 = ".OpenURICommand\"" + NL + "            sequence=\"M1+U\"" + NL + "            schemeId=\"org.eclipse.ui.defaultAcceleratorConfiguration\"/>" + NL + "      <key" + NL + "            commandId=\"";
+  protected final String TEXT_288 = ".OpenCommand\"" + NL + "            sequence=\"M1+O\"" + NL + "            schemeId=\"org.eclipse.ui.defaultAcceleratorConfiguration\"/>" + NL + "   </extension>" + NL + "" + NL + "   <extension point=\"org.eclipse.ui.actionSets\">" + NL + "      <?gmfgen generated=\"true\"?>" + NL + "      <actionSet" + NL + "            label=\"%applicationActionSetLabel\"" + NL + "            visible=\"true\"" + NL + "            id=\"";
+  protected final String TEXT_289 = ".ActionSet\">" + NL + "         <action" + NL + "               label=\"%aboutActionLabel\"" + NL + "               class=\"";
+  protected final String TEXT_290 = "$AboutAction\"" + NL + "               menubarPath=\"help/additions\"" + NL + "               id=\"";
+  protected final String TEXT_291 = ".AboutAction\">" + NL + "         </action>" + NL + "         <action" + NL + "               label=\"%openURIActionLabel\"" + NL + "               definitionId=\"";
+  protected final String TEXT_292 = ".OpenURICommand\"" + NL + "               class=\"";
+  protected final String TEXT_293 = "$OpenURIAction\"" + NL + "               menubarPath=\"file/additions\"" + NL + "               id=\"";
+  protected final String TEXT_294 = ".OpenURIAction\">" + NL + "         </action>" + NL + "         <action" + NL + "               label=\"%openActionLabel\"" + NL + "               definitionId=\"";
+  protected final String TEXT_295 = ".OpenCommand\"" + NL + "               class=\"";
+  protected final String TEXT_296 = "$OpenAction\"" + NL + "               menubarPath=\"file/additions\"" + NL + "               id=\"";
+  protected final String TEXT_297 = ".OpenAction\">" + NL + "         </action>" + NL + "      </actionSet>" + NL + "   </extension>";
+  protected final String TEXT_298 = NL + "</plugin>";
 
   public String generate(Object argument)
   {
@@ -1084,8 +1097,34 @@ for (Iterator ttfIt = typeTabFilter.getAllTypes().iterator(); ttfIt.hasNext();) 
     stringBuffer.append(TEXT_283);
     stringBuffer.append(genPlugin.getID());
     stringBuffer.append(TEXT_284);
-    }
+    stringBuffer.append(genPlugin.getID());
     stringBuffer.append(TEXT_285);
+    stringBuffer.append(genPlugin.getID());
+    stringBuffer.append(TEXT_286);
+    stringBuffer.append(genPlugin.getID());
+    stringBuffer.append(TEXT_287);
+    stringBuffer.append(genPlugin.getID());
+    stringBuffer.append(TEXT_288);
+    stringBuffer.append(genPlugin.getID());
+    stringBuffer.append(TEXT_289);
+    stringBuffer.append(editorGen.getApplication().getActionBarAdvisorQualifiedClassName());
+    stringBuffer.append(TEXT_290);
+    stringBuffer.append(genPlugin.getID());
+    stringBuffer.append(TEXT_291);
+    stringBuffer.append(genPlugin.getID());
+    stringBuffer.append(TEXT_292);
+    stringBuffer.append(editorGen.getApplication().getActionBarAdvisorQualifiedClassName());
+    stringBuffer.append(TEXT_293);
+    stringBuffer.append(genPlugin.getID());
+    stringBuffer.append(TEXT_294);
+    stringBuffer.append(genPlugin.getID());
+    stringBuffer.append(TEXT_295);
+    stringBuffer.append(editorGen.getApplication().getActionBarAdvisorQualifiedClassName());
+    stringBuffer.append(TEXT_296);
+    stringBuffer.append(genPlugin.getID());
+    stringBuffer.append(TEXT_297);
+    }
+    stringBuffer.append(TEXT_298);
     return stringBuffer.toString();
   }
 }
