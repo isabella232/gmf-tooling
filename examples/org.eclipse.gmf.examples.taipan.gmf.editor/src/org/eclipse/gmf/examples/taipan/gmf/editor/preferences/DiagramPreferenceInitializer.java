d6b087fe858638b09fe2125f5fe2fe66afdc49c6
dstadnik <dstadnik>
2006-12-28 18:04:33 +0000

    regenerated

--- examples/org.eclipse.gmf.examples.taipan.gmf.editor/src/org/eclipse/gmf/examples/taipan/gmf/editor/preferences/DiagramPreferenceInitializer.java
+++ examples/org.eclipse.gmf.examples.taipan.gmf.editor/src/org/eclipse/gmf/examples/taipan/gmf/editor/preferences/DiagramPreferenceInitializer.java
@@ -25,10 +25,15 @@ public class DiagramPreferenceInitializer extends AbstractPreferenceInitializer 
 	 */
 	public void initializeDefaultPreferences() {
 		IPreferenceStore store = getPreferenceStore();
+
 		DiagramGeneralPreferencePage.initDefaults(store);
+
 		DiagramAppearancePreferencePage.initDefaults(store);
+
 		DiagramConnectionsPreferencePage.initDefaults(store);
+
 		DiagramPrintingPreferencePage.initDefaults(store);
+
 		DiagramRulersAndGridPreferencePage.initDefaults(store);
 	}
 
