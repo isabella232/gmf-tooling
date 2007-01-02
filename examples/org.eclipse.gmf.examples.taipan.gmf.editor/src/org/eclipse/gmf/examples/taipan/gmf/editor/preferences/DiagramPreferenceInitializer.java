8697fee6f8de4fe39e10869d401d206c7d55523f
dstadnik <dstadnik>
2007-01-02 18:15:39 +0000

    regenerated

--- examples/org.eclipse.gmf.examples.taipan.gmf.editor/src/org/eclipse/gmf/examples/taipan/gmf/editor/preferences/DiagramPreferenceInitializer.java
+++ examples/org.eclipse.gmf.examples.taipan.gmf.editor/src/org/eclipse/gmf/examples/taipan/gmf/editor/preferences/DiagramPreferenceInitializer.java
@@ -13,7 +13,11 @@ package org.eclipse.gmf.examples.taipan.gmf.editor.preferences;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.gmf.examples.taipan.gmf.editor.part.TaiPanDiagramEditorPlugin;
+import org.eclipse.gmf.runtime.diagram.ui.figures.DiagramColorConstants;
+import org.eclipse.gmf.runtime.diagram.ui.preferences.IPreferenceConstants;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.PreferenceConverter;
+import org.eclipse.swt.graphics.FontData;
 
 /**
  * @generated
@@ -26,9 +30,15 @@ public class DiagramPreferenceInitializer extends AbstractPreferenceInitializer 
 	public void initializeDefaultPreferences() {
 		IPreferenceStore store = getPreferenceStore();
 
-		DiagramGeneralPreferencePage.initDefaults(store);
+		PreferenceConverter.setDefault(store, IPreferenceConstants.PREF_DEFAULT_FONT, new FontData("monospaced", 12, org.eclipse.swt.SWT.NORMAL));
 
-		DiagramAppearancePreferencePage.initDefaults(store);
+		PreferenceConverter.setDefault(store, IPreferenceConstants.PREF_FONT_COLOR, DiagramColorConstants.black.getRGB());
+
+		PreferenceConverter.setDefault(store, IPreferenceConstants.PREF_FILL_COLOR, DiagramColorConstants.white.getRGB());
+
+		PreferenceConverter.setDefault(store, IPreferenceConstants.PREF_LINE_COLOR, new org.eclipse.swt.graphics.RGB(64, 64, 64));
+
+		DiagramGeneralPreferencePage.initDefaults(store);
 
 		DiagramConnectionsPreferencePage.initDefaults(store);
 
