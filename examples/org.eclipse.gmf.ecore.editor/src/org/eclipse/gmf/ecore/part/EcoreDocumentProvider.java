d6e2c1b52be7eb7d44966aee7cff0f335a36a931
ashatalin <ashatalin>
2007-01-02 16:46:36 +0000

    [169303] - DocumentProvider.computeModificationStamp fails for plugin resources

--- examples/org.eclipse.gmf.ecore.editor/src/org/eclipse/gmf/ecore/part/EcoreDocumentProvider.java
+++ examples/org.eclipse.gmf.ecore.editor/src/org/eclipse/gmf/ecore/part/EcoreDocumentProvider.java
@@ -132,9 +132,9 @@ public class EcoreDocumentProvider extends StorageDocumentProvider implements ID
 			IFile file = WorkspaceSynchronizer.getFile(nextResource);
 			if (file != null) {
 				if (file.getLocation() != null) {
-					result += file.getModificationStamp();
-				} else {
 					result += file.getLocation().toFile().lastModified();
+				} else {
+					result += file.getModificationStamp();
 				}
 			}
 		}
