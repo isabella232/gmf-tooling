1b51b2a11097ae114ca6209475760d40454e5af6
bblajer <bblajer>
2007-01-12 21:51:55 +0000

    Tests for [170340]: Expand collapsed compartments automatically when new elements are added to it; Test for [170248] compartments collapsibility have moved to a class with related tests

--- tests/org.eclipse.gmf.tests.lite/src/org/eclipse/gmf/tests/lite/gef/DiagramNodeCloneMoveTest.java
+++ tests/org.eclipse.gmf.tests.lite/src/org/eclipse/gmf/tests/lite/gef/DiagramNodeCloneMoveTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2006 Borland Software Corporation
+ * Copyright (c) 2006, 2007 Borland Software Corporation
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -28,6 +28,7 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.command.AddCommand;
+import org.eclipse.emf.edit.command.SetCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.emf.workspace.EMFCommandOperation;
@@ -39,8 +40,10 @@ import org.eclipse.gmf.codegen.gmfgen.GenCompartment;
 import org.eclipse.gmf.codegen.gmfgen.GenLink;
 import org.eclipse.gmf.codegen.gmfgen.GenNode;
 import org.eclipse.gmf.runtime.notation.Diagram;
+import org.eclipse.gmf.runtime.notation.DrawerStyle;
 import org.eclipse.gmf.runtime.notation.Edge;
 import org.eclipse.gmf.runtime.notation.Node;
+import org.eclipse.gmf.runtime.notation.NotationPackage;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.gmf.tests.lite.setup.LibraryConstrainedSetup;
 import org.eclipse.gmf.tests.rt.GeneratedCanvasTest;
@@ -355,5 +358,110 @@ public class DiagramNodeCloneMoveTest extends GeneratedCanvasTest {
 			return;
 		}
 		fail(msg);
+	}
+
+	public void testCompartmentCollapsibility() throws Exception {
+		GenNode nodeB = getSetup().getGenModel().getNodeB();
+		assertTrue("Incorrect Setup: passed node has no compartments", nodeB.getCompartments().size() > 0);
+		GenCompartment genCompartment = (GenCompartment) nodeB.getCompartments().get(0);
+		assertTrue("Incorrect Setup: passed node has no children", genCompartment.getChildNodes().size() > 0);
+
+		GenNode childNode = (GenNode) genCompartment.getChildNodes().get(0);
+
+		Node nodeBInstance = createNode(nodeB, getDiagram());
+		
+		Node nodeBCompartment = (Node) findChildView(nodeBInstance, genCompartment);
+		assertNotNull("Failed to find the compartment", nodeBCompartment);
+		DrawerStyle drawerStyle = (DrawerStyle) nodeBCompartment.getStyle(NotationPackage.eINSTANCE.getDrawerStyle());
+		assertNotNull("Drawer style not added automatically", drawerStyle);
+		assertFalse("Compartment should be expanded by default", drawerStyle.isCollapsed());
+
+		Node level1Child = createNode(childNode, nodeBCompartment);
+		assertNotNull("Child not created", level1Child);
+		assertTrue(nodeBCompartment.getChildren().contains(level1Child));
+
+		EditPart compartmentEP = findEditPart(nodeBCompartment);
+		assertNotNull("Edit part for compartment is missing", compartmentEP);
+		EditPart childEP = findEditPart(level1Child);
+		assertNotNull("Edit part for child is missing", childEP);
+		assertTrue("Edit part for child is inactive", childEP.isActive());
+		assertSame("Unexpected parent of the child edit part", compartmentEP, childEP.getParent());
+
+		TransactionalEditingDomain domain = TransactionUtil.getEditingDomain(drawerStyle);
+		org.eclipse.emf.common.command.Command command = SetCommand.create(domain, drawerStyle, NotationPackage.eINSTANCE.getDrawerStyle_Collapsed(), Boolean.TRUE);
+		assertTrue("Failed to obtain command to collapse the compartment", command != null && command.canExecute());
+		new EMFCommandOperation(domain, command).execute(new NullProgressMonitor(), null);
+		assertTrue("Compartment failed to collapse", drawerStyle.isCollapsed());
+		assertNotNull("Collapsing compartment should not have removed the view", level1Child.eResource());
+		assertFalse("Collapsing compartment should have removed the child edit part", childEP.isActive());
+		childEP = findEditPart(level1Child);
+		assertNull("Collapsing compartment should have made the child edit part go away", childEP);
+
+		command = SetCommand.create(domain, drawerStyle, NotationPackage.eINSTANCE.getDrawerStyle_Collapsed(), Boolean.FALSE);
+		assertTrue("Failed to obtain command to expand the compartment", command != null && command.canExecute());
+		new EMFCommandOperation(domain, command).execute(new NullProgressMonitor(), null);
+		assertFalse("Compartment failed to expand", drawerStyle.isCollapsed());
+		assertNotNull("Expanding compartment should not have done anything to the view", level1Child.eResource());
+		childEP = findEditPart(level1Child);
+		assertTrue("Expanding compartment should have made the child edit part to reappear", childEP.isActive());
+	}
+
+	public void testCompartmentExpandsOnAddingToIt() throws Exception {
+		GenNode nodeB = getSetup().getGenModel().getNodeB();
+		assertTrue("Incorrect Setup: passed node has no compartments", nodeB.getCompartments().size() > 0);
+		GenCompartment genCompartment = (GenCompartment) nodeB.getCompartments().get(0);
+		assertTrue("Incorrect Setup: passed node has no children", genCompartment.getChildNodes().size() > 0);
+
+		GenNode childNode = (GenNode) genCompartment.getChildNodes().get(0);
+
+		Node nodeBInstance = createNode(nodeB, getDiagram());
+		
+		Node nodeBCompartment = (Node) findChildView(nodeBInstance, genCompartment);
+		assertNotNull("Failed to find the compartment", nodeBCompartment);
+		DrawerStyle drawerStyle = (DrawerStyle) nodeBCompartment.getStyle(NotationPackage.eINSTANCE.getDrawerStyle());
+		assertNotNull("Drawer style not added automatically", drawerStyle);
+		TransactionalEditingDomain domain = TransactionUtil.getEditingDomain(drawerStyle);
+
+		//Collapse the compartment
+		org.eclipse.emf.common.command.Command command = SetCommand.create(domain, drawerStyle, NotationPackage.eINSTANCE.getDrawerStyle_Collapsed(), Boolean.TRUE);
+		assertTrue("Failed to obtain command to collapse the compartment", command != null && command.canExecute());
+		new EMFCommandOperation(domain, command).execute(new NullProgressMonitor(), null);
+		assertTrue("Compartment failed to collapse", drawerStyle.isCollapsed());
+
+		//1. Create a child, check that the compartment has expanded in response.
+		Node child = createNode(childNode, nodeBCompartment);
+		assertNotNull("Child not created", child);
+		assertTrue(nodeBCompartment.getChildren().contains(child));
+		assertFalse("Compartment failed to expand automatically on adding a new child", drawerStyle.isCollapsed());
+
+		Node nodeBClone = cloneOrMoveNode(nodeBInstance, getDiagram(), true);
+		assertNotNull("Failed to clone node", nodeBClone);
+		View compartmentClone = findChildView(nodeBClone, genCompartment);
+		assertNotNull("Cloning node failed to clone its child compartment", compartmentClone);
+		Node childClone = (Node) findChildView(compartmentClone, childNode);
+
+		//Collapse the compartment again.
+		command = SetCommand.create(domain, drawerStyle, NotationPackage.eINSTANCE.getDrawerStyle_Collapsed(), Boolean.TRUE);
+		assertTrue("Failed to obtain command to collapse the compartment", command != null && command.canExecute());
+		new EMFCommandOperation(domain, command).execute(new NullProgressMonitor(), null);
+		assertTrue("Compartment failed to collapse", drawerStyle.isCollapsed());
+
+		//2. Clone a node onto the compartment, check the compartment has expanded in response.
+		Node anotherChild = cloneOrMoveNode(childClone, nodeBCompartment, true);
+		assertNotNull("Clone not created", anotherChild);
+		assertTrue(nodeBCompartment.getChildren().contains(anotherChild));
+		assertFalse("Compartment failed to expand automatically on adding a new child", drawerStyle.isCollapsed());
+
+		//Collapse the compartment again.
+		command = SetCommand.create(domain, drawerStyle, NotationPackage.eINSTANCE.getDrawerStyle_Collapsed(), Boolean.TRUE);
+		assertTrue("Failed to obtain command to collapse the compartment", command != null && command.canExecute());
+		new EMFCommandOperation(domain, command).execute(new NullProgressMonitor(), null);
+		assertTrue("Compartment failed to collapse", drawerStyle.isCollapsed());
+
+		//3. Move a node onto the compartment, check the compartment has expanded in response.
+		anotherChild = cloneOrMoveNode(childClone, nodeBCompartment, false);
+		assertNotNull("Clone not created", anotherChild);
+		assertTrue(nodeBCompartment.getChildren().contains(anotherChild));
+		assertFalse("Compartment failed to expand automatically on adding a new child", drawerStyle.isCollapsed());
 	}
 }
