68565bad82ed7f245276c70b24f3c619db15dd1f
mfeldman <mfeldman>
2007-02-21 11:20:05 +0000

    Migrate from startup.jar to Equinox launcher

--- releng/org.eclipse.gmf.releng.builder/scripts/build.sh
+++ releng/org.eclipse.gmf.releng.builder/scripts/build.sh
@@ -1,3 +1,3 @@
 DISPLAY=:0.0
 export DISPLAY
-java -Xmx1024m -cp ../../org.eclipse.releng.basebuilder/startup.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner "$@"
+java -Xmx1024m -cp ../../org.eclipse.releng.basebuilder/plugins/org.eclipse.equinox.launcher.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner "$@"
