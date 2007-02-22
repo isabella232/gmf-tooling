c90ff27aaba4cab9415f076e719e06d0841c6586
ahunter <ahunter>
2007-02-22 21:48:00 +0000

    gmf_head ahunter 070222 EclipseCon 2007 Library Example

--- examples/org.eclipse.gmf.examples.eclipsecon.library/src/org/eclipse/gmf/examples/eclipsecon/library/util/LibraryAdapterFactory.java
+++ examples/org.eclipse.gmf.examples.eclipsecon.library/src/org/eclipse/gmf/examples/eclipsecon/library/util/LibraryAdapterFactory.java
@@ -2,7 +2,7 @@
  * <copyright>
  * </copyright>
  *
- * $Id: LibraryAdapterFactory.java,v 1.1 2007/02/20 01:36:47 ahunter Exp $
+ * $Id: LibraryAdapterFactory.java,v 1.2 2007/02/22 21:48:00 ahunter Exp $
  */
 package org.eclipse.gmf.examples.eclipsecon.library.util;
 
@@ -25,164 +25,164 @@ import org.eclipse.gmf.examples.eclipsecon.library.*;
  */
 public class LibraryAdapterFactory extends AdapterFactoryImpl {
 	/**
-	 * The cached model package.
-	 * <!-- begin-user-doc -->
+     * The cached model package.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	protected static LibraryPackage modelPackage;
 
 	/**
-	 * Creates an instance of the adapter factory.
-	 * <!-- begin-user-doc -->
+     * Creates an instance of the adapter factory.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public LibraryAdapterFactory() {
-		if (modelPackage == null) {
-			modelPackage = LibraryPackage.eINSTANCE;
-		}
-	}
+        if (modelPackage == null) {
+            modelPackage = LibraryPackage.eINSTANCE;
+        }
+    }
 
 	/**
-	 * Returns whether this factory is applicable for the type of the object.
-	 * <!-- begin-user-doc -->
+     * Returns whether this factory is applicable for the type of the object.
+     * <!-- begin-user-doc -->
 	 * This implementation returns <code>true</code> if the object is either the model's package or is an instance object of the model.
 	 * <!-- end-user-doc -->
-	 * @return whether this factory is applicable for the type of the object.
-	 * @generated
-	 */
+     * @return whether this factory is applicable for the type of the object.
+     * @generated
+     */
 	public boolean isFactoryForType(Object object) {
-		if (object == modelPackage) {
-			return true;
-		}
-		if (object instanceof EObject) {
-			return ((EObject)object).eClass().getEPackage() == modelPackage;
-		}
-		return false;
-	}
+        if (object == modelPackage) {
+            return true;
+        }
+        if (object instanceof EObject) {
+            return ((EObject)object).eClass().getEPackage() == modelPackage;
+        }
+        return false;
+    }
 
 	/**
-	 * The switch the delegates to the <code>createXXX</code> methods.
-	 * <!-- begin-user-doc -->
+     * The switch the delegates to the <code>createXXX</code> methods.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	protected LibrarySwitch modelSwitch =
 		new LibrarySwitch() {
-			public Object caseAuthor(Author object) {
-				return createAuthorAdapter();
-			}
-			public Object caseBook(Book object) {
-				return createBookAdapter();
-			}
-			public Object caseEmployee(Employee object) {
-				return createEmployeeAdapter();
-			}
-			public Object caseLibrary(Library object) {
-				return createLibraryAdapter();
-			}
-			public Object caseShelf(Shelf object) {
-				return createShelfAdapter();
-			}
-			public Object defaultCase(EObject object) {
-				return createEObjectAdapter();
-			}
-		};
+            public Object caseAuthor(Author object) {
+                return createAuthorAdapter();
+            }
+            public Object caseBook(Book object) {
+                return createBookAdapter();
+            }
+            public Object caseEmployee(Employee object) {
+                return createEmployeeAdapter();
+            }
+            public Object caseLibrary(Library object) {
+                return createLibraryAdapter();
+            }
+            public Object caseShelf(Shelf object) {
+                return createShelfAdapter();
+            }
+            public Object defaultCase(EObject object) {
+                return createEObjectAdapter();
+            }
+        };
 
 	/**
-	 * Creates an adapter for the <code>target</code>.
-	 * <!-- begin-user-doc -->
+     * Creates an adapter for the <code>target</code>.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @param target the object to adapt.
-	 * @return the adapter for the <code>target</code>.
-	 * @generated
-	 */
+     * @param target the object to adapt.
+     * @return the adapter for the <code>target</code>.
+     * @generated
+     */
 	public Adapter createAdapter(Notifier target) {
-		return (Adapter)modelSwitch.doSwitch((EObject)target);
-	}
+        return (Adapter)modelSwitch.doSwitch((EObject)target);
+    }
 
 
 	/**
-	 * Creates a new adapter for an object of class '{@link org.eclipse.gmf.examples.eclipsecon.library.Author <em>Author</em>}'.
-	 * <!-- begin-user-doc -->
+     * Creates a new adapter for an object of class '{@link org.eclipse.gmf.examples.eclipsecon.library.Author <em>Author</em>}'.
+     * <!-- begin-user-doc -->
 	 * This default implementation returns null so that we can easily ignore cases;
 	 * it's useful to ignore a case when inheritance will catch all the cases anyway.
 	 * <!-- end-user-doc -->
-	 * @return the new adapter.
-	 * @see org.eclipse.gmf.examples.eclipsecon.library.Author
-	 * @generated
-	 */
+     * @return the new adapter.
+     * @see org.eclipse.gmf.examples.eclipsecon.library.Author
+     * @generated
+     */
 	public Adapter createAuthorAdapter() {
-		return null;
-	}
+        return null;
+    }
 
 	/**
-	 * Creates a new adapter for an object of class '{@link org.eclipse.gmf.examples.eclipsecon.library.Book <em>Book</em>}'.
-	 * <!-- begin-user-doc -->
+     * Creates a new adapter for an object of class '{@link org.eclipse.gmf.examples.eclipsecon.library.Book <em>Book</em>}'.
+     * <!-- begin-user-doc -->
 	 * This default implementation returns null so that we can easily ignore cases;
 	 * it's useful to ignore a case when inheritance will catch all the cases anyway.
 	 * <!-- end-user-doc -->
-	 * @return the new adapter.
-	 * @see org.eclipse.gmf.examples.eclipsecon.library.Book
-	 * @generated
-	 */
+     * @return the new adapter.
+     * @see org.eclipse.gmf.examples.eclipsecon.library.Book
+     * @generated
+     */
 	public Adapter createBookAdapter() {
-		return null;
-	}
+        return null;
+    }
 
 	/**
-	 * Creates a new adapter for an object of class '{@link org.eclipse.gmf.examples.eclipsecon.library.Employee <em>Employee</em>}'.
-	 * <!-- begin-user-doc -->
+     * Creates a new adapter for an object of class '{@link org.eclipse.gmf.examples.eclipsecon.library.Employee <em>Employee</em>}'.
+     * <!-- begin-user-doc -->
 	 * This default implementation returns null so that we can easily ignore cases;
 	 * it's useful to ignore a case when inheritance will catch all the cases anyway.
 	 * <!-- end-user-doc -->
-	 * @return the new adapter.
-	 * @see org.eclipse.gmf.examples.eclipsecon.library.Employee
-	 * @generated
-	 */
+     * @return the new adapter.
+     * @see org.eclipse.gmf.examples.eclipsecon.library.Employee
+     * @generated
+     */
 	public Adapter createEmployeeAdapter() {
-		return null;
-	}
+        return null;
+    }
 
 	/**
-	 * Creates a new adapter for an object of class '{@link org.eclipse.gmf.examples.eclipsecon.library.Library <em>Library</em>}'.
-	 * <!-- begin-user-doc -->
+     * Creates a new adapter for an object of class '{@link org.eclipse.gmf.examples.eclipsecon.library.Library <em>Library</em>}'.
+     * <!-- begin-user-doc -->
 	 * This default implementation returns null so that we can easily ignore cases;
 	 * it's useful to ignore a case when inheritance will catch all the cases anyway.
 	 * <!-- end-user-doc -->
-	 * @return the new adapter.
-	 * @see org.eclipse.gmf.examples.eclipsecon.library.Library
-	 * @generated
-	 */
+     * @return the new adapter.
+     * @see org.eclipse.gmf.examples.eclipsecon.library.Library
+     * @generated
+     */
 	public Adapter createLibraryAdapter() {
-		return null;
-	}
+        return null;
+    }
 
 	/**
-	 * Creates a new adapter for an object of class '{@link org.eclipse.gmf.examples.eclipsecon.library.Shelf <em>Shelf</em>}'.
-	 * <!-- begin-user-doc -->
+     * Creates a new adapter for an object of class '{@link org.eclipse.gmf.examples.eclipsecon.library.Shelf <em>Shelf</em>}'.
+     * <!-- begin-user-doc -->
 	 * This default implementation returns null so that we can easily ignore cases;
 	 * it's useful to ignore a case when inheritance will catch all the cases anyway.
 	 * <!-- end-user-doc -->
-	 * @return the new adapter.
-	 * @see org.eclipse.gmf.examples.eclipsecon.library.Shelf
-	 * @generated
-	 */
+     * @return the new adapter.
+     * @see org.eclipse.gmf.examples.eclipsecon.library.Shelf
+     * @generated
+     */
 	public Adapter createShelfAdapter() {
-		return null;
-	}
+        return null;
+    }
 
 	/**
-	 * Creates a new adapter for the default case.
-	 * <!-- begin-user-doc -->
+     * Creates a new adapter for the default case.
+     * <!-- begin-user-doc -->
 	 * This default implementation returns null.
 	 * <!-- end-user-doc -->
-	 * @return the new adapter.
-	 * @generated
-	 */
+     * @return the new adapter.
+     * @generated
+     */
 	public Adapter createEObjectAdapter() {
-		return null;
-	}
+        return null;
+    }
 
 } //LibraryAdapterFactory
--- examples/org.eclipse.gmf.examples.eclipsecon.library/src/org/eclipse/gmf/examples/eclipsecon/library/util/LibrarySwitch.java
+++ examples/org.eclipse.gmf.examples.eclipsecon.library/src/org/eclipse/gmf/examples/eclipsecon/library/util/LibrarySwitch.java
@@ -2,7 +2,7 @@
  * <copyright>
  * </copyright>
  *
- * $Id: LibrarySwitch.java,v 1.1 2007/02/20 01:36:47 ahunter Exp $
+ * $Id: LibrarySwitch.java,v 1.2 2007/02/22 21:48:00 ahunter Exp $
  */
 package org.eclipse.gmf.examples.eclipsecon.library.util;
 
@@ -28,187 +28,187 @@ import org.eclipse.gmf.examples.eclipsecon.library.*;
  */
 public class LibrarySwitch {
 	/**
-	 * The cached model package
-	 * <!-- begin-user-doc -->
+     * The cached model package
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	protected static LibraryPackage modelPackage;
 
 	/**
-	 * Creates an instance of the switch.
-	 * <!-- begin-user-doc -->
+     * Creates an instance of the switch.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public LibrarySwitch() {
-		if (modelPackage == null) {
-			modelPackage = LibraryPackage.eINSTANCE;
-		}
-	}
+        if (modelPackage == null) {
+            modelPackage = LibraryPackage.eINSTANCE;
+        }
+    }
 
 	/**
-	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
-	 * <!-- begin-user-doc -->
+     * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @return the first non-null result returned by a <code>caseXXX</code> call.
-	 * @generated
-	 */
+     * @return the first non-null result returned by a <code>caseXXX</code> call.
+     * @generated
+     */
 	public Object doSwitch(EObject theEObject) {
-		return doSwitch(theEObject.eClass(), theEObject);
-	}
+        return doSwitch(theEObject.eClass(), theEObject);
+    }
 
 	/**
-	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
-	 * <!-- begin-user-doc -->
+     * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @return the first non-null result returned by a <code>caseXXX</code> call.
-	 * @generated
-	 */
+     * @return the first non-null result returned by a <code>caseXXX</code> call.
+     * @generated
+     */
 	protected Object doSwitch(EClass theEClass, EObject theEObject) {
-		if (theEClass.eContainer() == modelPackage) {
-			return doSwitch(theEClass.getClassifierID(), theEObject);
-		}
-		else {
-			List eSuperTypes = theEClass.getESuperTypes();
-			return
-				eSuperTypes.isEmpty() ?
-					defaultCase(theEObject) :
-					doSwitch((EClass)eSuperTypes.get(0), theEObject);
-		}
-	}
+        if (theEClass.eContainer() == modelPackage) {
+            return doSwitch(theEClass.getClassifierID(), theEObject);
+        }
+        else {
+            List eSuperTypes = theEClass.getESuperTypes();
+            return
+                eSuperTypes.isEmpty() ?
+                    defaultCase(theEObject) :
+                    doSwitch((EClass)eSuperTypes.get(0), theEObject);
+        }
+    }
 
 	/**
-	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
-	 * <!-- begin-user-doc -->
+     * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @return the first non-null result returned by a <code>caseXXX</code> call.
-	 * @generated
-	 */
+     * @return the first non-null result returned by a <code>caseXXX</code> call.
+     * @generated
+     */
 	protected Object doSwitch(int classifierID, EObject theEObject) {
-		switch (classifierID) {
-			case LibraryPackage.AUTHOR: {
-				Author author = (Author)theEObject;
-				Object result = caseAuthor(author);
-				if (result == null) result = defaultCase(theEObject);
-				return result;
-			}
-			case LibraryPackage.BOOK: {
-				Book book = (Book)theEObject;
-				Object result = caseBook(book);
-				if (result == null) result = defaultCase(theEObject);
-				return result;
-			}
-			case LibraryPackage.EMPLOYEE: {
-				Employee employee = (Employee)theEObject;
-				Object result = caseEmployee(employee);
-				if (result == null) result = defaultCase(theEObject);
-				return result;
-			}
-			case LibraryPackage.LIBRARY: {
-				Library library = (Library)theEObject;
-				Object result = caseLibrary(library);
-				if (result == null) result = defaultCase(theEObject);
-				return result;
-			}
-			case LibraryPackage.SHELF: {
-				Shelf shelf = (Shelf)theEObject;
-				Object result = caseShelf(shelf);
-				if (result == null) result = defaultCase(theEObject);
-				return result;
-			}
-			default: return defaultCase(theEObject);
-		}
-	}
+        switch (classifierID) {
+            case LibraryPackage.AUTHOR: {
+                Author author = (Author)theEObject;
+                Object result = caseAuthor(author);
+                if (result == null) result = defaultCase(theEObject);
+                return result;
+            }
+            case LibraryPackage.BOOK: {
+                Book book = (Book)theEObject;
+                Object result = caseBook(book);
+                if (result == null) result = defaultCase(theEObject);
+                return result;
+            }
+            case LibraryPackage.EMPLOYEE: {
+                Employee employee = (Employee)theEObject;
+                Object result = caseEmployee(employee);
+                if (result == null) result = defaultCase(theEObject);
+                return result;
+            }
+            case LibraryPackage.LIBRARY: {
+                Library library = (Library)theEObject;
+                Object result = caseLibrary(library);
+                if (result == null) result = defaultCase(theEObject);
+                return result;
+            }
+            case LibraryPackage.SHELF: {
+                Shelf shelf = (Shelf)theEObject;
+                Object result = caseShelf(shelf);
+                if (result == null) result = defaultCase(theEObject);
+                return result;
+            }
+            default: return defaultCase(theEObject);
+        }
+    }
 
 	/**
-	 * Returns the result of interpretting the object as an instance of '<em>Author</em>'.
-	 * <!-- begin-user-doc -->
+     * Returns the result of interpretting the object as an instance of '<em>Author</em>'.
+     * <!-- begin-user-doc -->
 	 * This implementation returns null;
 	 * returning a non-null result will terminate the switch.
 	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
-	 * @return the result of interpretting the object as an instance of '<em>Author</em>'.
-	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
-	 * @generated
-	 */
+     * @param object the target of the switch.
+     * @return the result of interpretting the object as an instance of '<em>Author</em>'.
+     * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+     * @generated
+     */
 	public Object caseAuthor(Author object) {
-		return null;
-	}
+        return null;
+    }
 
 	/**
-	 * Returns the result of interpretting the object as an instance of '<em>Book</em>'.
-	 * <!-- begin-user-doc -->
+     * Returns the result of interpretting the object as an instance of '<em>Book</em>'.
+     * <!-- begin-user-doc -->
 	 * This implementation returns null;
 	 * returning a non-null result will terminate the switch.
 	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
-	 * @return the result of interpretting the object as an instance of '<em>Book</em>'.
-	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
-	 * @generated
-	 */
+     * @param object the target of the switch.
+     * @return the result of interpretting the object as an instance of '<em>Book</em>'.
+     * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+     * @generated
+     */
 	public Object caseBook(Book object) {
-		return null;
-	}
+        return null;
+    }
 
 	/**
-	 * Returns the result of interpretting the object as an instance of '<em>Employee</em>'.
-	 * <!-- begin-user-doc -->
+     * Returns the result of interpretting the object as an instance of '<em>Employee</em>'.
+     * <!-- begin-user-doc -->
 	 * This implementation returns null;
 	 * returning a non-null result will terminate the switch.
 	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
-	 * @return the result of interpretting the object as an instance of '<em>Employee</em>'.
-	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
-	 * @generated
-	 */
+     * @param object the target of the switch.
+     * @return the result of interpretting the object as an instance of '<em>Employee</em>'.
+     * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+     * @generated
+     */
 	public Object caseEmployee(Employee object) {
-		return null;
-	}
+        return null;
+    }
 
 	/**
-	 * Returns the result of interpretting the object as an instance of '<em>Library</em>'.
-	 * <!-- begin-user-doc -->
+     * Returns the result of interpretting the object as an instance of '<em>Library</em>'.
+     * <!-- begin-user-doc -->
 	 * This implementation returns null;
 	 * returning a non-null result will terminate the switch.
 	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
-	 * @return the result of interpretting the object as an instance of '<em>Library</em>'.
-	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
-	 * @generated
-	 */
+     * @param object the target of the switch.
+     * @return the result of interpretting the object as an instance of '<em>Library</em>'.
+     * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+     * @generated
+     */
 	public Object caseLibrary(Library object) {
-		return null;
-	}
+        return null;
+    }
 
 	/**
-	 * Returns the result of interpretting the object as an instance of '<em>Shelf</em>'.
-	 * <!-- begin-user-doc -->
+     * Returns the result of interpretting the object as an instance of '<em>Shelf</em>'.
+     * <!-- begin-user-doc -->
 	 * This implementation returns null;
 	 * returning a non-null result will terminate the switch.
 	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
-	 * @return the result of interpretting the object as an instance of '<em>Shelf</em>'.
-	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
-	 * @generated
-	 */
+     * @param object the target of the switch.
+     * @return the result of interpretting the object as an instance of '<em>Shelf</em>'.
+     * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+     * @generated
+     */
 	public Object caseShelf(Shelf object) {
-		return null;
-	}
+        return null;
+    }
 
 	/**
-	 * Returns the result of interpretting the object as an instance of '<em>EObject</em>'.
-	 * <!-- begin-user-doc -->
+     * Returns the result of interpretting the object as an instance of '<em>EObject</em>'.
+     * <!-- begin-user-doc -->
 	 * This implementation returns null;
 	 * returning a non-null result will terminate the switch, but this is the last case anyway.
 	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
-	 * @return the result of interpretting the object as an instance of '<em>EObject</em>'.
-	 * @see #doSwitch(org.eclipse.emf.ecore.EObject)
-	 * @generated
-	 */
+     * @param object the target of the switch.
+     * @return the result of interpretting the object as an instance of '<em>EObject</em>'.
+     * @see #doSwitch(org.eclipse.emf.ecore.EObject)
+     * @generated
+     */
 	public Object defaultCase(EObject object) {
-		return null;
-	}
+        return null;
+    }
 
 } //LibrarySwitch
