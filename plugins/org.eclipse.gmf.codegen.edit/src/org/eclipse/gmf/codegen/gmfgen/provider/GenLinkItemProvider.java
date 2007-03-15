085d51d3af8220683aee4598d6e8793a3c0b50c3
dstadnik <dstadnik>
2007-03-15 16:11:39 +0000

    [114348] Generate command to reorient link

--- plugins/org.eclipse.gmf.codegen.edit/src/org/eclipse/gmf/codegen/gmfgen/provider/GenLinkItemProvider.java
+++ plugins/org.eclipse.gmf.codegen.edit/src/org/eclipse/gmf/codegen/gmfgen/provider/GenLinkItemProvider.java
@@ -67,6 +67,7 @@ public class GenLinkItemProvider
 			addOutgoingCreationAllowedPropertyDescriptor(object);
 			addIncomingCreationAllowedPropertyDescriptor(object);
 			addViewDirectionAlignedWithModelPropertyDescriptor(object);
+			addReorientCommandClassNamePropertyDescriptor(object);
 		}
 		return itemPropertyDescriptors;
 	}
@@ -134,6 +135,28 @@ public class GenLinkItemProvider
 				 false,
 				 ItemPropertyDescriptor.BOOLEAN_VALUE_IMAGE,
 				 getString("_UI_DiagramLinkPropertyCategory"),
+				 null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Reorient Command Class Name feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void addReorientCommandClassNamePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_GenLink_reorientCommandClassName_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_GenLink_reorientCommandClassName_feature", "_UI_GenLink_type"),
+				 GMFGenPackage.eINSTANCE.getGenLink_ReorientCommandClassName(),
+				 true,
+				 false,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
 				 null));
 	}
 
@@ -209,6 +232,7 @@ public class GenLinkItemProvider
 			case GMFGenPackage.GEN_LINK__OUTGOING_CREATION_ALLOWED:
 			case GMFGenPackage.GEN_LINK__INCOMING_CREATION_ALLOWED:
 			case GMFGenPackage.GEN_LINK__VIEW_DIRECTION_ALIGNED_WITH_MODEL:
+			case GMFGenPackage.GEN_LINK__REORIENT_COMMAND_CLASS_NAME:
 				fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
 				return;
 			case GMFGenPackage.GEN_LINK__MODEL_FACET:
