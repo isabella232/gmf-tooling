b829052b9af180f7a5acc243da9ff02c3d2704f1
dstadnik <dstadnik>
2007-03-16 16:13:33 +0000

    regenerated

--- examples/org.eclipse.gmf.examples.taipan.gmf.editor.rcp/src/org/eclipse/gmf/examples/taipan/gmf/editor/part/TaiPanInitDiagramFileAction.java
+++ examples/org.eclipse.gmf.examples.taipan.gmf.editor.rcp/src/org/eclipse/gmf/examples/taipan/gmf/editor/part/TaiPanInitDiagramFileAction.java
@@ -11,12 +11,22 @@
  */
 package org.eclipse.gmf.examples.taipan.gmf.editor.part;
 
+import java.io.File;
+import org.eclipse.emf.common.util.WrappedException;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.gmf.examples.taipan.gmf.editor.edit.parts.AquatoryEditPart;
+import org.eclipse.gmf.runtime.emf.core.GMFEditingDomainFactory;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
@@ -62,7 +72,30 @@ public class TaiPanInitDiagramFileAction implements IWorkbenchWindowActionDelega
 	 * @generated
 	 */
 	public void run(IAction action) {
-		Wizard wizard = new TaiPanNewDiagramFileWizard();
+		FileDialog fileDialog = new FileDialog(getShell(), SWT.OPEN);
+		fileDialog.open();
+		String fileName = fileDialog.getFileName();
+		if (fileName == null || fileName.length() == 0) {
+			return;
+		}
+		if (fileDialog.getFilterPath() != null) {
+			fileName = fileDialog.getFilterPath() + File.separator + fileName;
+		}
+		org.eclipse.emf.common.util.URI domainModelURI = org.eclipse.emf.common.util.URI.createFileURI(fileName);
+		TransactionalEditingDomain editingDomain = GMFEditingDomainFactory.INSTANCE.createEditingDomain();
+		ResourceSet resourceSet = editingDomain.getResourceSet();
+		EObject diagramRoot = null;
+		try {
+			Resource resource = resourceSet.getResource(domainModelURI, true);
+			diagramRoot = (EObject) resource.getContents().get(0);
+		} catch (WrappedException ex) {
+			TaiPanDiagramEditorPlugin.getInstance().logError("Unable to load resource: " + domainModelURI, ex);
+		}
+		if (diagramRoot == null) {
+			MessageDialog.openError(getShell(), "Error", "Model file loading failed");
+			return;
+		}
+		Wizard wizard = new TaiPanNewDiagramFileWizard(domainModelURI, diagramRoot, editingDomain);
 		IDialogSettings pluginDialogSettings = TaiPanDiagramEditorPlugin.getInstance().getDialogSettings();
 		IDialogSettings initDiagramFileSettings = pluginDialogSettings.getSection("InitDiagramFile"); //$NON-NLS-1$
 		if (initDiagramFileSettings == null) {
