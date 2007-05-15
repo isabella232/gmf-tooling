71d792dbbff43dcedbaabb86b90c50890a52b12f
dstadnik <dstadnik>
2007-05-15 10:37:29 +0000

    add prefs

--- examples/org.eclipse.gmf.examples.taipan.port.diagram/src/org/eclipse/gmf/examples/taipan/port/diagram/preferences/DiagramPreferenceInitializer.java
+++ examples/org.eclipse.gmf.examples.taipan.port.diagram/src/org/eclipse/gmf/examples/taipan/port/diagram/preferences/DiagramPreferenceInitializer.java
@@ -12,8 +12,15 @@
 package org.eclipse.gmf.examples.taipan.port.diagram.preferences;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.gef.rulers.RulerProvider;
 import org.eclipse.gmf.examples.taipan.port.diagram.part.PortDiagramEditorPlugin;
+import org.eclipse.gmf.runtime.diagram.ui.figures.DiagramColorConstants;
+import org.eclipse.gmf.runtime.diagram.ui.preferences.IPreferenceConstants;
+import org.eclipse.gmf.runtime.notation.Routing;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.PreferenceConverter;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.FontData;
 
 /**
  * @generated
@@ -26,10 +33,28 @@ public class DiagramPreferenceInitializer extends AbstractPreferenceInitializer 
 	public void initializeDefaultPreferences() {
 		IPreferenceStore store = getPreferenceStore();
 		DiagramPrintingPreferencePage.initDefaults(store);
-		DiagramGeneralPreferencePage.initDefaults(store);
-		DiagramAppearancePreferencePage.initDefaults(store);
-		DiagramConnectionsPreferencePage.initDefaults(store);
-		DiagramRulersAndGridPreferencePage.initDefaults(store);
+
+		store.setDefault(IPreferenceConstants.PREF_SHOW_CONNECTION_HANDLES, true);
+		store.setDefault(IPreferenceConstants.PREF_SHOW_POPUP_BARS, true);
+		store.setDefault(IPreferenceConstants.PREF_ENABLE_ANIMATED_LAYOUT, true);
+		store.setDefault(IPreferenceConstants.PREF_ENABLE_ANIMATED_ZOOM, true);
+		store.setDefault(IPreferenceConstants.PREF_ENABLE_ANTIALIAS, true);
+
+		PreferenceConverter.setDefault(store, IPreferenceConstants.PREF_DEFAULT_FONT, new FontData("Lucida Grande", 12, SWT.NORMAL));
+
+		PreferenceConverter.setDefault(store, IPreferenceConstants.PREF_FONT_COLOR, DiagramColorConstants.darkGray.getRGB());
+
+		PreferenceConverter.setDefault(store, IPreferenceConstants.PREF_FILL_COLOR, DiagramColorConstants.white.getRGB());
+
+		PreferenceConverter.setDefault(store, IPreferenceConstants.PREF_LINE_COLOR, DiagramColorConstants.black.getRGB());
+		store.setDefault(IPreferenceConstants.PREF_LINE_STYLE, Routing.MANUAL);
+		store.setDefault(IPreferenceConstants.PREF_SHOW_RULERS, false);
+		store.setDefault(IPreferenceConstants.PREF_RULER_UNITS, false);
+		store.setDefault(IPreferenceConstants.PREF_SHOW_GRID, RulerProvider.UNIT_INCHES);
+		store.setDefault(IPreferenceConstants.PREF_SNAP_TO_GRID, true);
+		store.setDefault(IPreferenceConstants.PREF_GRID_SPACING, 0.125);
+		store.setDefault(IPreferenceConstants.PREF_PROMPT_ON_DEL_FROM_MODEL, false);
+		store.setDefault(IPreferenceConstants.PREF_PROMPT_ON_DEL_FROM_DIAGRAM, false);
 	}
 
 	/**
