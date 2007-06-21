c3ef54e9f899ac4801e8afd865d77dd1c6593ff0
dstadnik <dstadnik>
2007-06-21 14:06:23 +0000

    regenerated

--- examples/org.eclipse.gmf.ecore.editor/src/org/eclipse/gmf/ecore/edit/parts/EAnnotation2EditPart.java
+++ examples/org.eclipse.gmf.ecore.editor/src/org/eclipse/gmf/ecore/edit/parts/EAnnotation2EditPart.java
@@ -193,8 +193,7 @@ public class EAnnotation2EditPart extends CompartmentEditPart implements ITextAw
 	 * @generated
 	 */
 	protected EObject getParserElement() {
-		EObject element = resolveSemanticElement();
-		return element != null ? element : (View) getModel();
+		return resolveSemanticElement();
 	}
 
 	/**
@@ -213,8 +212,9 @@ public class EAnnotation2EditPart extends CompartmentEditPart implements ITextAw
 	 */
 	protected String getLabelText() {
 		String text = null;
-		if (getParser() != null) {
-			text = getParser().getPrintString(new EObjectAdapter(getParserElement()), getParserOptions().intValue());
+		EObject parserElement = getParserElement();
+		if (parserElement != null && getParser() != null) {
+			text = getParser().getPrintString(new EObjectAdapter(parserElement), getParserOptions().intValue());
 		}
 		if (text == null || text.length() == 0) {
 			text = defaultText;
@@ -237,7 +237,7 @@ public class EAnnotation2EditPart extends CompartmentEditPart implements ITextAw
 	 * @generated
 	 */
 	public String getEditText() {
-		if (getParser() == null) {
+		if (getParserElement() == null || getParser() == null) {
 			return ""; //$NON-NLS-1$
 		}
 		return getParser().getEditString(new EObjectAdapter(getParserElement()), getParserOptions().intValue());
@@ -283,7 +283,7 @@ public class EAnnotation2EditPart extends CompartmentEditPart implements ITextAw
 	 * @generated
 	 */
 	public IContentAssistProcessor getCompletionProcessor() {
-		if (getParser() == null) {
+		if (getParserElement() == null || getParser() == null) {
 			return null;
 		}
 		return getParser().getCompletionProcessor(new EObjectAdapter(getParserElement()));
--- examples/org.eclipse.gmf.ecore.editor/src/org/eclipse/gmf/ecore/edit/parts/EReferenceLowerBoundUpperBoundEditPart.java
+++ examples/org.eclipse.gmf.ecore.editor/src/org/eclipse/gmf/ecore/edit/parts/EReferenceLowerBoundUpperBoundEditPart.java
@@ -189,8 +189,7 @@ public class EReferenceLowerBoundUpperBoundEditPart extends LabelEditPart implem
 	 * @generated
 	 */
 	protected EObject getParserElement() {
-		EObject element = resolveSemanticElement();
-		return element != null ? element : (View) getModel();
+		return resolveSemanticElement();
 	}
 
 	/**
@@ -205,8 +204,9 @@ public class EReferenceLowerBoundUpperBoundEditPart extends LabelEditPart implem
 	 */
 	protected String getLabelText() {
 		String text = null;
-		if (getParser() != null) {
-			text = getParser().getPrintString(new EObjectAdapter(getParserElement()), getParserOptions().intValue());
+		EObject parserElement = getParserElement();
+		if (parserElement != null && getParser() != null) {
+			text = getParser().getPrintString(new EObjectAdapter(parserElement), getParserOptions().intValue());
 		}
 		if (text == null || text.length() == 0) {
 			text = defaultText;
@@ -229,7 +229,7 @@ public class EReferenceLowerBoundUpperBoundEditPart extends LabelEditPart implem
 	 * @generated
 	 */
 	public String getEditText() {
-		if (getParser() == null) {
+		if (getParserElement() == null || getParser() == null) {
 			return ""; //$NON-NLS-1$
 		}
 		return getParser().getEditString(new EObjectAdapter(getParserElement()), getParserOptions().intValue());
@@ -275,7 +275,7 @@ public class EReferenceLowerBoundUpperBoundEditPart extends LabelEditPart implem
 	 * @generated
 	 */
 	public IContentAssistProcessor getCompletionProcessor() {
-		if (getParser() == null) {
+		if (getParserElement() == null || getParser() == null) {
 			return null;
 		}
 		return getParser().getCompletionProcessor(new EObjectAdapter(getParserElement()));
