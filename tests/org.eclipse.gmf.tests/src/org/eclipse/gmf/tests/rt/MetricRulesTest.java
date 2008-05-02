7650f3a0207f4f0941a93d58d3b1833c16c39d3e
atikhomirov <atikhomirov>
2008-05-02 18:38:50 +0000

    [230014] refactor MetricProvider.xpt

--- tests/org.eclipse.gmf.tests/src/org/eclipse/gmf/tests/rt/MetricRulesTest.java
+++ tests/org.eclipse.gmf.tests/src/org/eclipse/gmf/tests/rt/MetricRulesTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005 Borland Software Corporation
+ * Copyright (c) 2005, 2008 Borland Software Corporation
  * 
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which
@@ -11,8 +11,8 @@
 package org.eclipse.gmf.tests.rt;
 
 import java.lang.reflect.Method;
-import java.util.List;
 
+import org.eclipse.emf.codegen.util.CodeGenUtil;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EObject;
@@ -21,7 +21,10 @@ import org.eclipse.gmf.codegen.gmfgen.GenMetricContainer;
 import org.eclipse.gmf.codegen.gmfgen.GenMetricRule;
 import org.eclipse.gmf.mappings.MetricContainer;
 
-
+/**
+ * TODO: need to validate that not only calcXXX methods are present and work,
+ * but also rest of the calculate[Domain|Diagram|Notation]ElementMetrics. 
+ */
 public class MetricRulesTest extends RuntimeDiagramTestBase {
 	private MetricContainer metricContainer;
 	private GenMetricContainer genMetricContainer;
@@ -31,7 +34,7 @@ public class MetricRulesTest extends RuntimeDiagramTestBase {
 	}
 
 	protected void setUp() throws Exception {
-		super.setUp();		
+		super.setUp();
 		this.metricContainer = getSetup().getMapModel().getMapping().getMetrics();
 		this.genMetricContainer = getGenModel().getGenDiagram().getEditorGen().getMetrics();		
 		assertTrue(
@@ -44,47 +47,47 @@ public class MetricRulesTest extends RuntimeDiagramTestBase {
 				"Problem in gen-model transformation detected", //$NON-NLS-1$
 				genMetricContainer != null && genMetricContainer.getMetrics().size() == metricContainer.getMetrics().size());
 		Class<?> providerClass = null;
-		Class<?> viewClass = null;		
+//		Class<?> viewClass = null;		
 		try {
 			providerClass = loadGeneratedClass(getGenModel().getGenDiagram().getMetricProviderQualifiedClassName());		
-			viewClass = loadGeneratedClass(getGenModel().getGenDiagram().getMetricProviderQualifiedClassName() + "$ResultView"); //$NON-NLS-1$
+			loadGeneratedClass(getGenModel().getGenDiagram().getMetricProviderQualifiedClassName() + "$ResultView"); //$NON-NLS-1$
 		} catch (ClassNotFoundException e) {
 			fail("Could not find generated metric provider or view"); //$NON-NLS-1$
 		}
 			
-		Method metricAccessor = null;
-		try {
-			metricAccessor = providerClass.getMethod("getMetrics", (Class[]) null); //$NON-NLS-1$
-		} catch (Exception e) {			
-			fail("Could not find generated metric definitions accessor"); //$NON-NLS-1$			
-		}
-		
-		List<?> metrics = (List<?>)metricAccessor.invoke(viewClass, (Object[]) null); 
-		assertEquals(
-			"All metrics from gmfmap must be registered", //$NON-NLS-1$
-			metrics.size(), metricContainer.getMetrics().size());	
+//		Method metricAccessor = null;
+//		try {
+//			metricAccessor = providerClass.getMethod("getMetricKeys", (Class[]) null); //$NON-NLS-1$
+//		} catch (Exception e) {			
+//			fail("Could not find generated accessor for metric keys in " + providerClass.getName()); //$NON-NLS-1$			
+//		}
+//		
+//		String[] metricKeys = (String[])metricAccessor.invoke(viewClass WTF??? - method was static!, (Object[]) null); 
+//		assertEquals("All metrics from gmfmap must be registered", //$NON-NLS-1$
+//			metricKeys.length, metricContainer.getMetrics().size());	
 		assertMetricEvaluation(providerClass);
 	}
 	
 	private void assertMetricEvaluation(Class<?> metricProviderClass) {
 		Method calcMetricMethod = null;
-		try {
-			calcMetricMethod = metricProviderClass.getMethod("calculateMetric", new Class[] { String.class, Object.class }); //$NON-NLS-1$
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail("Could not find calculateMetric method in the provider. " + e.toString()); //$NON-NLS-1$
-		} 
-		
 		for (GenMetricRule nextRule : genMetricContainer.getMetrics()) {
 			EClassifier eClassifier = nextRule.getTarget().getContext().getEcoreClassifier();
 			EPackage ePackage = EPackage.Registry.INSTANCE.getEPackage(eClassifier.getEPackage().getNsURI());
 			assertNotNull("EPackage must be already generated and initialized", ePackage); //$NON-NLS-1$
 			EClassifier generatedClassifier = ePackage.getEClassifier(eClassifier.getName());
 			assertNotNull("Could not find metric target classifier", generatedClassifier); //$NON-NLS-1$
-			
+			assertNotNull("PREREQ", generatedClassifier.getInstanceClass());
+
+			try {
+				calcMetricMethod = metricProviderClass.getMethod("calc" + CodeGenUtil.capName(CodeGenUtil.validJavaIdentifier(nextRule.getKey())), new Class[] { generatedClassifier.getInstanceClass() }); //$NON-NLS-1$
+			} catch (Exception e) {
+				e.printStackTrace();
+				fail("Could not find calculateMetric method in the provider. " + e.toString()); //$NON-NLS-1$
+			} 
+
 			EObject contextInstance = ePackage.getEFactoryInstance().create((EClass)generatedClassifier); 
 			try {
-				Object metricValue = calcMetricMethod.invoke(null, new Object[] { nextRule.getKey(), contextInstance });
+				Object metricValue = calcMetricMethod.invoke(null, new Object[] { contextInstance });
 				assertTrue(metricValue instanceof Number);
 			} catch (Exception e) {
 				fail("Metric rule evaluation failed. " + e.toString()); //$NON-NLS-1$
