dc8a063d026ef37810b9635605dd16007cf8af65
ashatalin <ashatalin>
2008-07-10 12:29:49 +0000

    [217307] - "Add extension model" action: impossible use for additing nondeployed models

--- plugins/org.eclipse.gmf.map.edit/src/org/eclipse/gmf/mappings/presentation/GMFMapModelWizard.java
+++ plugins/org.eclipse.gmf.map.edit/src/org/eclipse/gmf/mappings/presentation/GMFMapModelWizard.java
@@ -20,6 +20,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.ContentHandler;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -127,7 +128,7 @@ public class GMFMapModelWizard extends Wizard implements INewWizard {
 
 							// Create a resource for this file.
 							//
-							Resource resource = resourceSet.createResource(fileURI);
+							Resource resource = resourceSet.createResource(fileURI, ContentHandler.UNSPECIFIED_CONTENT_TYPE);
 
 							// Add the initial model object to the contents.
 							//
