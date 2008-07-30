0a8a90d18af413159f602eda1da6c34d6ddc1511
dstadnik <dstadnik>
2008-07-30 21:34:56 +0000

    [2410] support document properties editing

--- plugins/org.eclipse.gmf.runtime.lite.svg/src/org/eclipse/gmf/runtime/lite/svg/SVGFigure.java
+++ plugins/org.eclipse.gmf.runtime.lite.svg/src/org/eclipse/gmf/runtime/lite/svg/SVGFigure.java
@@ -15,6 +15,11 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import org.apache.batik.dom.svg.SAXSVGDocumentFactory;
 import org.apache.batik.transcoder.Transcoder;
 import org.apache.batik.transcoder.TranscoderException;
@@ -27,10 +32,12 @@ import org.eclipse.draw2d.Figure;
 import org.eclipse.draw2d.Graphics;
 import org.eclipse.gmf.internal.runtime.lite.svg.Activator;
 import org.eclipse.gmf.internal.runtime.lite.svg.ImageTranscoderEx;
+import org.eclipse.gmf.internal.runtime.lite.svg.InferringNamespaceContext;
 import org.eclipse.gmf.internal.runtime.lite.svg.SVGGraphics2D;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 public class SVGFigure extends Figure {
 
@@ -66,7 +73,7 @@ public class SVGFigure extends Figure {
 		}
 	}
 
-	private Document getDocument() {
+	protected final Document getDocument() {
 		if (failedToLoadDocument) {
 			return null;
 		}
@@ -74,6 +81,27 @@ public class SVGFigure extends Figure {
 			loadDocument();
 		}
 		return document;
+	}
+
+	private XPath getXPath() {
+		XPath xpath = XPathFactory.newInstance().newXPath();
+		xpath.setNamespaceContext(new InferringNamespaceContext(getDocument().getDocumentElement()));
+		return xpath;
+	}
+
+	/**
+	 * Executes XPath query over the SVG document.
+	 */
+	protected final NodeList getNodes(String query) {
+		Document document = getDocument();
+		if (document != null) {
+			try {
+				return (NodeList) getXPath().evaluate(query, document, XPathConstants.NODESET);
+			} catch (XPathExpressionException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return null;
 	}
 
 	private void renderDocument(Transcoder transcoder, Document document) {
