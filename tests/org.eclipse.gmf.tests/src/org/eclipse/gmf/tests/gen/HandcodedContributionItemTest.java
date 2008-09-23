abe0021c287f4bbbcea4ef888c68191a3ccaaed6
atikhomirov <atikhomirov>
2008-09-23 14:54:41 +0000

    [174961] Code generation support for custom actions

--- tests/org.eclipse.gmf.tests/src/org/eclipse/gmf/tests/gen/HandcodedContributionItemTest.java
+++ tests/org.eclipse.gmf.tests/src/org/eclipse/gmf/tests/gen/HandcodedContributionItemTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006 Borland Software Corporation
+ * Copyright (c) 2006, 2008 Borland Software Corporation
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -14,7 +14,12 @@ package org.eclipse.gmf.tests.gen;
 import org.eclipse.gmf.codegen.gmfgen.GMFGenFactory;
 import org.eclipse.gmf.codegen.gmfgen.GenActionFactoryContributionItem;
 import org.eclipse.gmf.codegen.gmfgen.GenApplication;
+import org.eclipse.gmf.codegen.gmfgen.GenContributionItem;
+import org.eclipse.gmf.codegen.gmfgen.GenCustomAction;
+import org.eclipse.gmf.codegen.gmfgen.GenEditorGenerator;
 import org.eclipse.gmf.codegen.gmfgen.GenMenuManager;
+import org.eclipse.gmf.codegen.gmfgen.GenSharedContributionItem;
+import org.eclipse.gmf.codegen.gmfgen.GenToolBarManager;
 
 import junit.framework.TestCase;
 
@@ -23,38 +28,46 @@ import junit.framework.TestCase;
  */
 public class HandcodedContributionItemTest extends TestCase {
 
-	/**
-	 * @param name
-	 */
 	public HandcodedContributionItemTest(String name) {
 		super(name);
 	}
 
-	public void testGetApplicationMethod() {
+	public void testGetEditorGenMethod() {
+		GenEditorGenerator editorGen = GMFGenFactory.eINSTANCE.createGenEditorGenerator();
 		GenApplication app = GMFGenFactory.eINSTANCE.createGenApplication();
+		editorGen.setApplication(app);
 		final GenActionFactoryContributionItem item1 = GMFGenFactory.eINSTANCE.createGenActionFactoryContributionItem();
 		item1.setName("aaa");
 		app.getSharedContributionItems().add(item1);
 		GenMenuManager menu = GMFGenFactory.eINSTANCE.createGenMenuManager();
-		final GenActionFactoryContributionItem item2 = GMFGenFactory.eINSTANCE.createGenActionFactoryContributionItem();
-		item2.setName("bbb");
+		final GenSharedContributionItem item2 = GMFGenFactory.eINSTANCE.createGenSharedContributionItem();
+		item2.setActualItem(item1);
 		menu.getItems().add(item2);
 		GenMenuManager subMenu = GMFGenFactory.eINSTANCE.createGenMenuManager();
-		final GenActionFactoryContributionItem item3 = GMFGenFactory.eINSTANCE.createGenActionFactoryContributionItem();
+		final GenCustomAction item3 = GMFGenFactory.eINSTANCE.createGenCustomAction();
 		item3.setName("bbb");
 		subMenu.getItems().add(item3);
 		menu.getItems().add(subMenu);
+		GenToolBarManager toolbarManager = GMFGenFactory.eINSTANCE.createGenToolBarManager();
+		GenContributionItem toolbarItem1;
+		GenContributionItem nestedToolbarMenu;
+		toolbarManager.getItems().add(toolbarItem1 = GMFGenFactory.eINSTANCE.createGenCommandAction());
+		toolbarManager.getItems().add(nestedToolbarMenu = GMFGenFactory.eINSTANCE.createGenMenuManager());
 		
 		app.setMainMenu(menu);
+		app.setMainToolBar(toolbarManager);
 
-		assertEquals(app, menu.getApplication());
+		assertEquals(editorGen, menu.getEditorGen());
+		assertEquals(app, menu.getEditorGen().getApplication());
 		assertNull(menu.getOwner());
-		assertEquals(menu, item2.getOwner());
-		assertEquals(app, item2.getApplication());
-		assertEquals(app, item1.getApplication());
 		assertNull(item1.getOwner());
-		assertEquals(app, subMenu.getApplication());
-		assertEquals(app, item3.getApplication());
+		assertEquals(menu, item2.getOwner());
+		assertEquals(editorGen, item2.getOwner().getEditorGen());
+		assertEquals(editorGen, item3.getOwner().getEditorGen());
+		assertEquals(editorGen, toolbarManager.getEditorGen());
+		assertEquals(toolbarManager, toolbarItem1.getOwner());
+		assertEquals(editorGen, toolbarItem1.getOwner().getEditorGen());
+		assertEquals(editorGen, nestedToolbarMenu.getOwner().getEditorGen());
 		assertEquals(subMenu, item3.getOwner());
 		assertEquals(menu, subMenu.getOwner());
 	}
