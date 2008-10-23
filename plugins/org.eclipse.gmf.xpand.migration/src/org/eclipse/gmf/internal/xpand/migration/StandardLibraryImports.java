c1b83083ae1f1dd6dfdbfb70f98faece26d61549
ashatalin <ashatalin>
2008-10-23 10:23:18 +0000

    [243157] - Develop automatic migration tool creating QVTO/OCL constructions having Xtend as an input - Additional String operation necessary for "pureOCL" query language

--- plugins/org.eclipse.gmf.xpand.migration/src/org/eclipse/gmf/internal/xpand/migration/StandardLibraryImports.java
+++ plugins/org.eclipse.gmf.xpand.migration/src/org/eclipse/gmf/internal/xpand/migration/StandardLibraryImports.java
@@ -39,6 +39,8 @@ public class StandardLibraryImports extends AbstractImportsManager {
 		operationsMap.put(BuiltinMetaModel.EString_Matches, MapEntry.newStringLibEntry("xpandMatches"));
 		operationsMap.put(BuiltinMetaModel.EString_StartsWith, MapEntry.newStringLibEntry("xpandStartsWith"));
 		operationsMap.put(BuiltinMetaModel.EString_EndsWith, MapEntry.newStringLibEntry("xpandEndsWith"));
+		operationsMap.put(BuiltinMetaModel.EString_Plus_EJavaObject, MapEntry.newEntry("concat"));
+		operationsMap.put(BuiltinMetaModel.EString_Length, MapEntry.newStringLibEntry("xpandLength"));
 		operationsMap.put(BuiltinMetaModel.Object_CompareTo, MapEntry.newOclAnyLibEntry("xpandCompareTo"));
 		operationsMap.put(BuiltinMetaModel.Object_ToString, MapEntry.newEntry("repr"));
 		operationsMap.put(BuiltinMetaModel.Int_Div_Int, MapEntry.newEntry("div"));
