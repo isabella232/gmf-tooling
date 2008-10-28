5bc0e83caa2429841d7222b15cffdb5debe9b9cf
ashatalin <ashatalin>
2008-10-28 16:40:02 +0000

    Generating .qvto extensions using first xpand extension parameter as a context instead of static extensions.

--- tests/org.eclipse.gmf.tests.xpand.migration/resources/org/eclipse/gmf/tests/xpand/migration/JavaExtensionsContainer.java
+++ tests/org.eclipse.gmf.tests.xpand.migration/resources/org/eclipse/gmf/tests/xpand/migration/JavaExtensionsContainer.java
@@ -1,9 +1,9 @@
 package org.eclipse.gmf.tests.xpand.migration;
 public class JavaExtensionsContainer {
 public static class Metainfo {
-public static String[] simpleNativeExtension(String parameter, Boolean parameter2) { return new String[] {"oclstdlib::OclVoid", "oclstdlib::String", "oclstdlib::Boolean", "oclstdlib::String"}; }
-public static String[] modelElementAsNativeExtensionParameter(org.eclipse.emf.ecore.EObject param1, org.eclipse.emf.ecore.EPackage param2) { return new String[] {"oclstdlib::OclVoid", "ecore::EObject", "ecore::EPackage", "ecore::EClass"}; }
-public static String[] collectionsInNativeExtensions(java.util.Collection param1, java.util.Set param2, java.util.List param3, java.util.List param4) { return new String[] {"oclstdlib::OclVoid", "Collection(ecore::EObject)", "Set(ecore::EPackage)", "Sequence(oclstdlib::OclAny)", "Sequence(ecore::EClassifier)", "Sequence(ecore::EClass)"}; }
+public static String[] simpleNativeExtension(String parameter, Boolean parameter2) { return new String[] {"oclstdlib::String", "oclstdlib::Boolean", "oclstdlib::String"}; }
+public static String[] modelElementAsNativeExtensionParameter(org.eclipse.emf.ecore.EObject param1, org.eclipse.emf.ecore.EPackage param2) { return new String[] {"ecore::EObject", "ecore::EPackage", "ecore::EClass"}; }
+public static String[] collectionsInNativeExtensions(java.util.Collection param1, java.util.Set param2, java.util.List param3, java.util.List param4) { return new String[] {"Collection(ecore::EObject)", "Set(ecore::EPackage)", "Sequence(oclstdlib::OclAny)", "Sequence(ecore::EClassifier)", "Sequence(ecore::EClass)"}; }
 }
 public String simpleNativeExtension(String parameter, Boolean parameter2) { return org.eclipse.gmf.tests.xpand.migration.NativeExtensionsContainer.simpleNativeExtension(parameter, parameter2); }
 public org.eclipse.emf.ecore.EClass modelElementAsNativeExtensionParameter(org.eclipse.emf.ecore.EObject param1, org.eclipse.emf.ecore.EPackage param2) { return org.eclipse.gmf.tests.xpand.migration.NativeExtensionsContainer.modelElementAsNativeExtensionParameter(param1, param2); }
