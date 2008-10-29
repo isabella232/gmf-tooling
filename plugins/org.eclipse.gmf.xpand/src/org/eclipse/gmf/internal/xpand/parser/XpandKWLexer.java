47b0e6bd6804b8a373a606297a72f8d732c3b629
atikhomirov <atikhomirov>
2008-10-29 17:24:17 +0000

    mistakenly deleted copyright

--- plugins/org.eclipse.gmf.xpand/src/org/eclipse/gmf/internal/xpand/parser/XpandKWLexer.java
+++ plugins/org.eclipse.gmf.xpand/src/org/eclipse/gmf/internal/xpand/parser/XpandKWLexer.java
@@ -1,4 +1,15 @@
-package org.eclipse.gmf.internal.xpand.parser;
+/*
+ * Copyright (c) 2006, 2008 Borland Software Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     committers of openArchitectureWare - Xpand language syntax
+ *     Artem Tikhomirov (Borland) - LALR grammar
+ *                                - Migration to OCL expressions
+ */package org.eclipse.gmf.internal.xpand.parser;
 
 
 
