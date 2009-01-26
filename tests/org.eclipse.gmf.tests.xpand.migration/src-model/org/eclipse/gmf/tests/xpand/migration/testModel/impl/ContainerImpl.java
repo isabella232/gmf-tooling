124906e5ce834a47b7c8343f7916ff0cf845e2e0
ashatalin <ashatalin>
2009-01-26 16:15:43 +0000

    Copyrights were updated.

--- tests/org.eclipse.gmf.tests.xpand.migration/src-model/org/eclipse/gmf/tests/xpand/migration/testModel/impl/ContainerImpl.java
+++ tests/org.eclipse.gmf.tests.xpand.migration/src-model/org/eclipse/gmf/tests/xpand/migration/testModel/impl/ContainerImpl.java
@@ -1,6 +1,13 @@
 /**
- * <copyright>
- * </copyright>
+ * Copyright (c) 2008, 2009 Borland Software Corp.
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    Alexander Shatalin (Borland) - initial API and implementation
  *
  * $Id$
  */
