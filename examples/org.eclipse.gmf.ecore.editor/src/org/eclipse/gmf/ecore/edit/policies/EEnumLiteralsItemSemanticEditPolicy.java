e952f6c4c77319ffee3622592ad6dd0e1ecb08ef
atikhomirov <atikhomirov>
2009-02-18 16:39:40 +0000

    organize imports

--- examples/org.eclipse.gmf.ecore.editor/src/org/eclipse/gmf/ecore/edit/policies/EEnumLiteralsItemSemanticEditPolicy.java
+++ examples/org.eclipse.gmf.ecore.editor/src/org/eclipse/gmf/ecore/edit/policies/EEnumLiteralsItemSemanticEditPolicy.java
@@ -11,7 +11,6 @@
  */
 package org.eclipse.gmf.ecore.edit.policies;
 
-import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gmf.ecore.edit.commands.EEnumLiteralCreateCommand;
 import org.eclipse.gmf.ecore.providers.EcoreElementTypes;
