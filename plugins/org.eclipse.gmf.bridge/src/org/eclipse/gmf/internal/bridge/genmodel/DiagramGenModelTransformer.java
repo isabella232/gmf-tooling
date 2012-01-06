891e84d74692cf9d1703112c617b40990c187347
ghillairet <hillairet@montages.com>
2012-01-06 17:01:36 +0100

    suppress unnecessary annotation

--- plugins/org.eclipse.gmf.bridge/src/org/eclipse/gmf/internal/bridge/genmodel/DiagramGenModelTransformer.java
+++ plugins/org.eclipse.gmf.bridge/src/org/eclipse/gmf/internal/bridge/genmodel/DiagramGenModelTransformer.java
@@ -1014,7 +1014,6 @@ public class DiagramGenModelTransformer extends MappingTransformer {
 				if (lm != null) {
 					genBase = myHistory.find(lm);
 					assert genBase != null;
-					@SuppressWarnings("null")
 					boolean isGenBaseNull = genBase == null;
 					if(!isGenBaseNull) {
 						diagramTarget.getElement().add(genBase);
