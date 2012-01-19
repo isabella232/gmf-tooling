5db2a608d58a747949a93a42891f2534537ac7d8
ghillairet <hillairet@montages.com>
2012-01-19 20:26:30 +0100

    [QVTo bridge] Updated Header/Copyright form

--- tests/org.eclipse.gmf.tests/src/org/eclipse/gmf/tests/tr/QvtTransformComparisonNodeLabelsTest.java
+++ tests/org.eclipse.gmf.tests/src/org/eclipse/gmf/tests/tr/QvtTransformComparisonNodeLabelsTest.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+* Copyright (c) 2011, 2012 Montages A.G.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+* 	Guillaume Hillairet (Montages A.G.) : initial implementation
+*******************************************************************************/
 package org.eclipse.gmf.tests.tr;
 
 public class QvtTransformComparisonNodeLabelsTest extends QvtTransformCompareAbstractTest {
