cae119df8c290f80ab0d4fc6576598eaae888c0d
mgolubev <golubev@montages.com>
2012-03-18 23:51:57 +0100

    [161545] - required dependencies and singleton access to default GMFT OclTrackeFactory implementation

--- plugins/org.eclipse.gmf.tooling.runtime/src/org/eclipse/gmf/tooling/runtime/GMFToolingRuntimePlugin.java
+++ plugins/org.eclipse.gmf.tooling.runtime/src/org/eclipse/gmf/tooling/runtime/GMFToolingRuntimePlugin.java
@@ -1,5 +1,6 @@
 package org.eclipse.gmf.tooling.runtime;
 
+import org.eclipse.gmf.tooling.runtime.ocl.tracker.OclTrackerFactory;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -21,6 +22,28 @@ public class GMFToolingRuntimePlugin extends AbstractUIPlugin {
 
 	public static GMFToolingRuntimePlugin getInstance() {
 		return ourInstance;
+	}
+
+	/**
+	 * This is the main intended way to access the {@link OclTrackerFactory}. 
+	 * This method will return the suited implementation based on the available plugins.
+	 * Caller should not make any assumptions against the return implementation type.   
+	 */
+	public OclTrackerFactory getOclTrackerFactory() {
+		return null;
+	}
+
+	/**
+	 * This is the helper method allowing caller to access the specific {@link OclTrackerFactory} 
+	 * implementation without adding an explicit dependency to the containing plugin. 
+	 * <p> 
+	 * This method will try to return the preferred implementation based on the available plugins, 
+	 * but will roll back to default implementation if preferred one is not available.  
+	 * Caller still is not recommended to make any assumptions about the returned implementation type.   
+	 */
+	public OclTrackerFactory getOclTrackerFactory(OclTrackerFactory.Type type) {
+		//XXX: without IA included to Eclipse distribution, this method always return the default GMFT implementation.
+		return getOclTrackerFactory();
 	}
 
 }
