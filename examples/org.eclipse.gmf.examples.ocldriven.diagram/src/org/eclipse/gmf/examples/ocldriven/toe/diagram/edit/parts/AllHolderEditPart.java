c3d2e27a69a6a18754ecaf4224aed5deb1af6257
mgolubev <golubev@montages.com>
2012-05-02 03:19:37 +0200

    [237059] - sample diagram regenerated after: "custom reparent-aware editpolicy is generated for all containers", no need for custom EP anymore

--- examples/org.eclipse.gmf.examples.ocldriven.diagram/src/org/eclipse/gmf/examples/ocldriven/toe/diagram/edit/parts/AllHolderEditPart.java
+++ examples/org.eclipse.gmf.examples.ocldriven.diagram/src/org/eclipse/gmf/examples/ocldriven/toe/diagram/edit/parts/AllHolderEditPart.java
@@ -8,12 +8,13 @@ import org.eclipse.gef.Request;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.editpolicies.NonResizableEditPolicy;
 import org.eclipse.gef.handles.MoveHandle;
-import org.eclipse.gmf.examples.ocldriven.diagram.custom.policies.TOECreationEditPolicyWithCustomReparent;
 import org.eclipse.gmf.examples.ocldriven.toe.diagram.edit.policies.AllHolderCanonicalEditPolicy;
 import org.eclipse.gmf.examples.ocldriven.toe.diagram.edit.policies.AllHolderItemSemanticEditPolicy;
+import org.eclipse.gmf.examples.ocldriven.toe.diagram.part.TOEVisualIDRegistry;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
 import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.gmf.tooling.runtime.edit.policies.reparent.CreationEditPolicyWithCustomReparent;
 
 /**
  * @generated
@@ -44,7 +45,7 @@ public class AllHolderEditPart extends DiagramEditPart {
 		super.createDefaultEditPolicies();
 		installEditPolicy(EditPolicyRoles.SEMANTIC_ROLE, new AllHolderItemSemanticEditPolicy());
 		installEditPolicy(EditPolicyRoles.CANONICAL_ROLE, new AllHolderCanonicalEditPolicy());
-		installEditPolicy(EditPolicyRoles.CREATION_ROLE, new TOECreationEditPolicyWithCustomReparent());
+		installEditPolicy(EditPolicyRoles.CREATION_ROLE, new CreationEditPolicyWithCustomReparent(TOEVisualIDRegistry.TYPED_INSTANCE));
 		// removeEditPolicy(org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles.POPUPBAR_ROLE);
 	}
 
--- examples/org.eclipse.gmf.examples.ocldriven.diagram/src/org/eclipse/gmf/examples/ocldriven/toe/diagram/edit/parts/DepartmentDepartment_staff2EditPart.java
+++ examples/org.eclipse.gmf.examples.ocldriven.diagram/src/org/eclipse/gmf/examples/ocldriven/toe/diagram/edit/parts/DepartmentDepartment_staff2EditPart.java
@@ -1,17 +1,17 @@
 package org.eclipse.gmf.examples.ocldriven.toe.diagram.edit.parts;
 
 import org.eclipse.draw2d.IFigure;
-import org.eclipse.gmf.examples.ocldriven.diagram.custom.policies.TOECreationEditPolicyWithCustomReparent;
 import org.eclipse.gmf.examples.ocldriven.toe.diagram.edit.policies.DepartmentDepartment_staff2CanonicalEditPolicy;
 import org.eclipse.gmf.examples.ocldriven.toe.diagram.edit.policies.DepartmentDepartment_staff2ItemSemanticEditPolicy;
 import org.eclipse.gmf.examples.ocldriven.toe.diagram.part.Messages;
+import org.eclipse.gmf.examples.ocldriven.toe.diagram.part.TOEVisualIDRegistry;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeCompartmentEditPart;
-import org.eclipse.gmf.runtime.diagram.ui.editpolicies.CreationEditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.DragDropEditPolicy;
 import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
 import org.eclipse.gmf.runtime.diagram.ui.figures.ResizableCompartmentFigure;
 import org.eclipse.gmf.runtime.draw2d.ui.figures.ConstrainedToolbarLayout;
 import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.gmf.tooling.runtime.edit.policies.reparent.CreationEditPolicyWithCustomReparent;
 
 /**
  * @generated
@@ -52,10 +52,9 @@ public class DepartmentDepartment_staff2EditPart extends ShapeCompartmentEditPar
 	protected void createDefaultEditPolicies() {
 		super.createDefaultEditPolicies();
 		installEditPolicy(EditPolicyRoles.SEMANTIC_ROLE, new DepartmentDepartment_staff2ItemSemanticEditPolicy());
-		installEditPolicy(EditPolicyRoles.CREATION_ROLE, new CreationEditPolicy());
+		installEditPolicy(EditPolicyRoles.CREATION_ROLE, new CreationEditPolicyWithCustomReparent(TOEVisualIDRegistry.TYPED_INSTANCE));
 		installEditPolicy(EditPolicyRoles.DRAG_DROP_ROLE, new DragDropEditPolicy());
 		installEditPolicy(EditPolicyRoles.CANONICAL_ROLE, new DepartmentDepartment_staff2CanonicalEditPolicy());
-		installEditPolicy(EditPolicyRoles.CREATION_ROLE, new TOECreationEditPolicyWithCustomReparent());
 	}
 
 	/**
