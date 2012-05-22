a1a47bf74907be1c9a57499069f56ce4b382142b
mgolubev <golubev@montages.com>
2012-05-22 21:42:22 +0200

    [379484] post merge: VerticalLabelLocator repackaged to *.locators package

--- plugins/org.eclipse.gmf.tooling.runtime/src/org/eclipse/gmf/tooling/runtime/directedit/locator/CellEditorLocatorAccess.java
+++ plugins/org.eclipse.gmf.tooling.runtime/src/org/eclipse/gmf/tooling/runtime/directedit/locator/CellEditorLocatorAccess.java
@@ -4,17 +4,20 @@ import org.eclipse.draw2d.Label;
 import org.eclipse.gef.tools.CellEditorLocator;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.ITextAwareEditPart;
 import org.eclipse.gmf.runtime.draw2d.ui.figures.WrappingLabel;
+import org.eclipse.gmf.tooling.runtime.draw2d.labels.VerticalLabel;
 
 public class CellEditorLocatorAccess {
 
 	public static final CellEditorLocatorAccess INSTANCE = new CellEditorLocatorAccess();
 
 	public CellEditorLocator getTextCellEditorLocator(ITextAwareEditPart source) {
-		if (source.getFigure() instanceof WrappingLabel)
+		if (source.getFigure() instanceof WrappingLabel) {
 			return new TextCellEditorLocator((WrappingLabel) source.getFigure());
-		else {
-			return new LabelCellEditorLocator((Label) source.getFigure());
 		}
+		if (source.getFigure() instanceof VerticalLabel) {
+			return new VerticalLabelCellEditorLocator((VerticalLabel) source.getFigure());
+		}
+		return new LabelCellEditorLocator((Label) source.getFigure());
 	}
 
 }
