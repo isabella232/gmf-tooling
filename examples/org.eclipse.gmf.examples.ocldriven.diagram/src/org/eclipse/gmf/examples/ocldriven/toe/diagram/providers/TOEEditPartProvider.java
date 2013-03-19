ec40af92724aaf637f0e896887aacd7cbcfbb05f
mgolubev <golubev@montages.com>
2013-03-19 01:14:24 +0100

    [??????] - non generated part of the XXXEditPartProvider extracted to simplify the template (+ TOE sample to illustrate)

--- examples/org.eclipse.gmf.examples.ocldriven.diagram/src/org/eclipse/gmf/examples/ocldriven/toe/diagram/providers/TOEEditPartProvider.java
+++ examples/org.eclipse.gmf.examples.ocldriven.diagram/src/org/eclipse/gmf/examples/ocldriven/toe/diagram/providers/TOEEditPartProvider.java
@@ -1,141 +1,20 @@
 package org.eclipse.gmf.examples.ocldriven.toe.diagram.providers;
 
-import java.lang.ref.WeakReference;
-
-import org.eclipse.gef.EditPart;
-import org.eclipse.gef.EditPartFactory;
 import org.eclipse.gmf.examples.ocldriven.toe.diagram.edit.parts.AllHolderEditPart;
 import org.eclipse.gmf.examples.ocldriven.toe.diagram.edit.parts.TOEEditPartFactory;
 import org.eclipse.gmf.examples.ocldriven.toe.diagram.part.TOEVisualIDRegistry;
-import org.eclipse.gmf.runtime.common.core.service.IOperation;
-import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
-import org.eclipse.gmf.runtime.diagram.ui.services.editpart.AbstractEditPartProvider;
-import org.eclipse.gmf.runtime.diagram.ui.services.editpart.CreateGraphicEditPartOperation;
-import org.eclipse.gmf.runtime.diagram.ui.services.editpart.IEditPartOperation;
-import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.gmf.tooling.runtime.providers.DefaultEditPartProvider;
 
 /**
  * @generated
  */
-public class TOEEditPartProvider extends AbstractEditPartProvider {
+public class TOEEditPartProvider extends DefaultEditPartProvider {
 
 	/**
-	* @generated
-	*/
-	private EditPartFactory factory;
-
-	/**
-	* @generated
-	*/
-	private boolean allowCaching;
-
-	/**
-	* @generated
-	*/
-	private WeakReference cachedPart;
-
-	/**
-	* @generated
-	*/
-	private WeakReference cachedView;
-
-	/**
-	* @generated
-	*/
+	 * @generated
+	 */
 	public TOEEditPartProvider() {
-		setFactory(new TOEEditPartFactory());
-		setAllowCaching(true);
+		super(new TOEEditPartFactory(), TOEVisualIDRegistry.TYPED_INSTANCE, AllHolderEditPart.MODEL_ID);
 	}
 
-	/**
-	* @generated
-	*/
-	public final EditPartFactory getFactory() {
-		return factory;
-	}
-
-	/**
-	* @generated
-	*/
-	protected void setFactory(EditPartFactory factory) {
-		this.factory = factory;
-	}
-
-	/**
-	* @generated
-	*/
-	public final boolean isAllowCaching() {
-		return allowCaching;
-	}
-
-	/**
-	* @generated
-	*/
-	protected synchronized void setAllowCaching(boolean allowCaching) {
-		this.allowCaching = allowCaching;
-		if (!allowCaching) {
-			cachedPart = null;
-			cachedView = null;
-		}
-	}
-
-	/**
-	* @generated
-	*/
-	protected IGraphicalEditPart createEditPart(View view) {
-		EditPart part = factory.createEditPart(null, view);
-		if (part instanceof IGraphicalEditPart) {
-			return (IGraphicalEditPart) part;
-		}
-		return null;
-	}
-
-	/**
-	* @generated
-	*/
-	protected IGraphicalEditPart getCachedPart(View view) {
-		if (cachedView != null && cachedView.get() == view) {
-			return (IGraphicalEditPart) cachedPart.get();
-		}
-		return null;
-	}
-
-	/**
-	* @generated
-	*/
-	public synchronized IGraphicalEditPart createGraphicEditPart(View view) {
-		if (isAllowCaching()) {
-			IGraphicalEditPart part = getCachedPart(view);
-			cachedPart = null;
-			cachedView = null;
-			if (part != null) {
-				return part;
-			}
-		}
-		return createEditPart(view);
-	}
-
-	/**
-	* @generated
-	*/
-	public synchronized boolean provides(IOperation operation) {
-		if (operation instanceof CreateGraphicEditPartOperation) {
-			View view = ((IEditPartOperation) operation).getView();
-			if (!AllHolderEditPart.MODEL_ID.equals(TOEVisualIDRegistry.getModelID(view))) {
-				return false;
-			}
-			if (isAllowCaching() && getCachedPart(view) != null) {
-				return true;
-			}
-			IGraphicalEditPart part = createEditPart(view);
-			if (part != null) {
-				if (isAllowCaching()) {
-					cachedPart = new WeakReference(part);
-					cachedView = new WeakReference(view);
-				}
-				return true;
-			}
-		}
-		return false;
-	}
 }
