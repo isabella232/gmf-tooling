e0eaaa07939b7f5de4900ad78d83ec36f180c707
mgolubev <golubev@montages.com>
2014-03-12 22:52:43 +0100

    [releng]: create-drops script updated with codegen-xtend features as for Luna
    Change-Id: Ia5240dbe4c4ab605a5b278c2797917ffb0fb55b9

--- repository/gmft-create-drops.sh
+++ repository/gmft-create-drops.sh
@@ -1,53 +1,91 @@
+#./gmft-create-drops.sh -l 3.2.M201403121500 -i /home/data/users/mgolubev/milestones-3.2.M6a -o /home/data/users/mgolubev/drops
+
 #!/bin/bash
-# Script may take 3 command line parameters:
-# $1: Path to input gmf repository
-# $2: Path to output packages
-# $3: Label to append to the version string when creating drop files, e.g. M5 or RC1
-#
+#### key - value
+# -i -- inputPath
+# -o -- outputPath
+# -l -- outputFilesLabel
+# -e -- eclipse path
+#### single keys
+# -x -- don't use xtend
+# -r -- don't add root to xtend
+# set default paths, used when no corresponding key present
+PATH_TO_ECLIPSE=/home/data/users/mgolubev/temp-eclipse/eclipse/
+inputPath=/home/data/httpd/download.eclipse.org/modeling/gmp/gmf-tooling/updates/releases/
+outputPath=/home/data/users/mgolubev/drops
+useRoot=""
+useXtend=""
 
-PATH_TO_ECLIPSE=~/temp-eclipse/eclipse/
-
-#check script input parameters
-if [ $# -eq 3 ];
-then
-    inputPath=$1
-    outputPath=$2
-
-    if [ -d "$inputPath" -a -w "$inputPath" ]
-    then
-        echo "Input path: $inputPath"
+now=$(date +"%T")
+outputFilesLabel=""
+#get shell parameters
+echo "split params"
+while [ "$#" -ge 1 ]
+do
+  case "$1" in
+    "-i")
+       inputPath=$2
+       ;;
+    "-o")
+      outputPath=$2
+       ;;
+    "-e")
+       PATH_TO_ECLIPSE=$2
+       ;;
+    "-l")
+       outputFilesLabel=$2
+       ;;
+    "-x")
+       useXtend="-x"
+       ;;
+    "-r")
+       useRoot="-r"
+       ;;
+    esac
+    shift
+done;
+##########################################################
+    if [ -d "$PATH_TO_ECLIPSE" -a -x "$PATH_TO_ECLIPSE/eclipse" ]; then
+        echo "*Script built-in path to eclipse binary: $PATH_TO_ECLIPSE"
+    else
+        echo "*Script built-in path to eclipse binary $PATH_TO_ECLIPSE\ doesn't exist, edit the script to specify correct path to eclipse"
+        exit 1
+    fi
+    if [ -d "$inputPath" -a -w "$inputPath" ]; then
+        echo "*Input path: $inputPath"
     else
         echo "$inputPath doesn't exist"
         exit 1
     fi
-
-    if [ -d "$outputPath" -a -w "$outputPath" ]
-    then
-        echo "Output path: $outputPath"
+    if [ -d "$outputPath" -a -w "$outputPath" ]; then
+        echo "*Output path: $outputPath"
     else
         echo "$outputPath doesn't exist"
         exit 1
     fi
 
-    if [ -n "$3" ];
-    then
-        outputFilesLabel=$3
-        echo "Output files label: $outputFilesLabel"
-    fi
-else
-        echo "Input parameters are wrong: you must specify input repo path, output path and result prefix"
+if [ -z "$outputFilesLabel" ]; then
+    echo "Chose release. Input one of parameter:"
+    echo " [r] - releases,"
+    echo " [s] - stable,"
+    echo " [m] - milestone,"
+    echo " [i] - integration,"
+    echo " [n] - nightly"
+    read b
+
+    build="$(echo $b | awk '{print tolower($0)}')"
+    if [ "$build" != "r" -a "$build" != "s" -a "$build" != "m" -a "$build" != "i" -a "$build" != "n" ]; then
+        echo "unedfined type of build - $build" 
         exit 1
+    fi
+    echo "Input current version"
+    read version
+    mydate=$(date +'%Y%m%d')
+    mytime=$(date +'%H%M')
+    outputFilesLabel=$version$build$mydate$mytime
+    echo "current label = $outputFilesLabel"
 fi
-
-#check predefined path to eclipse binary
-if [ -d "$PATH_TO_ECLIPSE" -a -x "$PATH_TO_ECLIPSE/eclipse" ]
-then
-    echo "Script built-in path to eclipse binary: $PATH_TO_ECLIPSE"
-else
-    echo "Script built-in path to eclipse binary $PATH_TO_ECLIPSE doesn't exist, edit the script to specify correct path to eclipse"
-    exit 1
-fi
-
+##########################################################
 TEMP_DIR=~/temp/gmf-packaging-$outputFilesLabel
 mkdir -p $TEMP_DIR
 TEMP_DIR=$(cd "$TEMP_DIR" && pwd )
@@ -61,46 +99,25 @@ echo "Creating output directory: $OUTPUT_PATH"
 echo "Converting update site to runnable form"
 $PATH_TO_ECLIPSE/eclipse -nosplash -consoleLog -application org.eclipse.equinox.p2.repository.repo2runnable -source $inputPath -destination $TEMP_DIR/drops/eclipse
 cd $TEMP_DIR/drops
+##########################################################
 
-#SDK-EXPERIMENTAL
-zip -rq gmf-tooling-sdk-experimental-$outputFilesLabel.zip\
-    eclipse/features/org.eclipse.gmf.runtime.thirdparty_*\
-    eclipse/features/org.eclipse.gmf.sdk.experimental.source_*\
-    eclipse/features/org.eclipse.gmf.sdk.experimental_*\
-    eclipse/plugins/org.eclipse.gmf.codegen.lite.source_*\
-    eclipse/plugins/org.eclipse.gmf.codegen.lite.ui.source_*\
-    eclipse/plugins/org.eclipse.gmf.graphdef.editor.source_*\
-    eclipse/plugins/org.eclipse.gmf.runtime.lite.source_*\
-    eclipse/plugins/org.eclipse.gmf.map.editor.source_*\
-    eclipse/plugins/org.eclipse.gmf.codegen.lite_*\
-    eclipse/plugins/org.eclipse.gmf.codegen.lite.ui_*\
-    eclipse/plugins/org.eclipse.gmf.graphdef.editor_*\
-    eclipse/plugins/org.eclipse.gmf.runtime.lite_*\
-    eclipse/plugins/org.eclipse.gmf.map.editor_*
-
-#writing gmf-tooling-sdk-experimental package root files
-cd $TEMP_DIR
-mkdir -p "eclipse"
-unzip -oq drops/eclipse/binary/org.eclipse.gmf.sdk.experimental_root_* -d eclipse
-zip -rgq drops/gmf-tooling-sdk-experimental-$outputFilesLabel.zip eclipse
-rm -rf "eclipse"
-cd $TEMP_DIR/drops
-
-#calculating gmf-tooling-sdk-experimental package checksum
-md5sum gmf-tooling-sdk-experimental-$outputFilesLabel.zip > gmf-tooling-sdk-experimental-$outputFilesLabel.zip.md5
-echo "Created gmf-tooling-sdk-experimental-$outputFilesLabel.zip"
-
+##########################################################
 #SDK
+#REMOVE#
+#eclipse/plugins/org.eclipse.gmf.bridge.trace.source_*\
+#ADD#
+#eclipse/plugins/org.eclipse.gmf.runtime.lite.svg_*\+
+
 zip -rq gmf-tooling-sdk-$outputFilesLabel.zip\
-    eclipse/features/org.eclipse.gmf.doc_*\
     eclipse/features/org.eclipse.gmf.sdk_*\
+    eclipse/features/org.eclipse.gmf.doc_*\
     eclipse/features/org.eclipse.gmf.tooling.source_*\
     eclipse/features/org.eclipse.gmf.tooling_*\
     eclipse/features/org.eclipse.gmf.tooling.runtime_*\
     eclipse/features/org.eclipse.gmf.tooling.runtime.source_*\
+    eclipse/plugins/org.eclipse.gmf.sdk_*\
     eclipse/plugins/org.eclipse.gmf.doc_*\
     eclipse/plugins/org.eclipse.gmf.doc.ui_*\
-    eclipse/plugins/org.eclipse.gmf.sdk_*\
     eclipse/plugins/org.eclipse.gmf.common.source_*\
     eclipse/plugins/org.eclipse.gmf.codegen.source_*\
     eclipse/plugins/org.eclipse.gmf.codegen.edit.source_*\
@@ -143,8 +160,8 @@ zip -rq gmf-tooling-sdk-$outputFilesLabel.zip\
     eclipse/plugins/org.eclipse.gmf.xpand.editor_*\
     eclipse/plugins/org.eclipse.gmf.xpand.qvtlibrary_*\
     eclipse/plugins/org.eclipse.gmf.bridge.trace_*\
-	eclipse/plugins/org.eclipse.gmf.bridge.trace.source_*\
     eclipse/plugins/org.eclipse.gmf.tooling.runtime_*\
+    eclipse/plugins/org.eclipse.gmf.runtime.lite.svg_*\
     eclipse/plugins/org.eclipse.gmf.tooling.runtime.source_*
 
 #writing gmf-sdk package root files
@@ -159,10 +176,13 @@ cd $TEMP_DIR/drops
 md5sum gmf-tooling-sdk-$outputFilesLabel.zip > gmf-tooling-sdk-$outputFilesLabel.zip.md5
 echo "Created gmf-tooling-sdk-$outputFilesLabel.zip"
 
+##########################################################
 #TOOLING-RUNTIME
 zip -rq gmf-tooling-runtime-$outputFilesLabel.zip\
     eclipse/features/org.eclipse.gmf.tooling.runtime_*\
+    eclipse/features/org.eclipse.gmf.tooling.runtime.source_*\
     eclipse/plugins/org.eclipse.gmf.tooling.runtime_*\
+    eclipse/plugins/org.eclipse.gmf.tooling.runtime.source_*\
     eclipse/plugins/org.eclipse.gmf.runtime.lite.svg_*
 
 #writing gmf-tooling-runtime package root files
@@ -177,25 +197,29 @@ cd $TEMP_DIR/drops
 md5sum gmf-tooling-runtime-$outputFilesLabel.zip > gmf-tooling-runtime-$outputFilesLabel.zip.md5
 echo "Created gmf-tooling-runtime-$outputFilesLabel.zip"
 
-#TOOLING-RUNTIME-SOURCE
-zip -rq gmf-tooling-runtime-source-$outputFilesLabel.zip\
-    eclipse/features/org.eclipse.gmf.tooling.runtime.source_*\
-    eclipse/plugins/org.eclipse.gmf.tooling.runtime.source_*\
-    eclipse/plugins/org.eclipse.gmf.runtime.lite.svg.source_*
+##########################################################
+#TESTS
+#zip -rq gmf-tests-$outputFilesLabel.zip\
+#    eclipse/features/org.eclipse.gmf.tests_*\
+#    eclipse/plugins/org.eclipse.test_*\
+#    eclipse/plugins/org.eclipse.ant.optional.junit_*\
+#    eclipse/plugins/org.eclipse.gmf.tests_*\
+#    eclipse/plugins/org.eclipse.gmf.tests.xpand
+#
+#writing gmf-tests package root files
+#cd $TEMP_DIR
+#mkdir -p "eclipse"
+#unzip -oq drops/eclipse/binary/org.eclipse.gmf.tests_root_* -d eclipse
+#zip -rgq drops/gmf-tests-$outputFilesLabel.zip #eclipse
+#rm -rf "eclipse"
+#cd $TEMP_DIR/drops
+#
+#calculating gmf-tests package checksum
+#md5sum gmf-tests-$outputFilesLabel.zip > gmf-tests-$outputFilesLabel.zip.md5
+#echo "Created gmf-tests-$outputFilesLabel.zip"
 
-#writing gmf-tooling-runtime-source package root files
-cd $TEMP_DIR
-mkdir -p "eclipse"
-unzip -oq drops/eclipse/binary/org.eclipse.gmf.tooling.runtime_root_* -d eclipse
-zip -rgq drops/gmf-tooling-runtime-source-$outputFilesLabel.zip eclipse
-rm -rf "eclipse"
-cd $TEMP_DIR/drops
-
-#calculating gmf-tooling-runtime-source package checksum
-md5sum gmf-tooling-runtime-source-$outputFilesLabel.zip > gmf-tooling-runtime-source-$outputFilesLabel.zip.md5
-echo "Created gmf-tooling-runtime-source-$outputFilesLabel.zip"
-
-#TOOLING-SIMPLEMAP
+##########################################################
+#SIMPLEMAP-RUNTIME
 zip -rq gmf-tooling-simplemap-$outputFilesLabel.zip\
     eclipse/features/org.eclipse.gmf.tooling.simplemap_*\
     eclipse/plugins/org.eclipse.gmf.tooling.simplemap.bridge.ui_*\
@@ -206,7 +230,7 @@ zip -rq gmf-tooling-simplemap-$outputFilesLabel.zip\
     eclipse/plugins/org.eclipse.gmf.tooling.simplemap.model_*\
     eclipse/plugins/org.eclipse.gmf.tooling.simplemap.model.delegates_*\
     eclipse/plugins/org.eclipse.gmf.tooling.simplemap.model.edit_*\
-	eclipse/plugins/org.eclipse.gmf.tooling.simplemap.model.edit.properties_*\
+    eclipse/plugins/org.eclipse.gmf.tooling.simplemap.model.edit.properties_*\
     eclipse/plugins/org.eclipse.gmf.tooling.simplemap.model.triggers_*
 
 #writing gmf-tooling-simplemap package root files
@@ -221,10 +245,34 @@ cd $TEMP_DIR/drops
 md5sum gmf-tooling-simplemap-$outputFilesLabel.zip > gmf-tooling-simplemap-$outputFilesLabel.zip.md5
 echo "Created gmf-tooling-simplemap-$outputFilesLabel.zip"
 
+##########################################################
+#XTEND2
+if [ -z "$useXtend" ] 
+then
+zip -rq gmf-tooling-xtend2-"$outputFilesLabel".zip eclipse/features/org.eclipse.gmf.tooling.codegen.xtend2_*\
+    eclipse/plugins/org.eclipse.gmf.codegen.xtend_*\
+    eclipse/plugins/org.eclipse.gmf.codegen.xtend.ui_*
+
+#writing gmf-tooling-xtend2 package root files
+cd $TEMP_DIR
+mkdir -p "eclipse"
+if [ -z "$useRoot" ]
+then
+  unzip -oq drops/eclipse/binary/org.eclipse.gmf.tooling.codegen.xtend2_root_* -d eclipse
+  zip -rgq drops/gmf-tooling-xtend2-$outputFilesLabel.zip eclipse
+fi
+rm -rf "eclipse"
+cd $TEMP_DIR/drops
+#calculating gmf-tooling-xtend2 package checksum
+md5sum gmf-tooling-xtend2-$outputFilesLabel.zip > gmf-tooling-xtend2-$outputFilesLabel.zip.md5
+echo "Created gmf-tooling-xtend2-$outputFilesLabel.zip"
+fi
+##########################################################
 #copy result files to output dir
 echo "Copying result to $OUTPUT_PATH"
 cp *.zip $OUTPUT_PATH
 cp *.md5 $OUTPUT_PATH
+
 
 #deleting temporary files
 echo "Deleting temporary directory $TEMP_DIR"
