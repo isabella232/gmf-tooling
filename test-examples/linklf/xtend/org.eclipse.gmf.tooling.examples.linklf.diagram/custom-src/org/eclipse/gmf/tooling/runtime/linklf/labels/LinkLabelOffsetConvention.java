25a1a53a48c47fc9f76197fc61d89d9f61bb0dea
mgolubev <golubev@montages.com>
2014-08-17 02:29:56 +0200

    [441579] LinksLF implemented for ad-hoc sample, custom code, not separated
    Change-Id: I316a0c08c1a37cbf32e346baf304ee1fca0b804b

---
+++ test-examples/linklf/xtend/org.eclipse.gmf.tooling.examples.linklf.diagram/custom-src/org/eclipse/gmf/tooling/runtime/linklf/labels/LinkLabelOffsetConvention.java
@@ -0,0 +1,19 @@
+package org.eclipse.gmf.tooling.runtime.linklf.labels;
+
+import org.eclipse.draw2d.ConnectionLocator;
+
+class LinkLabelOffsetConvention {
+
+	public static int getPercentageOffsetAmongTheLineForAlignment(int alignment) {
+		switch (alignment) {
+		case ConnectionLocator.SOURCE:
+			return 100;
+		case ConnectionLocator.TARGET:
+			return 0;
+		case ConnectionLocator.MIDDLE:
+		default:
+			return 50;
+		}
+	}
+
+}
---
+++ test-examples/linklf/xtend/org.eclipse.gmf.tooling.examples.linklf.diagram/custom-src/org/eclipse/gmf/tooling/runtime/linklf/router/SnapToGridRectilinearRouter.java
@@ -0,0 +1,5 @@
+package org.eclipse.gmf.tooling.runtime.linklf.router;
+
+public class SnapToGridRectilinearRouter extends RectilinearRouter2 {
+
+}
